07507029d5f57b83ec320ff1d8f47d2fe229b797
Peter Ovchyn <peter@boarlabs.xyz>
2024-02-14 20:24:11 +0200

    Temp

--- src/django/contricleaner/lib/facility_list_item_validator.py
+++ src/django/contricleaner/lib/facility_list_item_validator.py
@@ -8,3 +8,33 @@ class FacilityListItemValidator:
         pass
 
 
+class SourceParser:
+    def __init__(self):
+        pass
+
+    def parsed_header(self):
+        pass
+    
+    def parsed_rows(self) -> list[dict]:
+        pass
+
+
+class CsvSourceParser(SourceParser):
+    def __init__(self):
+        pass
+
+    def parsed_header(self):
+        pass
+    
+    def parsed_rows(self):
+        pass
+
+class JSONSourceParser(SourceParser):
+    def __init__(self):
+        pass
+
+    def parsed_header(self):
+        pass
+    
+    def parsed_rows(self):
+        pass
