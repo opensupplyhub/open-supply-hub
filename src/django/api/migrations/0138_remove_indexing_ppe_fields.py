b33da4d328dc9f1d45fa70df4a8840bcc5d5f05a
mazur <mazursasha1990@gmail.com>
2024-02-15 09:48:21 +0200

    Update indexing functions to not index ppe fields

---
+++ src/django/api/migrations/0138_remove_indexing_ppe_fields.py
@@ -0,0 +1,42 @@
+from api.migrations._migration_helper import MigrationHelper
+
+from django.db import connection
+from django.db.migrations import Migration, RunPython
+
+helper = MigrationHelper(connection)
+
+
+def update_indexing_functions(apps, schema_editor):
+    '''
+    This function replaces the old index_facilities and
+    index_facilities_by functions with similar ones that do not
+    index ppe fields.
+    '''
+
+    helper.run_sql_files([
+        '0138_index_facilities.sql',
+        '0138_index_facilities_by.sql'
+        ])
+
+
+def revert_updating_indexing_functions(apps, schema_editor):
+    '''
+    This function reverts the changes made by update_indexing_functions
+    by running the SQL files for the previous version of indexing functions.
+    '''
+    helper.run_sql_files([
+        '0130_index_facilities.sql',
+        '0130_index_facilities_by.sql'
+        ])
+
+
+class Migration(Migration):
+
+    dependencies = [
+        ('api', '0137_remove_ppe_fields'),
+    ]
+
+    operations = [
+        RunPython(update_indexing_functions,
+                  revert_updating_indexing_functions)
+    ]
