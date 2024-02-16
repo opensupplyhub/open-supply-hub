dc2dea5cdbb8791b5565a778d4bb51ad95ef4377
Roman Stolar <roman.stolar@speedandfunction.com>
2024-02-16 12:19:36 +0200

    move date preparation to utils object instead of context that should collect scenario

--- src/django/api/limitation/date/date_limitation_context.py
+++ src/django/api/limitation/date/date_limitation_context.py
@@ -1,31 +1,21 @@
-from datetime import datetime
-
 from api.limitation.date.date_limitation import (
     DateLimitation
+)
+
+from api.limitation.date.date_limitation_utils import (
+    DateLimitationUtils
 )
 
 
 class DateLimitationContext:
     __date_limitation: DateLimitation
 
+    utils = DateLimitationUtils()
+
     def setStrategy(self, strategy: DateLimitation):
         self.__date_limitation = strategy
 
     def execute(self, start_date) -> DateLimitation:
-        date = self.__prepare_start_date(start_date)
+        date = self.utils.prepare_start_date(start_date)
         return self.__date_limitation.execute(date)
 
-    def __prepare_start_date(self, date: datetime):
-        MINIMUM_DAY_TO_ROUND = 28
-        END_OF_THE_YEAR = 12
-
-        if date.day > MINIMUM_DAY_TO_ROUND:
-            if date.month == END_OF_THE_YEAR:
-                return date.replace(day=1,
-                                    month=1,
-                                    year=date.year + 1)
-
-            return date.replace(day=1,
-                                month=date.month + 1)
-
-        return date
