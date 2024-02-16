267e713f05d1e02a30c9369f82a5fac5e29ab4b8
mazur <mazursasha1990@gmail.com>
2024-02-16 13:09:24 +0200

    Add migration that removes the ppe switch

---
+++ src/django/api/migrations/0138_remove_ppe_switch.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+def remove_ppe_switch(apps, schema_editor):
+    '''
+    This function removes the ppe switch.
+    '''
+    Switch = apps.get_model('waffle', 'Switch')
+    Switch.objects.remove(name='ppe')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0137_remove_ppe_fields'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_ppe_switch)
+    ]
