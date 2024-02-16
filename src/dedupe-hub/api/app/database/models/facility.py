20f2a2bb8b3f7b0b0f6dbc4220d60cb8767c2c50
mazur <mazursasha1990@gmail.com>
2024-02-16 09:47:54 +0200

    Change formating in the modified files

--- src/dedupe-hub/api/app/database/models/facility.py
+++ src/dedupe-hub/api/app/database/models/facility.py
@@ -17,6 +17,6 @@ class Facility(Base):
     new_os_id = Column(String, nullable=True)
     has_inexact_coordinates = Column(Boolean, nullable=False, default=False)
     created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=func.now())
+                       nullable=False, server_default=func.now())
     updated_at = Column(TIMESTAMP(timezone=True),
-                        default=None, onupdate=func.now())
+                       default=None, onupdate=func.now())
