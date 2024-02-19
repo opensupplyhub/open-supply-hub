b317e4081d62f009f10f51041ba6e3dd55ef625f
Inessa Druzhkova <inessa.druzhkova@speedandfunction.com>
2024-02-19 16:39:09 +0100

    changed tests

--- src/django/api/tests/test_user_api_info.py
+++ src/django/api/tests/test_user_api_info.py
@@ -19,15 +19,31 @@ class UserApiInfoTest(TestCase):
         self.email_two = "two@example.com"
         self.email_three = "three@example.com"
         self.email_four = "four@example.com"
+        self.password = "password"
         self.user_one = User.objects.create(email=self.email_one, id=1)
+        self.user_one.set_password(self.password)
+        self.user_one.save()
         self.user_two = User.objects.create(email=self.email_two, id=2)
+        self.user_two.set_password(self.password)
+        self.user_two.save()
         self.user_three = User.objects.create(email=self.email_three, id=3)
+        self.user_three.set_password(self.password)
+        self.user_three.save()
         self.user_four = User.objects.create(email=self.email_four, id=4)
+        self.user_four.set_password(self.password)
+        self.user_four.save()
+
+        for u in [self.email_one,self.email_two,self.email_three]
+            self.client.post(
+                "/user-login/",
+                {"email": self.email_one, "password": self.password},
+                format="json",
+            )
+
         self.yearly_renewal_period = 'YEARLY'
         self.monthly_renewal_period = 'MONTHLY'
 
         self.url = '/user-api-info/'
-        self.FORBIDDEN = '403 Forbidden'
         self.IS_NOT_SET = 'Is not set'
 
         self.contrib_one = Contributor.objects.create(
@@ -81,25 +97,26 @@ class UserApiInfoTest(TestCase):
 
         self.current_usage_one = 10
         self.current_usage_two = 2
-        self.current_usage_three = -1
 
         self.expected_content = {
             'apiCallAllowance': '10',
-            'currentCallCount': '0',
+            'currentCallCount': 0,
             'renewalPeriod': 'YEARLY',
         }
 
-        self.serializer_one = UserApiInfoSerializer(self.user_one.id)
-        self.serializer_two = UserApiInfoSerializer(self.user_two.id)
+        self.serializer_one = UserApiInfoSerializer(self.user_one.id,
+                                                    self.contrib_one.id)
+        self.serializer_two = UserApiInfoSerializer(self.user_two.id,
+                                                    self.contrib_two.id)
         self.serializer_three = UserApiInfoSerializer(
-            self.user_three.id)
+            self.user_three.id, self.contrib_three.id)
         self.serializer_four = UserApiInfoSerializer(
-            self.user_four.id)
+            self.user_four.id, self.contrib_four.id)
 
     def test_get_api_call_limit(self):
-        period_limit_one = self.serializer_one.get_api_call_limit()
-        period_limit_two = self.serializer_two.get_api_call_limit()
-        period_limit_four = self.serializer_four.get_api_call_limit()
+        period_limit_one = self.serializer_one.data['api_call_limit']
+        period_limit_two = self.serializer_two.data['api_call_limit']
+        period_limit_four = self.serializer_four.data['api_call_limit']
 
         self.assertEqual(
            str(self.limit_one.period_limit), period_limit_one
@@ -108,13 +125,13 @@ class UserApiInfoTest(TestCase):
                 str(self.limit_two.period_limit), period_limit_two
         )
         self.assertEqual(
-                self.FORBIDDEN, period_limit_four
+                self.IS_NOT_SET, period_limit_four
         )
 
     def test_get_renewal_period(self):
-        renewal_period_one = self.serializer_one.get_renewal_period()
-        renewal_period_two = self.serializer_two.get_renewal_period()
-        renewal_period_three = self.serializer_three.get_renewal_period()
+        renewal_period_one = self.serializer_one.data['renewal_period']
+        renewal_period_two = self.serializer_two.data['renewal_period']
+        renewal_period_three = self.serializer_three.data['renewal_period']
         self.assertEqual(
             self.yearly_renewal_period, renewal_period_one
         )
@@ -146,18 +163,18 @@ class UserApiInfoTest(TestCase):
             )
             r.created_at = now
             r.save()
-        current_usage_one = self.serializer_one.get_current_usage()
-        current_usage_two = self.serializer_two.get_current_usage()
-        current_usage_three = self.serializer_three.get_current_usage()
+        current_usage_one = self.serializer_one.data['current_usage']
+        current_usage_two = self.serializer_two.data['current_usage']
+        current_usage_three = self.serializer_three.data['current_usage']
 
         self.assertEqual(
-            current_usage_one, str(self.current_usage_one)
+            current_usage_one, self.current_usage_one
         )
         self.assertEqual(
-            current_usage_two, str(self.current_usage_two)
+            current_usage_two, self.current_usage_two
         )
         self.assertEqual(
-            current_usage_three, '0'
+            current_usage_three, 0
         )
 
     def test_user_api_endpoint(self):
