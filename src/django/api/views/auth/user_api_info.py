5d2f92a5be874d69b399249c7d33a84130a8c71f
Inessa Druzhkova <inessa.druzhkova@speedandfunction.com>
2024-02-19 12:50:50 +0100

    added verification for user

--- src/django/api/views/auth/user_api_info.py
+++ src/django/api/views/auth/user_api_info.py
@@ -2,13 +2,19 @@ from api.serializers.user_api_info_serializer import UserApiInfoSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from ...permissions import IsRegisteredAndConfirmed
+
 
 class UserAPIInfo(APIView):
+    permission_classes = (IsRegisteredAndConfirmed,)
+
     def get(self, request, uid=None):
         user_api_info_serializer = UserApiInfoSerializer(uid)
+
         period_limit = user_api_info_serializer.get_api_call_limit()
         renewal_period = user_api_info_serializer.get_renewal_period()
         successful_calls = user_api_info_serializer.get_current_usage()
+
         api_call_info_data = {
             "apiCallAllowance": period_limit,
             "currentCallCount": successful_calls,
