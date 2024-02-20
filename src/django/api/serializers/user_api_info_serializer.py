0c4db2fb5435eaaefe759e069bb9c101e6aa01a1
Inna Kocherzhuk <innakocherzhuk@gmail.com>
2024-02-20 13:21:25 +0100

    [OSDEV-752] API. Include token and call info on API settings tab (#32)
    [OSDEV-752](https://opensupplyhub.atlassian.net/browse/OSDEV-752)
    
      User can see in “API” tab:
      
      their API call allowance (API limit)
      
      current call count
      
      their renewal period (monthly or annual)
    
    ---------
    
    Co-authored-by: Roman Stolar <roman.stolar@speedandfunction.com>
    Co-authored-by: Inessa Druzhkova <inessa.druzhkova@speedandfunction.com>
    Co-authored-by: vladsha-dev <newshapovalov2002@gmail.com>

--- src/django/api/serializers/user_api_info_serializer.py
+++ src/django/api/serializers/user_api_info_serializer.py
@@ -1,4 +1,9 @@
+from django.utils import timezone
 from api.models import (RequestLog, ApiLimit)
+from api.limitation.date.date_limitation_context import DateLimitationContext
+from api.limitation.date.monthly_date_limitation import MonthlyDateLimitation
+from api.limitation.date.yearly_date_limitation import YearlyDateLimitation
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class UserApiInfoSerializer:
@@ -7,9 +12,35 @@ class UserApiInfoSerializer:
     def __init__(self, uid, contributor_id):
         self.data = {
             "api_call_limit": self.__get_api_call_limit(contributor_id),
-            "current_usage": self.__get_current_usage(uid),
             "renewal_period": self.__get_renewal_period(contributor_id),
+            "current_usage": self.__get_current_usage(uid, contributor_id),
         }
+
+    def __get_start_end_date_for_current_usage(self, contributor_id):
+        try:
+            apiLimit = ApiLimit.objects.get(
+                contributor_id=contributor_id)
+            renewal_period = apiLimit.renewal_period
+            date = apiLimit.period_start_date
+        except ObjectDoesNotExist:
+            apiLimit = None
+            renewal_period = ''
+
+        context = DateLimitationContext()
+
+        if renewal_period == '':
+            return {}
+        if renewal_period == 'MONTHLY':
+            context.set_strategy(MonthlyDateLimitation())
+        if renewal_period == 'YEARLY':
+            context.set_strategy(YearlyDateLimitation())
+
+        date_limitation = context.execute(date)
+
+        return {
+                "start_date": date_limitation.get_start_date(),
+                "end_date": timezone.now()
+                }
 
     def __get_api_call_limit(self, contributor_id):
         try:
@@ -30,11 +61,19 @@ class UserApiInfoSerializer:
         except ApiLimit.DoesNotExist:
             return 'Is not set'
 
-    def __get_current_usage(self, uid):
+    def __get_current_usage(self, uid, contributor_id):
+        range_of_dates = self.__get_start_end_date_for_current_usage(
+            contributor_id)
+
+        if not range_of_dates:
+            return 0
+
         try:
             successful_calls = RequestLog.objects.filter(
                 response_code__gte=200,
                 response_code__lte=299,
+                created_at__gte=range_of_dates["start_date"],
+                created_at__lte=range_of_dates["end_date"],
                 user_id=uid).count()
             return successful_calls
         except RequestLog.DoesNotExist:
