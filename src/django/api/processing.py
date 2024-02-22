d23a632196036230fbd39ee2f2883c220f002b12
mazur <mazursasha1990@gmail.com>
2024-02-22 12:45:58 +0200

    Move function get_country_code from processing.py to separate file in ./src/django/countries/

--- src/django/api/processing.py
+++ src/django/api/processing.py
@@ -165,22 +165,6 @@ def parse_csv_line(line):
     return list(csv.reader([line]))[0]
 
 
-def get_country_code(country):
-    # TODO: Handle minor spelling errors in country names
-    remove_new_lines_pattern = re.compile(r'[\n\r]+')
-
-    country = str(country).strip()
-    country = remove_new_lines_pattern.sub(' ', country)
-
-    if country.upper() in COUNTRY_NAMES:
-        return country.upper()
-    elif country.lower() in COUNTRY_CODES:
-        return COUNTRY_CODES[country.lower()]
-    else:
-        raise ValueError(
-            'Could not find a country code for "{0}".'.format(country))
-
-
 class ItemRemovedException(Exception):
     pass
 
