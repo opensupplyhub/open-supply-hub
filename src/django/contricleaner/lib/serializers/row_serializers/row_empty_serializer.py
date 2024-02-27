b971063f0b5c5d8ba02395d6453864cc0ca64ffc
mazur <mazursasha1990@gmail.com>
2024-02-27 16:38:55 +0200

    Add description for the RowEmptySerializer

--- src/django/contricleaner/lib/serializers/row_serializers/row_empty_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_empty_serializer.py
@@ -1,4 +1,8 @@
 class RowEmptySerializer:
+    '''
+    This class is used to validate the row and add the missing fields to the row.
+    '''
+
     def validate(self, row: dict, current: dict) -> dict:
         keys = set(row.keys()).difference(current.keys())
         for key in keys:
