390fe2b0c91ea9a45a0d6f4c5f411db90ea312d9
mazur <mazursasha1990@gmail.com>
2024-02-27 16:36:56 +0200

    Add description for the RowCountrySerializer

--- src/django/contricleaner/lib/serializers/row_serializers/row_country_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_country_serializer.py
@@ -2,6 +2,10 @@ from countries.lib.get_country_code import get_country_code
 
 
 class RowCountrySerializer:
+    '''
+    This class is responsible for validating the country field in the row.
+    '''
+
     def validate(self, row: dict, current: dict) -> dict:
         try:
             current["country_code"] = get_country_code(row["country"])
