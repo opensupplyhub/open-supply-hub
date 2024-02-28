2abfd0b9770e4bc0d2e0cd2c4ecb7a3305bd7d8c
Inessa Druzhkova <inessa.druzhkova@speedandfunction.com>
2024-02-28 11:37:26 +0100

    fixed query according to the UX, changed description

--- src/django/api/reports/api_request_by_user.sql
+++ src/django/api/reports/api_request_by_user.sql
@@ -1,19 +1,18 @@
--- Count the number of API requests made by each user for all the time
--- Count the number of API requests for the previous month
--- Count the number of API requests for the current month
+-- Count the number of API requests made by each user in each month up to the current date.
 -- Exclude users with openapparel.org or azavea.com email addresses.
 -- Count only successful calls
 
-select email, count(*) as request_call_count,
-  SUM(CASE WHEN to_char(l.created_at, 'YYYY-MM') >= to_char(date_trunc('month', now() - interval '1' month), 'YYYY-MM')
-  and to_char(l.created_at, 'YYYY-MM') <  to_char(date_trunc('month', now()), 'YYYY-MM') THEN 1 ELSE 0 END) AS prev_month,
-  SUM(CASE WHEN to_char(l.created_at, 'YYYY-MM-dd') >= to_char(date_trunc('month', now()), 'YYYY-MM-dd')
-  and to_char(l.created_at, 'YYYY-MM-dd') <  to_char(now(), 'YYYY-MM-dd') THEN 1 ELSE 0 END) AS curr_month
-from api_requestlog l
+SELECT
+  to_char(l.created_at, 'YYYY-MM') AS month,
+  email,
+  COUNT(*) AS api_request_count
+FROM api_requestlog l
 JOIN api_user u ON l.user_id = u.id
-where NOT u.email LIKE '%openapparel.org%'
+WHERE to_char(l.created_at, 'YYYY-MM-dd') < to_char(now(), 'YYYY-MM-dd')
+AND NOT u.email LIKE '%openapparel.org%'
 AND NOT u.email LIKE '%opensupplyhub.org%'
 AND NOT u.email LIKE '%azavea.com%'
-and l.response_code = 200 or l.response_code = 299
-GROUP BY email
-ORDER BY email;
+AND l.response_code = 200
+OR l.response_code = 299
+GROUP BY to_char(l.created_at, 'YYYY-MM'), email
+ORDER BY to_char(l.created_at, 'YYYY-MM'), email;
