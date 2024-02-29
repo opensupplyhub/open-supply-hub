f311cd7fc74c223e4993effb8488d94bd5ebae93
roman-stolar <88504352+roman-stolar@users.noreply.github.com>
2024-02-29 17:43:21 +0200

    [OSDEV-906] Create a shared interface for Row...Serializer's (#52)
    [OSDEV-906](https://opensupplyhub.atlassian.net/browse/OSDEV-906) Create
    a shared interface for Row...Serializer's
    - Added shared interface for RowSerializer's

--- src/django/contricleaner/lib/serializers/row_serializers/row_cleaned_user_data_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_cleaned_user_data_serializer.py
@@ -1,4 +1,7 @@
-class RowCleanedUserDataSerializer:
+from .row_serializer import RowSerializer
+
+
+class RowCleanedUserDataSerializer(RowSerializer):
     def validate(self, row: dict, current: dict) -> dict:
         current["cleaned_user_data"] = row.copy()
         return current
