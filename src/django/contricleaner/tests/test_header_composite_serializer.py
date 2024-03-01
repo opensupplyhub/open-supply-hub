e89157580ce8d9ab1a5edfd2a836cacec860870a
Roman Stolar <roman.stolar@speedandfunction.com>
2024-03-01 14:40:15 +0200

    add unit tests

---
+++ src/django/contricleaner/tests/test_header_composite_serializer.py
@@ -0,0 +1,30 @@
+import unittest
+
+from contricleaner.lib.dto.header_dto import HeaderDTO
+from contricleaner.lib.serializers.header_serializers.header_composite_serializer \
+    import HeaderCompositeSerializer
+
+
+class HeaderCompositeValidatorTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_validated_headers(self):
+        validator = HeaderCompositeSerializer()
+        headers = ["name",
+                   "address",
+                   "country",
+                   "sector"]
+
+        res = validator.get_validated_headers(headers)
+
+        expected = HeaderDTO(
+            headers=headers,
+            errors=[],
+        )
+
+        self.assertHeadersEqual(res, expected)
+
+    def assertHeadersEqual(self, res, expected):
+        self.assertEqual(res.errors, expected.errors)
+        self.assertEqual(res.headers, expected.headers)
---
+++ src/django/contricleaner/tests/test_header_required_serializer.py
@@ -0,0 +1,29 @@
+import unittest
+
+from contricleaner.lib.dto.header_dto import HeaderDTO
+from contricleaner.lib.serializers.header_serializers.header_required_serializer \
+    import HeaderRequiredSerializer
+
+
+class HeaderRequiredSerializerTest(unittest.TestCase):
+    def setUp(self):
+        self.serializer = HeaderRequiredSerializer()
+        self.current = {"errors": []}
+
+    def test_validate_required_headers(self):
+        headers_one = ["name",
+                       "address",
+                       "country",
+                       "sector"]
+
+        res_one = self.serializer.validate(headers_one, self.current)
+
+        self.assertCountEqual(len(res_one["errors"]), 0)
+
+        headers_two = ["name",
+                       "country",
+                       "sector"]
+
+        res_two = self.serializer.validate(headers_two, self.current)
+
+        self.assertCountEqual(len(res_two["errors"]), 1)
