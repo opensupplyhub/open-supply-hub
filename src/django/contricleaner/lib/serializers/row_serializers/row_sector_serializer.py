cea7450e77b1c9cdab3a25ec9debdd09096ea32b
mazur <mazursasha1990@gmail.com>
2024-03-04 12:37:44 +0200

    Comment the code that has to be moved to the contricleaner

--- src/django/contricleaner/lib/serializers/row_serializers/row_sector_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_sector_serializer.py
@@ -14,6 +14,29 @@ class RowSectorSerializer(RowSerializer):
         self.product_types = []
 
     def validate(self, row: dict, current: dict) -> dict:
+
+    # # facility_type_processing_type is a special "meta" field that attempts to
+        # # simplify the submission process for contributors.
+        # if (raw_data.get('facility_type_processing_type')):
+        #     if raw_data.get('facility_type') is None:
+        #         raw_data['facility_type'] = \
+        #             raw_data['facility_type_processing_type']
+        #     if raw_data.get('processing_type') is None:
+        #         raw_data['processing_type'] = \
+        #             raw_data['facility_type_processing_type']
+        # # Add a facility_type extended field if the user only
+        # # submitted a processing_type
+        # elif (raw_data.get('processing_type') and
+        #       raw_data.get('facility_type') is None):
+        #     raw_data['facility_type'] = raw_data['processing_type']
+        # # Add a processing_type extended field if the user only
+        # # submitted a facility_type
+        # elif (raw_data.get('facility_type') and
+        #       raw_data.get('processing_type') is None):
+        #     raw_data['processing_type'] = raw_data['facility_type']
+
+
+
         # self.parse_all_values(
         #     set([
         #         *row.get('sector', []),
@@ -21,6 +44,15 @@ class RowSectorSerializer(RowSerializer):
         #         *row.get('sector_product_type', [])
         #     ])
         # )
+
+
+        # if isinstance(row.get('sector', str)):
+        #     current["sector"]=row["sector"].split(",")
+        # else:
+        #     current["sector"]=[]
+
+        # "asdfasdf,asdfasdf,asdf,asd,asdf"=>["asdfasdf", "asdfasdf", "asdf", "asd", "asdf"]
+        # ["asdfasdf,asdfasdf,asdf,asd,asdf", "asdfasdf,asdfasdf,asdf,asd,asdf"] =>["asdfasdf", "asdfasdf", "asdf", "asd", "asdf", "asdfasdf", "asdfasdf", "asdf", "asd", "asdf" ]
 
         current["sector"] = row["sector"].split(",")
         print('current', current)
