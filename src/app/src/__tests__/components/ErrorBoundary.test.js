1029fcb048b0e0d9d73ca28f80607a165e573717
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2024-03-05 18:56:56 +0400

    Minor lint fixes

--- src/app/src/__tests__/components/ErrorBoundary.test.js
+++ src/app/src/__tests__/components/ErrorBoundary.test.js
@@ -11,62 +11,64 @@ const ERROR_MESSAGE = 'Error while fetching facilities'
 const CONTRIBUTOR_ID = 1705
 
 jest.mock('../../components/FacilityLists', () => {
-  const { Component } = jest.requireActual('react');
+	const { Component } = jest.requireActual('react');
 
-  class MockedFacilityLists extends Component {
-    render() {
-      throw new Error(ERROR_MESSAGE);
-    }
-  }
+	/* eslint-disable react/prefer-stateless-function */
+	class MockedFacilityLists extends Component {
+		/* eslint-disable react/require-render-return */
+		render() {
+			throw new Error(ERROR_MESSAGE);
+		}
+	}
 
-  return MockedFacilityLists;
+	return MockedFacilityLists;
 });
 
 const mockStore = configureStore([]);
 
 describe('Test ErrorBoundary component for API user with contributor id', () => {
-  let initialState;
-  let consoleSpy;
-  let mockLogErrorToRollbar;
+	let initialState;
+	let consoleSpy;
+	let mockLogErrorToRollbar;
 
-  beforeEach(() => {
-    initialState = mockStore({
-      auth: {
-        user: {
-          user: { ...USER_DEFAULT_STATE, 
-				isAnon: false, 
-				contributor_id: CONTRIBUTOR_ID, 
-				groups: [1, 2] 
+	beforeEach(() => {
+		initialState = mockStore({
+			auth: {
+			user: {
+				user: { ...USER_DEFAULT_STATE, 
+					isAnon: false, 
+					contributor_id: CONTRIBUTOR_ID, 
+					groups: [1, 2] 
+					},
+				},
 			},
-        },
-      },
-    });
+		});
 
-    mockLogErrorToRollbar = jest.spyOn(util, 'logErrorToRollbar').mockImplementation((window, error, user) => {
-		expect(user.contributor_id).toBe(CONTRIBUTOR_ID)
-		const userType = util.isApiUser(user) ? 'API user' : 'User';
-		expect(userType).toBe('API user')
-    });
-    // Suppress console.error() invocations during test runtime to avoid redundant output in the console.
-    consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
-  });
+		mockLogErrorToRollbar = jest.spyOn(util, 'logErrorToRollbar').mockImplementation((window, error, user) => {
+			expect(user.contributor_id).toBe(CONTRIBUTOR_ID)
+			const userType = util.isApiUser(user) ? 'API user' : 'User';
+			expect(userType).toBe('API user')
+		});
+		// Suppress console.error() invocations during test runtime to avoid redundant output in the console.
+		consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+	});
 
-  afterEach(() => {
-    consoleSpy.mockRestore();
-    mockLogErrorToRollbar.mockRestore();
-  });
+	afterEach(() => {
+		consoleSpy.mockRestore();
+		mockLogErrorToRollbar.mockRestore();
+	});
 
-  it('Displays an error message on error in FacilityLists', () => {
-    const { getByText, container } = render(
-      <Provider store={initialState}>
-        <ErrorBoundary>
-          <FacilityLists />
-        </ErrorBoundary>
-      </Provider>
-    );
+	it('Displays an error message on error in FacilityLists', () => {
+		const { container } = render(
+			<Provider store={initialState}>
+				<ErrorBoundary>
+					<FacilityLists />
+				</ErrorBoundary>
+			</Provider>
+		);
 
-	const errorDivElement = atob(container.querySelector('.notranslate').textContent);
-	expect(errorDivElement).toMatch(ERROR_MESSAGE);
-	expect(mockLogErrorToRollbar).toHaveBeenCalled();
-  });
+		const errorDivElement = atob(container.querySelector('.notranslate').textContent);
+		expect(errorDivElement).toMatch(ERROR_MESSAGE);
+		expect(mockLogErrorToRollbar).toHaveBeenCalled();
+	});
 });
