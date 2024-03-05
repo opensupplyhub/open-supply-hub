d3c9b87d2215670a83be92235254a51902221a57
mazur <mazursasha1990@gmail.com>
2024-03-05 11:54:28 +0200

    Implement parsing functionality for RowSectorSerializer

--- src/django/contricleaner/lib/serializers/row_serializers/row_sector_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_sector_serializer.py
@@ -1,5 +1,5 @@
 from .row_serializer import RowSerializer
-from typing import List
+from typing import Union, Set
 
 
 class SectorCacheInterface:
@@ -12,50 +12,31 @@ class RowSectorSerializer(RowSerializer):
     def __init__(self, sector_cache: SectorCacheInterface) -> None:
         self.sector_cache = sector_cache
 
-    # @staticmethod
-    # def split_values(value: any) -> List[str]:
-    #     if isinstance(value, str):
-    #         return value.split(', ')
-    #     elif isinstance(value, list):
-    #         return [RowSectorSerializer.split_values(v) for v in value]
-    #     else:
-    #         pass
-    #         # raise ValueError("Unsupported value type: {}".format(type(value)))
-    @staticmethod
-    def split_values(value: any) -> List[str]:
-        if isinstance(value, str):
-            return value.split(', ')
-        elif isinstance(value, list):
-            return [item for sublist in map(RowSectorSerializer.split_values, value) for item in sublist]
-        else:
-            return []
-
     def validate(self, row: dict, current: dict) -> dict:
+        values = RowSectorSerializer.split_values([
+                row.get('sector', []),
+                row.get('product_type', []),
+                row.get('sector_product_type', []),
+            ], ', ')
 
-        values = [
-                row.get('sector'),
-                row.get('product_type'),
-                row.get('sector_product_type'),
-            ]
-        print('values', values)
-        result_set = set()
-
-        for value in values:
-            if value and isinstance(value, str):
-                result_set.union(self.split_values(value))
-            elif value and isinstance(value, list):
-                result_set.union(self.split_values(tuple(value)))
-            else:
-                pass
-
-        print('result_set', result_set)
-
-        sectors, product_types = self.parse_all_values(result_set)
+        sectors, product_types = self.parse_all_values(values)
 
         current['product_types'] = product_types
         current['sectors'] = sectors
-        print('current', current)
+
         return current
+
+    @staticmethod
+    def split_values(value: Union[str, list], split: str) -> Set[str]:
+        if isinstance(value, str):
+            return set(value.split(split))
+        elif isinstance(value, list) or isinstance(value, set):
+            res = set()
+            for v in value:
+                res = res.union(RowSectorSerializer.split_values(v, split))
+            return res
+        else:
+            raise ValueError("Unsupported value type: {}".format(type(value)))
 
     def parse_all_values(self, all_values):
         DEFAULT_SECTOR_NAME = 'Unspecified'
@@ -65,18 +46,19 @@ class RowSectorSerializer(RowSerializer):
         product_types = []
         for value in all_values:
             clean_value = self.clean_value(value)
-            if (clean_value in sector_map):
+            if clean_value in sector_map:
                 sectors.append(sector_map[clean_value])
             else:
                 product_types.append(value)
 
-        if len(sectors) == 0:
+        if not sectors:
             sectors.append(DEFAULT_SECTOR_NAME)
-        
+
         sectors.sort()
         product_types.sort()
-        
+
         return sectors, product_types
 
-    def clean_value(self, value):
+    @staticmethod
+    def clean_value(value):
         return value.lower().strip()
