b175700ced01e33c149d986e6900dc824e393dc3
mazur <mazursasha1990@gmail.com>
2024-03-06 20:43:45 +0200

    Nodify RowFacilityTypeSerializer

--- src/django/contricleaner/lib/serializers/row_serializers/row_facility_type_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_facility_type_serializer.py
@@ -1,6 +1,7 @@
 from typing import Union, List
-from contricleaner.lib.serializers.row_serializers.row_serializer \
-    import RowSerializer
+from contricleaner.lib.serializers.row_serializers.row_serializer import (
+    RowSerializer,
+)
 
 
 class RowFacilityTypeSerializer(RowSerializer):
@@ -16,26 +17,28 @@ class RowFacilityTypeSerializer(RowSerializer):
         # attempts to simplify the submission process for contributors.
         if facility_type_processing_type:
             if facility_type is None:
-                current['facility_type'] = self.split_values(
-                    facility_type_processing_type
-                )
-            if processing_type is None:
-                current['processing_type'] = self.split_values(
-                    facility_type_processing_type
+                facility_type = facility_type_processing_type
 
-                )
+            if processing_type is None:
+                processing_type = facility_type_processing_type
+
         # Add a facility_type extended field if the user only
         # submitted a processing_type
         elif processing_type and not facility_type:
-            current['facility_type'] = self.split_values(
-               processing_type
-            )
+            facility_type = processing_type
+        # elif (row.get('processing_type') and
+        #         row.get('facility_type') is None):
+        #     current['facility_type'] = row['processing_type']
         # Add a processing_type extended field if the user only
         # submitted a facility_type
         elif facility_type and not processing_type:
-            current['processing_type'] = self.split_values(
-                facility_type
-            )
+            processing_type = facility_type
+        # elif (row.get('facility_type') and
+        #         row.get('processing_type') is None):
+        #     current['processing_type'] = row['facility_type']
+
+        current['facility_type'] = self.split_values(facility_type)
+        current['processing_type'] = self.split_values(processing_type)
 
         return current
 
