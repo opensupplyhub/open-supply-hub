2d79195f5f890fca657aff120ab373d2f7f5b46a
mazur <mazursasha1990@gmail.com>
2024-03-06 20:03:24 +0200

    Add basic functionality for RowFacilityTypeSerializer

--- src/django/contricleaner/lib/serializers/row_serializers/row_facility_type_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_facility_type_serializer.py
@@ -1,3 +1,4 @@
+from typing import Union, List
 from contricleaner.lib.serializers.row_serializers.row_serializer \
     import RowSerializer
 
@@ -5,22 +6,44 @@ from contricleaner.lib.serializers.row_serializers.row_serializer \
 class RowFacilityTypeSerializer(RowSerializer):
 
     def validate(self, row: dict, current: dict) -> dict:
+        facility_type = row.get('facility_type')
+        processing_type = row.get('processing_type')
+        facility_type_processing_type = row.get(
+            'facility_type_processing_type'
+        )
+
         # facility_type_processing_type is a special "meta" field that
         # attempts to simplify the submission process for contributors.
-        if (row.get('facility_type_processing_type')):
-            if row.get('facility_type') is None:
-                current['facility_type'] = row['facility_type_processing_type']
-            if row.get('processing_type') is None:
-                current['processing_type'] = row['facility_type_processing_type']
+        if facility_type_processing_type:
+            if facility_type is None:
+                current['facility_type'] = self.split_values(
+                    facility_type_processing_type
+                )
+            if processing_type is None:
+                current['processing_type'] = self.split_values(
+                    facility_type_processing_type
+
+                )
         # Add a facility_type extended field if the user only
         # submitted a processing_type
-        elif (row.get('processing_type') and
-                row.get('facility_type') is None):
-            current['facility_type'] = row['processing_type']
+        elif processing_type and not facility_type:
+            current['facility_type'] = self.split_values(
+               processing_type
+            )
         # Add a processing_type extended field if the user only
         # submitted a facility_type
-        elif (row.get('facility_type') and
-                row.get('processing_type') is None):
-            current['processing_type'] = row['facility_type']
+        elif facility_type and not processing_type:
+            current['processing_type'] = self.split_values(
+                facility_type
+            )
 
         return current
+
+    @staticmethod
+    def split_values(value: Union[str, list]) -> List[str]:
+        if isinstance(value, str):
+            return value.split('|')
+        elif isinstance(value, list):
+            return value
+        else:
+            raise ValueError("Unsupported value type: {}".format(type(value)))
