0c5b01059b532dcbe5bd8db640c66d0ca0eb35c1
mazur <mazursasha1990@gmail.com>
2024-03-06 20:44:23 +0200

    Add tests for RowFacilityTypeSerializer

---
+++ src/django/contricleaner/tests/test_row_facility_type_serializer.py
@@ -0,0 +1,59 @@
+from contricleaner.lib.serializers.row_serializers.\
+    row_facility_type_serializer import RowFacilityTypeSerializer
+
+from django.test import TestCase
+
+
+class RowFacilityTypeSerializerTest(TestCase):
+
+    def setUp(self):
+        self.serializer = RowFacilityTypeSerializer()
+
+    def test_validate_with_facility_type_processing_type(self):
+        row = {'facility_type_processing_type': 'Blending|Knitting'}
+        current = {}
+        validated = self.serializer.validate(row, current)
+        self.assertEqual(
+            validated,
+            {
+                'facility_type': ['Blending', 'Knitting'],
+                'processing_type': ['Blending', 'Knitting'],
+            },
+        )
+
+    def test_validate_with_processing_type_only(self):
+        row = {'processing_type': 'Knitting'}
+        current = {}
+        validated = self.serializer.validate(row, current)
+        self.assertEqual(
+            validated,
+            {'facility_type': ['Knitting'], 'processing_type': ['Knitting']},
+        )
+
+    def test_validate_with_facility_type_only(self):
+        row = {'facility_type': 'Blending'}
+        current = {}
+        validated = self.serializer.validate(row, current)
+        self.assertEqual(
+            validated,
+            {'facility_type': ['Blending'], 'processing_type': ['Blending']},
+        )
+
+    def test_validate_with_both_types_already_filled(self):
+        row = {'facility_type': 'Blending', 'processing_type': 'Knitting'}
+        current = {}
+        validated = self.serializer.validate(row, current)
+        self.assertEqual(
+            validated,
+            {'facility_type': ['Blending'], 'processing_type': ['Knitting']},
+        )
+
+    def test_split_values_with_string(self):
+        value = 'Blending|Knitting'
+        split_values = self.serializer.split_values(value)
+        self.assertEqual(split_values, ['Blending', 'Knitting'])
+
+    def test_split_values_with_list(self):
+        value = ['Blending', 'Knitting']
+        split_values = self.serializer.split_values(value)
+        self.assertEqual(split_values, ['Blending', 'Knitting'])
