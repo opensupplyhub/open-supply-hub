db3f7672333f28ba2b29c5639068911927b25911
mazur <mazursasha1990@gmail.com>
2024-03-08 17:50:17 +0200

    Add conditions how to create product_type field and cover these contions by tests

--- src/django/contricleaner/lib/serializers/row_serializers/row_sector_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_sector_serializer.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+from contricleaner.constants import DEFAULT_SECTOR_NAME, MAX_PRODUCT_TYPE_COUNT
 from contricleaner.lib.helpers.split_values import split_values
 from contricleaner.lib.sector_cache_interface import SectorCacheInterface
 from .row_serializer import RowSerializer
@@ -16,14 +18,19 @@ class RowSectorSerializer(RowSerializer):
 
         sectors, product_types = self.parse_all_values(values)
 
-        current['product_types'] = product_types
-        current['sectors'] = sectors
+        if len(product_types) > MAX_PRODUCT_TYPE_COUNT:
+            raise ValidationError(
+                f'You may submit a maximum of {MAX_PRODUCT_TYPE_COUNT} '
+                f'product types, not {len(product_types)}')
+
+        if product_types:
+            current['product_type'] = product_types
+
+        current['sector'] = sectors
 
         return current
 
     def parse_all_values(self, all_values):
-        DEFAULT_SECTOR_NAME = 'Unspecified'
-
         sector_map = self.sector_cache.sector_map
         sectors = []
         product_types = []
--- src/django/contricleaner/tests/test_row_sector_serializer.py
+++ src/django/contricleaner/tests/test_row_sector_serializer.py
@@ -1,3 +1,5 @@
+from contricleaner.constants import MAX_PRODUCT_TYPE_COUNT
+from django.core.exceptions import ValidationError
 from contricleaner.lib.serializers.row_serializers.row_sector_serializer \
     import RowSectorSerializer
 from contricleaner.tests.mockSectorCache import MockSectorCache
@@ -12,32 +14,38 @@ class RowSectorSerializerTest(TestCase):
         self.row_one = {
             "sector": ['Apparel', 'Finance'],
             "product_type": 'product one',
-            "sector_product_type":
-                [
-                    "Apparel, Finance",
-                    "Agriculture, product one, product two, product three",
-                    "Agriculture"
-                ]
-            }
+            "sector_product_type": [
+                "Apparel, Finance",
+                "Agriculture, product one, product two, product three",
+                "Agriculture",
+            ],
+        }
         self.row_two = {"sector": ['Apparel', 'product one']}
+        self.row_three = {"sector": 'Apparel'}
         self.current = {"errors": []}
 
     def test_validate_with_multiple_values(self):
         result = self.serializer.validate(self.row_one, self.current.copy())
 
         self.assertEqual(
-            result['product_types'],
-            ['product one', 'product three', 'product two']
+            result['product_type'],
+            ['product one', 'product three', 'product two'],
         )
         self.assertEqual(
-            result['sectors'], ['Agriculture', 'Apparel', 'Finance']
+            result['sector'], ['Agriculture', 'Apparel', 'Finance']
         )
 
     def test_validate_with_single_sector(self):
         result = self.serializer.validate(self.row_two, self.current.copy())
 
-        self.assertEqual(result['product_types'], ['product one'])
-        self.assertEqual(result['sectors'], ['Apparel'])
+        self.assertEqual(result['product_type'], ['product one'])
+        self.assertEqual(result['sector'], ['Apparel'])
+
+    def test_validate_without_product_type_values(self):
+        result = self.serializer.validate(self.row_three, self.current.copy())
+
+        self.assertEqual(result['sector'], ['Apparel'])
+        self.assertNotIn('product_type', result)
 
     def test_parse_all_values(self):
         all_values = ['technology', 'product one', 'finance']
@@ -55,3 +63,23 @@ class RowSectorSerializerTest(TestCase):
         value = '   Technology  '
         cleaned_value = self.serializer.clean_value(value)
         self.assertEqual(cleaned_value, 'technology')
+
+    def test_validate_max_product_types(self):
+        product_type_count = 60
+        product_type_list = [
+            'p{}'.format(i) for i in range(1, product_type_count + 1)
+        ]
+        row = {
+            "sector": ['Apparel'],
+            "product_type": product_type_list,
+        }
+
+        with self.assertRaises(ValidationError) as context:
+            self.serializer.validate(row, self.current.copy())
+
+        expected_error_message = (
+            f'You may submit a maximum of {MAX_PRODUCT_TYPE_COUNT} '
+            f'product types, not {product_type_count}'
+        )
+
+        self.assertEqual(context.exception.message, expected_error_message)
