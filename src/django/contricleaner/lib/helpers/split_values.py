01648af45b6d597a1ec64b42ca024ac8f30b5e13
mazursasha1990 <85229023+mazursasha1990@users.noreply.github.com>
2024-03-11 15:18:05 +0200

    [OSDEV-891] Implement facility_type_processing_type serializer in contricleaner (#75)
    Implement a new serializer in `contricleaner` that parses data from such
    fields: `facility_type_processing_type`, `facility_type`,
    `processing_type `
    
    [OSDEV-891](https://opensupplyhub.atlassian.net/browse/OSDEV-891)

---
+++ src/django/contricleaner/lib/helpers/split_values.py
@@ -0,0 +1,18 @@
+from typing import Union, Set
+
+
+def split_values(value: Union[str, list, set], split: str) -> Set[str]:
+    '''
+    Split a string, list, or set of strings into a set of strings.
+    If a list or set is passed, the function will recursively split
+    each value in the list or set.
+    '''
+    if isinstance(value, str):
+        return set(value.split(split))
+    elif isinstance(value, (list, set)):
+        res = set()
+        for v in value:
+            res = res.union(split_values(v, split))
+        return res
+    else:
+        raise ValueError("Unsupported value type: {}".format(type(value)))
