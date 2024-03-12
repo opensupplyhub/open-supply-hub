75a21f6afda3a0c7a3cefb232f8bcfe3159dea73
vladsha-dev <newshapovalov2002@gmail.com>
2024-03-12 13:28:57 +0100

    [OSDEV-893] Implement CSV and XLSX file parser strategies in the ContriCleaner library (#50)
    - Develop parsers for CSV and XLSX files, namely `SourceParserCSV` and
    `SourceParserXLSX`.
    - Incorporate preliminary cleanup during parsing.
    
    Quick update: The `server` script and the associated information in the
    README.md were updated due to changes made during the transition to the
    new repository.

--- src/django/contricleaner/lib/serializers/contri_cleaner_serializer.py
+++ src/django/contricleaner/lib/serializers/contri_cleaner_serializer.py
@@ -4,7 +4,7 @@ from contricleaner.lib.dto.row_dto import RowDTO
 from contricleaner.lib.sector_cache_interface import SectorCacheInterface
 from contricleaner.lib.serializers.row_serializers.row_composite_serializer \
     import RowCompositeSerializer
-from contricleaner.lib.source_parser import SourceParser
+from contricleaner.lib.parsers.source_parser import SourceParser
 
 
 class ContriCleanerSerializer:
@@ -17,6 +17,6 @@ class ContriCleanerSerializer:
         self.row_serializer = RowCompositeSerializer(sector_cache)
 
     def get_validated_rows(self) -> List[RowDTO]:
-        rows = self.__source_parser.parsed_rows()
+        rows = self.__source_parser.get_parsed_rows()
 
         return [self.row_serializer.get_validated_row(row) for row in rows]
