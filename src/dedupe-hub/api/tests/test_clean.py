6b634196d945be8211b8790c4f5131944d597a3f
roman-stolar <88504352+roman-stolar@users.noreply.github.com>
2024-03-15 14:01:21 +0200

    [OSDEV-911] Add code quality pipelines for dedupe-hub | Main Part (#90)
    [OSDEV-911](https://opensupplyhub.atlassian.net/browse/OSDEV-911) Add
    code quality pipelines for dedupe-hub | Main Part
    
    - Created tests config + added simple test
    - Updated code_quality pipeline

--- src/dedupe-hub/api/tests/test_clean.py
+++ src/dedupe-hub/api/tests/test_clean.py
@@ -5,7 +5,10 @@ from app.utils.clean import clean
 class TestClean(unittest.TestCase):
 
     def test_clean_column(self):
-        input = 'first-test:become,real'
+        input = "first-/test:become,'\nreal"
         output = clean(input)
 
-        self.assertEqual(output, 'firsttest becomereal')
+        self.assertEqual(output, "first test become real")
+
+if __name__ == '__main__':
+    unittest.main()
