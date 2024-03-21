820495cde62059a7f061780cda656cf9caaff0ca
Peter Ovchyn <peter@boarlabs.xyz>
2024-03-21 13:24:35 +0200

    Add restore and apply steps and pass arguments

--- src/anon-tools/do_restore.sh
+++ src/anon-tools/do_restore.sh
@@ -13,10 +13,10 @@ ls -la
 
 chmod 600 /keys/key
 
-echo "localhost:5433:opensupplyhub:opensupplyhub:" > ~/.pgpass
+echo "localhost:5433:$DATABASE_NAME:$DATABASE_USERNAME:$DATABASE_PASSWORD" > ~/.pgpass
 chmod 600 ~/.pgpass
 ssh-keyscan $bastion >> ~/.ssh/known_hosts
 
 ssh -f -i /keys/id_rsa -L 5433:database.service.osh.internal:5432 -N ec2-user@ec2-54-154-210-219.eu-west-1.compute.amazonaws.com
 
-pg_restore --verbose --clean --no-acl --no-owner -d opensupplyhub -U opensupplyhub -h localhost -p 5433 < /dumps/osh_prod_large.dump
+pg_restore --verbose --clean --no-acl --no-owner -d $DATABASE_NAME -U $DATABASE_USERNAME -h localhost -p 5433 < /dumps/osh_prod_large.dump
