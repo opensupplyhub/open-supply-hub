df36d564dfb33e1de1f0999d089a3e1b003d46c7
Peter Ovchyn <peter@boarlabs.xyz>
2024-03-21 13:38:59 +0200

    Call restore script

--- src/anon-tools/do_restore.sh
+++ src/anon-tools/do_restore.sh
@@ -17,6 +17,6 @@ echo "localhost:5433:$DATABASE_NAME:$DATABASE_USERNAME:$DATABASE_PASSWORD" > ~/.
 chmod 600 ~/.pgpass
 ssh-keyscan $bastion >> ~/.ssh/known_hosts
 
-ssh -f -i /keys/id_rsa -L 5433:database.service.osh.internal:5432 -N ec2-user@ec2-54-154-210-219.eu-west-1.compute.amazonaws.com
+ssh -f -i /keys/key -L 5433:database.service.osh.internal:5432 -N ec2-user@ec2-54-154-210-219.eu-west-1.compute.amazonaws.com
 
 pg_restore --verbose --clean --no-acl --no-owner -d $DATABASE_NAME -U $DATABASE_USERNAME -h localhost -p 5433 < /dumps/osh_prod_large.dump
