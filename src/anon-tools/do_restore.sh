46f5c15ccf2c7f2a92beae20412bc8999376e1c9
Peter Ovchyn <peter@boarlabs.xyz>
2024-03-21 12:36:08 +0200

    Add some stuff

--- src/anon-tools/do_restore.sh
+++ src/anon-tools/do_restore.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# ls -la /keys
-# chmod 600 /keys/id_rsa
-
-# echo "localhost:5433:opensupplyhub:opensupplyhub:" > ~/.pgpass
-# chmod 600 ~/.pgpass
-# ssh-keyscan ec2-54-154-210-219.eu-west-1.compute.amazonaws.com >> ~/.ssh/known_hosts
-
-# ssh -f -i /keys/id_rsa -L 5433:database.service.osh.internal:5432 -N ec2-user@ec2-54-154-210-219.eu-west-1.compute.amazonaws.com
 
 aws s3 cp s3://oshub-dumps-anonymized/osh_prod_large_anon.dump /dumps/osh_prod_large.dump
 
-aws ec2 describe-instances --filters "Name=tag:Environment,Values=Preprod" --query 'Reservations[*].Instances[*].[InstanceId]' --output text | xargs -I {} aws ec2 start-instances --instance-ids {}
+bastion = ="$(aws ec2 describe-instances --filters "Name=tag:Environment,Values=$ENVIRONMENT" --query 'Reservations[0].Instances[0].PublicDnsName' --output text)"
+
+echo "Bastion: $bastion"
+ls -la
+cd /keys
+ls -la
+
+
+chmod 600 /keys/key
+
+echo "localhost:5433:opensupplyhub:opensupplyhub:" > ~/.pgpass
+chmod 600 ~/.pgpass
+ssh-keyscan $bastion >> ~/.ssh/known_hosts
+
+ssh -f -i /keys/id_rsa -L 5433:database.service.osh.internal:5432 -N ec2-user@ec2-54-154-210-219.eu-west-1.compute.amazonaws.com
+
+pg_restore --verbose --clean --no-acl --no-owner -d opensupplyhub -U opensupplyhub -h localhost -p 5433 < /dumps/osh_prod_large.dump
