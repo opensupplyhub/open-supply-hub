75b6a1546472801cc6c52f655574aa9807a03894
Roman Stolar <roman.stolar@speedandfunction.com>
2024-03-27 09:53:46 +0200

    structure data add more info for each step

--- src/django/api/views/facility/facilities_view_set.py
+++ src/django/api/views/facility/facilities_view_set.py
@@ -577,7 +577,7 @@ class FacilitiesViewSet(ListModelMixin,
             raise PermissionDenied()
 
         log.info(f'[API Upload] Uploading data: {request.data}')
-        log.info('[API Upload] Started Parse process!')
+        log.info('[API Upload] Started CC Parse process!')
         split_pattern = r', |,|\|'
         contri_cleaner = ContriCleanerSerializer(
             SourceParserJSON(request.data), SectorCache(), split_pattern
@@ -585,7 +585,7 @@ class FacilitiesViewSet(ListModelMixin,
         rows = contri_cleaner.get_validated_rows()
         row = rows[0]
         if row.errors:
-            log.info(f'[API Upload] Parsing Errors: {row.errors}')
+            log.info(f'[API Upload] CC Parsing Errors: {row.errors}')
             return Response({
                 "message": "The provided data could not be parsed",
                 "errors": row.errors
@@ -639,10 +639,10 @@ class FacilitiesViewSet(ListModelMixin,
             }]
         )
 
-        log.info(f'[API Upload] Created Source Id: {source.id}')
-        log.info(f'[API Upload] Created Source Is Public: {source.is_public}')
-        log.info(f'[API Upload] Created Source Should Create: {source.create}')
-        log.info(f'[API Upload] Created FacilityListItem Id: {item.id}')
+        log.info(f'[API Upload] Source created. Id: {source.id}')
+        log.info(f'[API Upload] Source is public: {source.is_public}')
+        log.info(f'[API Upload] Source should create: {source.create}')
+        log.info(f'[API Upload] FacilityListItem created. Id: {item.id}')
 
         result = {
             'matches': [],
@@ -674,12 +674,14 @@ class FacilitiesViewSet(ListModelMixin,
             item.save()
             result['status'] = item.status
             result['message'] = error_message
-            log.info(f'[API Upload] Creation of EF error: {error_message}, FLI Id: {item.id}')
+            log.info(f'[API Upload] Creation of ExtendedField error: {error_message}')
+            log.info(f'[API Upload] FacilityListItem Id: {item.id}')
             return Response(result,
                             status=status.HTTP_400_BAD_REQUEST)
 
         geocode_started = str(timezone.now())
-        log.info(f'[API Upload] Started Geocode process, FLI Id {item.id}!')
+        log.info(f'[API Upload] Started Geocode process. FacilityListItem Id: {item.id}.')
+        log.info(f'[API Upload] Address: {row.address}, Country Code: {row.country_code}')
         try:
             geocode_result = geocode_address(row.address, row.country_code)
             if geocode_result['result_count'] > 0:
@@ -727,12 +729,16 @@ class FacilitiesViewSet(ListModelMixin,
             })
             item.save()
             result['status'] = item.status
-            log.info(f'[API Upload] Geocode Error: {str(exc)}, FLI Id {item.id}')
+            log.info(f'[API Upload] Geocode Error: {str(exc)}')
+            log.info(f'[API Upload] FacilityListItem Id: {item.id}')
+            log.info(f'[API Upload] Address: {row.address}, Country Code: {row.country_code}')
             return Response(result,
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         if item.status == FacilityListItem.GEOCODED:
-            log.info(f'[API Upload] Trying to start Match process! FLI Id: {item.id}')
+            log.info(f'[API Upload] Trying to start Match process!')
+            log.info(f'[API Upload] FacilityListItem Id: {item.id}')
+            log.info(f'[API Upload] Source Id: {item.id}')
             # Handle and produce message to Kafka with source_id data
             timer = 0
             timeout = 25
@@ -744,7 +750,10 @@ class FacilitiesViewSet(ListModelMixin,
                     source=source.id
                 )
                 if fli_temp.status == FacilityListItemTemp.GEOCODED:
-                    log.info(f'[API Upload] Started Match process! FLI Id" {item.id}, Source Id: {source.id}')
+                    log.info(f'[API Upload] Started Match process!')
+                    log.info(f'[API Upload] FacilityListItem Id: {item.id}')
+                    log.info(f'[API Upload] FacilityListItemTemp Id: {fli_temp.id}')
+                    log.info(f'[API Upload] Source Id: {item.id}')
                     asyncio.run(produce_message_match_process(source.id))
                     break
                 asyncio.sleep(1)
@@ -761,7 +770,10 @@ class FacilitiesViewSet(ListModelMixin,
         errors_status = [FacilityListItem.ERROR_MATCHING,
                          FacilityListItem.GEOCODED_NO_RESULTS]
         
-        log.info(f'[API Upload] Result data: {result} for FLI Id: {item.id}')
+        log.info(f'[API Upload] Result data: {result}')
+        log.info(f'[API Upload] FacilityListItem Id: {item.id}')
+        log.info(f'[API Upload] FacilityListItemTemp Id: {fli_temp.id}')
+        log.info(f'[API Upload] Source Id: {item.id}')
 
         if (should_create
                 and result['status'] not in errors_status):
