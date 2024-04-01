ba0f39937b2b6595797e78bdef0fba68f301c53e
Inessa Druzhkova <inessadruzkova@Inessas-MacBook-Pro.local>
2024-04-01 13:26:40 +0200

    joined two queries

--- src/django/api/reports/facility_lists_uploaded.sql
+++ src/django/api/reports/facility_lists_uploaded.sql
@@ -5,7 +5,8 @@
 -- Total Facilities w/ status = new facility
 -- # Public List Facilities w/ status = new facility
 -- Data is ordered from most recent to oldest
-SELECT
+
+select * from(SELECT
   to_char(l.created_at, 'YYYY-MM') AS month,
   COUNT(*) AS total_lists_uploaded,
   COUNT(u.email) filter (where u.email::text LIKE '%openapparel.org%'
@@ -16,20 +17,22 @@ JOIN api_contributor c ON s.contributor_id = c.id
 JOIN api_user u ON u.id = c.admin_id
 WHERE to_char(l.created_at, 'YYYY-MM-DD') != to_char(now(), 'YYYY-MM-DD')
 GROUP BY to_char(l.created_at, 'YYYY-MM')
-ORDER BY to_char(l.created_at, 'YYYY-MM') DESC;
-
---  SELECT
-  --  COUNT(*) AS total_facility_listitems_uploaded,
-  --  COUNT(u.email) filter (where u.email::text LIKE '%openapparel.org%'
-  --            OR u.email::text LIKE '%opensupplyhub.org%') as "Facilities From Public Lists",
-  --  COUNT(i.status) filter (where i.status::text ='PARSED') as "Total Facilities w/ status = new facility", -- UPLOADED
-  --  COUNT(i.status) filter (where i.status::text ='PARSED' and u.email::text LIKE '%openapparel.org%'
-  --            OR u.email::text LIKE '%opensupplyhub.org%') as "Public List Facilities w/ status = new facility" -- UPLOADED
-  --  FROM api_facilitylistitem i
-  --         JOIN api_source s on i.source_id = s.id
-  --         JOIN api_contributor c ON s.contributor_id = c.id
-  --         JOIN api_user u ON u.id = c.admin_id
-  -- WHERE to_char(i.created_at, 'YYYY-MM') != to_char(now(), 'YYYY-MM')
-  -- AND s.create = true
-  -- GROUP BY to_char(i.created_at, 'YYYY-MM')
-  -- ORDER BY to_char(i.created_at, 'YYYY-MM')
+ORDER BY to_char(l.created_at, 'YYYY-MM') desc) as query1
+ INNER JOIN (
+ SELECT
+   to_char(i.created_at, 'YYYY-MM') AS month,
+   COUNT(*) AS total_facility_listItems_uploaded,
+   COUNT(u.email) filter (where u.email::text LIKE '%openapparel.org%'
+             OR u.email::text LIKE '%opensupplyhub.org%') as "Facilities From Public Lists",
+   COUNT(i.status) filter (where i.status::text ='PARSED') as "Total Facilities w/ status = new facility",
+   COUNT(i.status) filter (where i.status::text ='PARSED' and u.email::text LIKE '%openapparel.org%'
+             OR u.email::text LIKE '%opensupplyhub.org%') as "Public List Facilities w/ status = new facility"
+   FROM api_facilitylistitem i
+          JOIN api_source s on i.source_id = s.id
+          JOIN api_contributor c ON s.contributor_id = c.id
+          JOIN api_user u ON u.id = c.admin_id
+  WHERE to_char(i.created_at, 'YYYY-MM-DD') != to_char(now(), 'YYYY-MM-DD')
+  AND s.create = true
+  GROUP BY to_char(i.created_at, 'YYYY-MM')
+  ORDER BY to_char(i.created_at, 'YYYY-MM') DESC
+  ) as query2 USING (month);
