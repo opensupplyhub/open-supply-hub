c6421db0e7ecb6cd9b860ca5c3509522876c39ea
mazur <mazursasha1990@gmail.com>
2024-04-12 10:03:34 +0300

    Format code

--- src/django/api/facility_actions/processing_facility.py
+++ src/django/api/facility_actions/processing_facility.py
@@ -13,12 +13,12 @@ from api.models.facility.facility_list_item_temp import FacilityListItemTemp
 from api.models.source import Source
 from api.processing import handle_external_match_process_result
 from api.views.fields.create_nonstandard_fields import (
-    create_nonstandard_fields,
+    create_nonstandard_fields
 )
 from oar.rollbar import report_error_to_rollbar
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from django.contrib.gis.geos import Point
 from django.core import exceptions as core_exceptions
@@ -272,13 +272,14 @@ class ProcessingFacility:
         header_row_keys = rows[0].raw_json.keys()
         header_str = ','.join(header_row_keys)
         new_list = FacilityList(
-                    name=name,
-                    description=description,
-                    file_name=uploaded_file.name,
-                    file=uploaded_file,
-                    header=header_str,
-                    replaces=replaces,
-                    match_responsibility=contributor.match_responsibility)
+            name=name,
+            description=description,
+            file_name=uploaded_file.name,
+            file=uploaded_file,
+            header=header_str,
+            replaces=replaces,
+            match_responsibility=contributor.match_responsibility,
+        )
         new_list.save()
         log.info(f'[List Upload] FacilityList created. Id {new_list.id}!')
 
@@ -287,7 +288,8 @@ class ProcessingFacility:
         source = Source.objects.create(
             contributor=contributor,
             source_type=Source.LIST,
-            facility_list=new_list)
+            facility_list=new_list,
+        )
         log.info(f'[List Upload] Source created. Id {source.id}!')
 
         is_geocoded = False
@@ -298,14 +300,15 @@ class ProcessingFacility:
             # and to provide an item for saving any errors that may exist
             # below.
             item = FacilityListItem.objects.create(
-                    row_index=idx,
-                    raw_data=','.join(
-                        f'"{value}"' for value in row.raw_json.values()),
-                    raw_json=row.raw_json,
-                    raw_header=header_str,
-                    sector=[],
-                    source=source
-                )
+                row_index=idx,
+                raw_data=','.join(
+                    f'"{value}"' for value in row.raw_json.values()
+                ),
+                raw_json=row.raw_json,
+                raw_header=header_str,
+                sector=[],
+                source=source,
+            )
             log.info(f'[List Upload] FacilityListItem created. Id {item.id}!')
 
             row_errors = row.errors
@@ -319,14 +322,16 @@ class ProcessingFacility:
                 )
                 log.info(f'[List Upload] FacilityListItem Id: {item.id}')
                 item.status = FacilityListItem.ERROR_PARSING
-                item.processing_results.append({
-                    'action': ProcessingAction.PARSE,
-                    'started_at': parsing_started,
-                    'error': True,
-                    'message': stringified_cc_err_messages,
-                    'trace': traceback.format_exc(),
-                    'finished_at': str(timezone.now()),
-                })
+                item.processing_results.append(
+                    {
+                        'action': ProcessingAction.PARSE,
+                        'started_at': parsing_started,
+                        'error': True,
+                        'message': stringified_cc_err_messages,
+                        'trace': traceback.format_exc(),
+                        'finished_at': str(timezone.now()),
+                    }
+                )
 
             if item.status != FacilityListItem.ERROR_PARSING:
                 item.sector = row.sector
@@ -336,8 +341,10 @@ class ProcessingFacility:
                 item.address = row.address
                 item.clean_address = row.clean_address
                 try:
-                    if (FileHeaderField.LAT in row.fields.keys()
-                            and FileHeaderField.LNG in row.fields.keys()):
+                    if (
+                        FileHeaderField.LAT in row.fields.keys()
+                        and FileHeaderField.LNG in row.fields.keys()
+                    ):
                         # TODO: Move floating to the ContriCleaner library.
                         lat = float(row.fields[FileHeaderField.LAT])
                         lng = float(row.fields[FileHeaderField.LNG])
@@ -350,34 +357,36 @@ class ProcessingFacility:
                         f'[List Upload] Creation of ExtendedField error: {e}'
                     )
                     report_error_to_rollbar(
-                        request=request,
-                        file=uploaded_file,
-                        exception=e
+                        request=request, file=uploaded_file, exception=e
                     )
                     log.info(f'[List Upload] FacilityListItem Id: {item.id}')
                     item.status = FacilityListItem.ERROR_PARSING
-                    item.processing_results.append({
-                        'action': ProcessingAction.PARSE,
-                        'started_at': parsing_started,
-                        'error': True,
-                        'message': str(e),
-                        'trace': traceback.format_exc(),
-                        'finished_at': str(timezone.now()),
-                    })
+                    item.processing_results.append(
+                        {
+                            'action': ProcessingAction.PARSE,
+                            'started_at': parsing_started,
+                            'error': True,
+                            'message': str(e),
+                            'trace': traceback.format_exc(),
+                            'finished_at': str(timezone.now()),
+                        }
+                    )
 
             if item.status != FacilityListItem.ERROR_PARSING:
                 item.status = FacilityListItem.PARSED
-                item.processing_results.append({
-                    'action': ProcessingAction.PARSE,
-                    'started_at': parsing_started,
-                    'error': False,
-                    'finished_at': str(timezone.now()),
-                    'is_geocoded': is_geocoded,
-                })
+                item.processing_results.append(
+                    {
+                        'action': ProcessingAction.PARSE,
+                        'started_at': parsing_started,
+                        'error': False,
+                        'finished_at': str(timezone.now()),
+                        'is_geocoded': is_geocoded,
+                    }
+                )
 
-                core_fields = '{}-{}-{}'.format(item.country_code,
-                                                item.clean_name,
-                                                item.clean_address)
+                core_fields = '{}-{}-{}'.format(
+                    item.country_code, item.clean_name, item.clean_address
+                )
                 if core_fields in parsed_items:
                     item.status = FacilityListItem.DUPLICATE
                 else:
