2aa5a2c88d3ec97fe7eef3a0dd11bcb384047c32
mazur <mazursasha1990@gmail.com>
2024-04-12 14:36:51 +0300

    Modify ProcessingFacilityExecutor

--- src/django/api/facility_actions/processing_facility_executor.py
+++ src/django/api/facility_actions/processing_facility_executor.py
@@ -2,20 +2,15 @@ from api.facility_actions.processing_facility import ProcessingFacility
 
 
 class ProcessingFacilityExecutor:
-    def __init__(self, strategy: ProcessingFacility) -> None:
-        """
-        Usually, the Context accepts a strategy through the constructor, but
-        also provides a setter to change it at runtime.
-        """
+    '''
+    Class to execute the processing of a facility.
+    It uses the strategy pattern to execute the processing.
+    '''
 
+    def __init__(self, strategy: ProcessingFacility) -> None:
         self._strategy = strategy
 
     def run_processing(self) -> None:
-        """
-        The Context delegates some work to the Strategy object instead of
-        implementing multiple versions of the algorithm on its own.
-        """
-
         result = self._strategy.process_facility()
 
         return result
