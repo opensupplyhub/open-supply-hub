e99ebfd228f43e103477f3dbf1e63eec26ac95fb
mazur <mazursasha1990@gmail.com>
2024-04-26 16:11:38 +0300

    Refactor TestExactItemMatch

--- src/dedupe-hub/api/tests/test_exact_item_match.py
+++ src/dedupe-hub/api/tests/test_exact_item_match.py
@@ -7,6 +7,11 @@ from app.database.models.facility_list_item_temp import FacilityListItemTemp
 class TestExactItemMatch(unittest.TestCase):
     def setUp(self):
         self.item_id = 1
+        self.started = "2024-04-25 12:00:00"
+        self.finished = "2024-04-25 12:10:00"
+        self.results = {}
+        self.automatic_threshold = 1.0
+
         self.matches_empty = []
         self.matches_single = [
             {"id": 2072, "facility_id": "CN20241096SFEBA", "score": "1"},
@@ -16,11 +21,8 @@ class TestExactItemMatch(unittest.TestCase):
             {"id": 2073, "facility_id": "CN20241096SFEBA", "score": "1"},
             {"id": 2074, "facility_id": "CN20241096SFEBA", "score": "1"},
         ]
-        self.started = "2024-04-25 12:00:00"
-        self.finished = "2024-04-25 12:10:00"
-        self.results = {}
-        self.automatic_threshold = 1.0
 
+        # Setup mock for database session in BaseItemMatch
         self.session_patcher = patch(
             'app.matching.matcher.base_item_match.get_session'
         )
@@ -28,100 +30,77 @@ class TestExactItemMatch(unittest.TestCase):
         self.mock_query = (
             self.mock_session.return_value.__enter__.return_value.query
         )
-        self.mock_item = MagicMock(spec=FacilityListItemTemp)
-        self.mock_item.id = 1
+        self.mock_item = MagicMock(
+            spec=FacilityListItemTemp,
+            id=self.item_id,
+        )
         # Setup the return values for query
         self.mock_query.return_value.get.return_value = self.mock_item
 
     def tearDown(self):
         self.session_patcher.stop()
 
-    def test_process_no_matches(self):
-        exact_match = ExactItemMatch(
+    def create_exact_match(self, matches):
+        return ExactItemMatch(
             self.item_id,
-            self.matches_empty,
-            self.started,
-            self.finished,
-            self.results,
-            self.automatic_threshold,
-        )
-        result = exact_match.process()
-        self.assertEqual(result, [])
-
-    @patch('app.matching.matcher.exact.exact_item_match.get_session')
-    def test_process_with_single_match(self, get_session_mock):
-        session_mock = MagicMock()
-        session_mock.query().filter().scalar.return_value = True
-        get_session_mock.return_value.__enter__.return_value = session_mock
-
-        exact_match = ExactItemMatch(
-            self.item_id,
-            self.matches_single,
+            matches,
             self.started,
             self.finished,
             self.results,
             self.automatic_threshold,
         )
 
-        result = exact_match.process()
-        expected_result = [
+    def expected_result_dict(self, match_type):
+        return [
             {
                 'facility_list_item_id': 1,
                 'facility_id': 'CN20241096SFEBA',
                 'status': 'AUTOMATIC',
-                'results': {'match_type': 'single_exact_match'},
+                'results': {'match_type': match_type},
                 'confidence': 1,
             }
         ]
+
+    def setup_session_mock(self, get_session_mock, return_value):
+        session_mock = MagicMock()
+        session_mock.query().filter().scalar.return_value = return_value
+        get_session_mock.return_value.__enter__.return_value = session_mock
+
+    def test_process_no_matches(self):
+        exact_match = self.create_exact_match(self.matches_empty)
+        result = exact_match.process()
+
+        self.assertEqual(result, [])
+
+    @patch('app.matching.matcher.exact.exact_item_match.get_session')
+    def test_process_with_single_match(self, get_session_mock):
+        self.setup_session_mock(get_session_mock, True)
+
+        exact_match = self.create_exact_match(self.matches_single)
+        result = exact_match.process()
+        expected_result = self.expected_result_dict('single_exact_match')
 
         self.assertEqual(result, expected_result)
         self.assertEqual(exact_match.item.status, FacilityListItemTemp.MATCHED)
 
     @patch('app.matching.matcher.exact.exact_item_match.get_session')
     def test_process_with_multiple_matches(self, get_session_mock):
-        session_mock = MagicMock()
-        session_mock.query().filter().scalar.return_value = True
-        get_session_mock.return_value.__enter__.return_value = session_mock
+        self.setup_session_mock(get_session_mock, True)
 
-        exact_match = ExactItemMatch(
-            self.item_id,
-            self.matches_multiple,
-            self.started,
-            self.finished,
-            self.results,
-            self.automatic_threshold,
-        )
-
+        exact_match = self.create_exact_match(self.matches_multiple)
         result = exact_match.process()
-        expected_result = [
-            {
-                'facility_list_item_id': 1,
-                'facility_id': 'CN20241096SFEBA',
-                'status': 'AUTOMATIC',
-                'results': {'match_type': 'multiple_exact_matches'},
-                'confidence': 1,
-            }
-        ]
+        expected_result = self.expected_result_dict('multiple_exact_matches')
 
         self.assertEqual(result, expected_result)
         self.assertEqual(exact_match.item.status, FacilityListItemTemp.MATCHED)
 
     @patch('app.matching.matcher.exact.exact_item_match.get_session')
     def test_process_without_session(self, get_session_mock):
-        session_mock = MagicMock()
-        session_mock.query().filter().scalar.return_value = False
-        get_session_mock.return_value.__enter__.return_value = session_mock
+        self.setup_session_mock(get_session_mock, False)
 
-        exact_match = ExactItemMatch(
-            self.item_id,
-            self.matches_multiple,
-            self.started,
-            self.finished,
-            self.results,
-            self.automatic_threshold,
-        )
-
+        exact_match = self.create_exact_match(self.matches_multiple)
         result = exact_match.process()
+
         self.assertEqual(result, [])
 
 
