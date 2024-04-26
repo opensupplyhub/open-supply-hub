7c7a164ed92bd9ba1c78b5e8a4fb15c5a79c686c
mazur <mazursasha1990@gmail.com>
2024-04-26 13:47:57 +0300

    Revert changes in the sqlalchemy.py

--- src/dedupe-hub/api/app/database/sqlalchemy.py
+++ src/dedupe-hub/api/app/database/sqlalchemy.py
@@ -5,7 +5,7 @@ from fastapi_utils.guid_type import setup_guids_postgresql
 from ..config import settings
 
 POSTGRES_URL = f"postgresql://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}"
-print('POSTGRES_URL', POSTGRES_URL)
+ 
 engine = create_engine(POSTGRES_URL, echo=True)
 setup_guids_postgresql(engine)         
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
