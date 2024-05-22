064810fbd2f1eca067acc983b4cc327c25c17d65
mazur <mazursasha1990@gmail.com>
2024-05-22 22:38:58 +0300

    Modify remove_facilitylistitems_with_potential_match_status_more_thirty_days transaction

--- src/django/api/models/transactions/remove_facilitylistitems_with_potential_match_status_more_thirty_days.py
+++ src/django/api/models/transactions/remove_facilitylistitems_with_potential_match_status_more_thirty_days.py
@@ -1,3 +1,4 @@
+import os
 from django.db import transaction, connection
 
 
@@ -5,8 +6,19 @@ from django.db import transaction, connection
 def remove_facilitylistitems_with_potential_match_status_more_thirty_days():
     with connection.cursor() as c:
         try:
+            print('Stripping all triggers...')
             c.execute("call strip_all_triggers();")
-            # c.execute("call remove_facilitylistitems_with_potential_match_status_more_thirty_days();")
+            print('All triggers stripped.')
+
+            print('Removing facilitylistitems where facility_id is null and related data...')
+            file_path = os.path.join('./sqls/', 'remove_facilitylistitems_where_facility_id_is_null_and_related_data.sql')
+            sql_statement = open(file_path).read()
+            with connection.cursor() as c:
+                c.execute(sql_statement)
+            print('Facilitylistitems where facility_id is null and related data removed.')
+            # c.callproc("remove_facilitylistitems_with_potential_match_status_more_thirty_days")
+            # c.execute("0135")
+
         except Exception as error:
             print(error)
         finally:
