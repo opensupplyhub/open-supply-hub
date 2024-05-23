ebde2f17fdb32d24a6e9e6423afac59b1a985fa9
mazur <mazursasha1990@gmail.com>
2024-05-23 14:00:01 +0300

    Add sql function remove_matches_with_pending_status_more_thirty_days

---
+++ src/django/sqls/remove_matches_with_pending_status_more_thirty_days.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION remove_matches_with_pending_status_more_thirty_days()
+RETURNS void AS $$
+BEGIN
+    -- Delete the matches that have status as PENDING and updated_at more than 30 days ago
+    DELETE FROM api_facilitymatch
+    WHERE api_facilitymatch.status='PENDING' 
+    AND api_facilitymatch.updated_at < (NOW() - interval '1 month');
+
+    DELETE FROM api_facilitymatchtemp 
+    WHERE api_facilitymatchtemp.status='PENDING'
+    AND api_facilitymatchtemp.updated_at < (NOW() - interval '1 month');
+    
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT remove_matches_with_pending_status_more_thirty_days();
