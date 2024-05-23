70a357f5eb84d1ae0c0aa4a1baf997e7ae73e0fa
mazur <mazursasha1990@gmail.com>
2024-05-23 15:59:28 +0300

    Modify migration 0145_new_functions_for_clean_facilitylistitems_command

--- src/django/api/migrations/0145_new_functions_for_clean_facilitylistitems_command.py
+++ src/django/api/migrations/0145_new_functions_for_clean_facilitylistitems_command.py
@@ -2,17 +2,62 @@ import os
 from django.db import connection
 from django.db import migrations
 
-
-def create_strip_all_triggers_procedure(apps, schema_editor):
-    file_path = os.path.join('./sqls/', 'strip_all_triggers.sql')
-    sql_statement = open(file_path).read()
-    with connection.cursor() as c:
-        c.execute(sql_statement)
+SQL_DIRECTORY = './sqls/'
 
 
-def drop_strip_all_triggers_procedure(apps, schema_editor):
-    with connection.cursor() as c:
-        c.execute('drop procedure strip_all_triggers;')
+def execute_sql_from_file(file_name):
+    file_path = os.path.join(SQL_DIRECTORY, file_name)
+    with open(file_path, 'r') as file:
+        sql_statement = file.read()
+    with connection.cursor() as cursor:
+        cursor.execute(sql_statement)
+
+
+def create_strip_all_triggers_proc(apps, schema_editor):
+    execute_sql_from_file('strip_all_triggers.sql')
+
+
+def drop_strip_all_triggers_proc(apps, schema_editor):
+    with connection.cursor() as cursor:
+        cursor.execute('DROP proc IF EXISTS strip_all_triggers;')
+
+
+def create_remove_items_where_facility_id_is_null_proc(apps, schema_editor):
+    execute_sql_from_file('remove_items_where_facility_id_is_null.sql')
+
+
+def drop_remove_items_where_facility_id_is_null_proc(apps, schema_editor):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            'DROP proc IF EXISTS remove_items_where_facility_id_is_null;'
+        )
+
+
+def create_remove_old_pending_matches_proc(apps, schema_editor):
+    execute_sql_from_file('remove_old_pending_matches.sql')
+
+
+def drop_remove_old_pending_matches_proc(apps, schema_editor):
+    with connection.cursor() as cursor:
+        cursor.execute('DROP proc IF EXISTS ' 'remove_old_pending_matches;')
+
+
+def create_remove_items_without_matches_and_related_facilities_proc(
+    apps, schema_editor
+):
+    execute_sql_from_file(
+        'remove_items_without_matches_and_related_facilities.sql'
+    )
+
+
+def drop_remove_items_without_matches_and_related_facilities_proc(
+    apps, schema_editor
+):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            'DROP proc IF EXISTS '
+            'remove_items_without_matches_and_related_facilities;'
+        )
 
 
 class Migration(migrations.Migration):
@@ -23,7 +68,19 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(
-            create_strip_all_triggers_procedure,
-            drop_strip_all_triggers_procedure
+            create_strip_all_triggers_proc,
+            drop_strip_all_triggers_proc,
+        ),
+        migrations.RunPython(
+            create_remove_items_where_facility_id_is_null_proc,
+            drop_remove_items_where_facility_id_is_null_proc,
+        ),
+        migrations.RunPython(
+            create_remove_old_pending_matches_proc,
+            drop_remove_old_pending_matches_proc,
+        ),
+        migrations.RunPython(
+            create_remove_items_without_matches_and_related_facilities_proc,
+            drop_remove_items_without_matches_and_related_facilities_proc,
         ),
     ]
