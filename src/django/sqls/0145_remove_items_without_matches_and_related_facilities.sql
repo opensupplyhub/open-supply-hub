d68262d0f4b83601511915fec783f47625859087
mazur <mazursasha1990@gmail.com>
2024-05-27 13:08:47 +0300

    Use variables and remove operations with temm tables in remove_items_without_matches_and_related_facilities

--- src/django/sqls/0145_remove_items_without_matches_and_related_facilities.sql
+++ src/django/sqls/0145_remove_items_without_matches_and_related_facilities.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE PROCEDURE remove_items_without_matches_and_related_facilities()
 LANGUAGE plpgsql
 AS $$
+DECLARE
+    item_ids INTEGER[];
 BEGIN
-    -- Create temporary tables to store the IDs of the items that do not have matches and are not have facilities created from them
-    CREATE TEMPORARY TABLE temporary_facility_list_item_ids AS
-    SELECT id 
+    SELECT array_agg(id) INTO item_ids
     FROM api_facilitylistitem 
-        WHERE NOT EXISTS (
+    WHERE NOT EXISTS (
         SELECT 1
         FROM api_facilitymatch
         WHERE api_facilitymatch.facility_list_item_id = api_facilitylistitem.id
@@ -17,43 +17,20 @@ BEGIN
         WHERE api_facility.created_from_id = api_facilitylistitem.id
     );
 
-    CREATE TEMPORARY TABLE temporary_facility_list_item_temp_ids AS
-    SELECT id
-    FROM api_facilitylistitemtemp 
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM api_facilitymatchtemp
-        WHERE api_facilitymatchtemp.facility_list_item_id = api_facilitylistitemtemp.id
-    )
-    AND NOT EXISTS (
-        SELECT 1
-        FROM api_facility
-        WHERE api_facility.created_from_id = api_facilitylistitemtemp.id
-    );
-
-    -- Perform deletions using the temporary collected IDs table
+    -- Perform deletions using the stored arrays of IDs
     DELETE FROM api_facilitylistitemfield
-    WHERE facility_list_item_id IN (SELECT id FROM temporary_facility_list_item_ids);
+    WHERE facility_list_item_id = ANY(item_ids);
 
     DELETE FROM api_extendedfield
-    WHERE facility_list_item_id IN (SELECT id FROM temporary_facility_list_item_ids);
+    WHERE facility_list_item_id = ANY(item_ids);
 
     DELETE FROM api_historicalfacilitymatch
-    WHERE facility_list_item_id IN (SELECT id FROM temporary_facility_list_item_ids);
+    WHERE facility_list_item_id = ANY(item_ids);
 
     DELETE FROM api_historicalextendedfield
-    WHERE facility_list_item_id IN (SELECT id FROM temporary_facility_list_item_ids);
-
-    DELETE FROM api_historicalfacilitymatchtemp
-    WHERE facility_list_item_id IN (SELECT id FROM temporary_facility_list_item_temp_ids);
+    WHERE facility_list_item_id = ANY(item_ids);
 
     -- Delete the items that do not have matches and are not have facilities created from them
-    DELETE FROM api_facilitylistitem WHERE id IN (SELECT id FROM temporary_facility_list_item_ids);
-    DELETE FROM api_facilitylistitemtemp WHERE id IN (SELECT id FROM temporary_facility_list_item_temp_ids);
-
-    -- Drop the temporary tables
-    DROP TABLE IF EXISTS temporary_facility_list_item_ids;
-    DROP TABLE IF EXISTS temporary_facility_list_item_temp_ids;
-
+    DELETE FROM api_facilitylistitem WHERE id = ANY(item_ids);
 END;
 $$;
