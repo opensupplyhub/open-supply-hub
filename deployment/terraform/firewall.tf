0c3e34dc45014f82fa5f76eff796782c476a7910
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2024-05-28 09:08:37 +0400

    Introduce egress and ingress rules for opensearch

--- deployment/terraform/firewall.tf
+++ deployment/terraform/firewall.tf
@@ -405,3 +405,23 @@ resource "aws_security_group_rule" "batch_msk_egress" {
   security_group_id        = aws_security_group.batch.id
   source_security_group_id = aws_security_group.msk.id
 }
+
+resource "aws_security_group_rule" "opensearch_egress" {
+  type      = "egress"
+  from_port = 9000
+  to_port   = 9000
+  protocol  = "tcp"
+
+  security_group_id        = module.database_enc.database_security_group_id
+  source_security_group_id = aws_security_group.opensearch.id
+}
+
+resource "aws_security_group_rule" "opensearch_ingress" {
+  type      = "ingress"
+  from_port = 9000
+  to_port   = 9000
+  protocol  = "tcp"
+
+  security_group_id        = module.database_enc.database_security_group_id
+  source_security_group_id = aws_security_group.opensearch.id
+}
