c2d265e09667bded633e7e98175c2745cab78cf2
vladsha-dev <newshapovalov2002@gmail.com>
2024-06-03 12:45:34 +0200

    Remove extra space

--- src/anon-tools/do_restore.sh
+++ src/anon-tools/do_restore.sh
@@ -27,4 +27,4 @@ END \$\$;"
 
 echo "Dropping tables"
 psql -d $DATABASE_NAME -U $DATABASE_USERNAME -h localhost -p 5433 -c "$SQL_SCRIPT"
-pg_restore --verbose --clean --no-acl --no-owner -d $DATABASE_NAME -U $DATABASE_USERNAME -h localhost -p 5433 < /dumps/osh_prod_large.dump  || true
+pg_restore --verbose --clean --no-acl --no-owner -d $DATABASE_NAME -U $DATABASE_USERNAME -h localhost -p 5433 < /dumps/osh_prod_large.dump || true
