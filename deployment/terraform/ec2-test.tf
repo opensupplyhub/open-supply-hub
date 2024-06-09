402617b7017b3a49fe69519eebe430210f50fa96
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2024-06-09 14:37:35 +0400

    Add test ec2 instance to private subnet

---
+++ deployment/terraform/ec2-test.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "ec2_security_group" {
+  vpc_id = module.vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-test-security-group"
+  }
+}
+
+resource "aws_security_group_rule" "allow_ec2_to_opensearch" {
+  type                     = "ingress"
+  from_port                = 9200
+  to_port                  = 9200
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.opensearch.id
+  source_security_group_id = aws_security_group.ec2_security_group.id
+}
+
+resource "aws_instance" "example_server" {
+  count           = length(module.vpc.private_subnet_ids)
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+  subnet_id = module.vpc.private_subnet_ids[count.index]
+
+  security_groups = [
+    aws_security_group.ec2_security_group.name,
+  ]
+
+  tags = {
+    Name = "EC2Test"
+  }
+}
