d82f4bebbb722ac120ffc1b241745fd07ca8b14f
Volodymyr Vorona <volodymyr.vorona87@gmail.com>
2024-06-20 22:02:02 +0300

    Run prettier

--- src/react/src/__tests__/components/FacilityDetailsClaimFlag.test.js
+++ src/react/src/__tests__/components/FacilityDetailsClaimFlag.test.js
@@ -1,46 +1,57 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render, screen } from "@testing-library/react";
-import FacilityDetailsClaimFlag from "../../components/FacilityDetailsClaimFlag"
+import { render, screen } from '@testing-library/react';
+import FacilityDetailsClaimFlag from '../../components/FacilityDetailsClaimFlag';
 
 describe('FacilityDetailsClaimFlag', () => {
     test('it renders without rashing', () => {
-        const osId = "id_221kjaksiie";
+        const osId = 'id_221kjaksiie';
         const isClaimed = true;
         const isPending = false;
         const isEmbed = false;
 
         render(
             <Router>
-                <FacilityDetailsClaimFlag osId={osId} isClaimed={isClaimed} isPending={isPending} isEmbed={isEmbed} />
-            </Router>
-        )
+                <FacilityDetailsClaimFlag
+                    osId={osId}
+                    isClaimed={isClaimed}
+                    isPending={isPending}
+                    isEmbed={isEmbed}
+                />
+            </Router>,
+        );
 
-        const banner = screen.getByTestId("cliam-banner")
+        const banner = screen.getByTestId('cliam-banner');
 
-        expect(banner).toBeInTheDocument()
-    })
+        expect(banner).toBeInTheDocument();
+    });
 
     test('facility has been claimed', () => {
-        const osId = "id_221kjaksiie";
+        const osId = 'id_221kjaksiie';
         const isClaimed = true;
         const isPending = false;
         const isEmbed = false;
-        const text = 'This production location has been claimed by an owner or manager';
+        const text =
+            'This production location has been claimed by an owner or manager';
 
         render(
             <Router>
-                <FacilityDetailsClaimFlag osId={osId} isClaimed={isClaimed} isPending={isPending} isEmbed={isEmbed} />
-            </Router>
-        )
+                <FacilityDetailsClaimFlag
+                    osId={osId}
+                    isClaimed={isClaimed}
+                    isPending={isPending}
+                    isEmbed={isEmbed}
+                />
+            </Router>,
+        );
 
-        const banner = screen.getByTestId("cliam-banner")
+        const banner = screen.getByTestId('cliam-banner');
 
-        expect(banner).toHaveTextContent(text)
-    })
+        expect(banner).toHaveTextContent(text);
+    });
 
     test('facility has not been claimed', () => {
-        const osId = "id_221kjaksiie";
+        const osId = 'id_221kjaksiie';
         const isClaimed = false;
         const isPending = false;
         const isEmbed = false;
@@ -48,17 +59,22 @@ describe('FacilityDetailsClaimFlag', () => {
 
         render(
             <Router>
-                <FacilityDetailsClaimFlag osId={osId} isClaimed={isClaimed} isPending={isPending} isEmbed={isEmbed} />
-            </Router>
-        )
+                <FacilityDetailsClaimFlag
+                    osId={osId}
+                    isClaimed={isClaimed}
+                    isPending={isPending}
+                    isEmbed={isEmbed}
+                />
+            </Router>,
+        );
 
-        const banner = screen.getByTestId("cliam-banner")
+        const banner = screen.getByTestId('cliam-banner');
 
-        expect(banner).toHaveTextContent(text)
-    })
+        expect(banner).toHaveTextContent(text);
+    });
 
     test('pending claim for facility ', () => {
-        const osId = "id_221kjaksiie";
+        const osId = 'id_221kjaksiie';
         const isClaimed = false;
         const isPending = true;
         const isEmbed = false;
@@ -66,31 +82,39 @@ describe('FacilityDetailsClaimFlag', () => {
 
         render(
             <Router>
-                <FacilityDetailsClaimFlag osId={osId} isClaimed={isClaimed} isPending={isPending} isEmbed={isEmbed} />
-            </Router>
-        )
+                <FacilityDetailsClaimFlag
+                    osId={osId}
+                    isClaimed={isClaimed}
+                    isPending={isPending}
+                    isEmbed={isEmbed}
+                />
+            </Router>,
+        );
 
-        const banner = screen.getByTestId("cliam-banner")
+        const banner = screen.getByTestId('cliam-banner');
 
-        expect(banner).toHaveTextContent(text)
-    })
+        expect(banner).toHaveTextContent(text);
+    });
 
     test('facility claim shoud not be rendered', () => {
-        const osId = "id_221kjaksiie";
+        const osId = 'id_221kjaksiie';
         const isClaimed = false;
         const isPending = true;
         const isEmbed = true;
 
         render(
             <Router>
-                <FacilityDetailsClaimFlag osId={osId} isClaimed={isClaimed} isPending={isPending} isEmbed={isEmbed} />
-            </Router>
-        )
+                <FacilityDetailsClaimFlag
+                    osId={osId}
+                    isClaimed={isClaimed}
+                    isPending={isPending}
+                    isEmbed={isEmbed}
+                />
+            </Router>,
+        );
 
-        const banner = screen.queryByTestId("cliam-banner")
+        const banner = screen.queryByTestId('cliam-banner');
 
-        expect(banner).not.toBeInTheDocument()
-    })
-
-
-})
+        expect(banner).not.toBeInTheDocument();
+    });
+});
