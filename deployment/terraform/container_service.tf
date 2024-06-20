faf17e1dacbbe3fa8f6ab3d7226a69b283a5472f
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2024-06-20 13:30:39 +0400

    Refactor count index in app_logstash template_file

--- deployment/terraform/container_service.tf
+++ deployment/terraform/container_service.tf
@@ -380,8 +380,7 @@ data "template_file" "app_logstash" {
     image                            = local.app_logstash_image
     log_group_name                   = "log${local.short}AppLogstash"
     aws_region                       = var.aws_region
-    # TODO: Once count in openseach.tf be removed, remove [count.index] as well.
-    opensearch_endpoint              = "${aws_opensearch_domain.opensearch[count.index].endpoint}:${var.opensearch_port}"
+    opensearch_endpoint              = "${aws_opensearch_domain.opensearch.endpoint}:${var.opensearch_port}"
     postgres_host                    = aws_route53_record.database.name
     postgres_port                    = module.database_enc.port
     postgres_user                    = var.rds_database_username
