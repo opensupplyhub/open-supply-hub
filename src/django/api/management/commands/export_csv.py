73dd17fedddd5ed056c7574a7a2f65997eb8ea8d
Roman Stolar <roman.stolar@speedandfunction.com>
2024-06-21 13:12:20 +0300

    refactor & improved perfomance

--- src/django/api/management/commands/export_csv.py
+++ src/django/api/management/commands/export_csv.py
@@ -9,6 +9,8 @@ from api.serializers.facility.facility_download_serializer import (
 )
 from api.models.facility.facility_index import FacilityIndex
 
+serializer = FacilityDownloadSerializer()
+
 
 def count_with_percent(i: int, count: int) -> str:
     return f"{i} out of {count} ({i / count * 100})%"
@@ -18,8 +20,28 @@ def print_with_time(text: str) -> None:
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- {text}")
 
 
-def create_chunk(list, size, last_id):
-    return list[last_id:last_id + size]
+def create_chunk(list, size, index):
+    return list[index:index + size]
+
+def create_with_headers():
+    now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f"./facilities-command-{now}.csv"
+    with open(filename, 'w+') as f:
+        writer = csv.writer(f)
+        print_with_time("Created file")
+
+        headers = serializer.get_headers()
+        writer.writerow(headers)
+        print_with_time("Written headers")
+    
+    return filename
+
+def modify(filename, chunk):
+    with open(filename, 'a') as f:
+        writer = csv.writer(f)
+        for facility in chunk:
+            row = serializer.get_row(facility)
+            writer.writerow(row)
 
 
 class Command(BaseCommand):
@@ -30,46 +52,31 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         facilities = FacilityIndex.objects.order_by("id").all()
+        size_of_chunk = 100000
         count = facilities.count()
         print_with_time(f"Amount of indexed facilities: {count}")
 
-        serializer = FacilityDownloadSerializer()
+        filename = create_with_headers()
 
-        now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        with open(f"./facilities-command-{now}.csv", 'w+') as f:
-            writer = csv.writer(f)
-            print_with_time("Opened file")
-
-            headers = serializer.get_headers()
-            writer.writerow(headers)
-
-        print_with_time("Written headers")
-
+        index = 0
         print_with_time("Fetching data from DB")
+        while count > index:
+            if index == 0:
+                print_with_time(
+                    f"Started exporting {count} facilities"
+                )
 
-        last_id = 0
-        size_of_chunk = 1000
-        while last_id != count - 1:
-            facility_chunk = create_chunk(facilities, size_of_chunk, last_id)
-            last_id = len(facility_chunk) - 1
-            with open(f"./facilities-command-{now}.csv", 'a') as f:
-                writer = csv.writer(f)
-                i = 0
-                for facility in facility_chunk:
-                    if i == 0:
-                        print_with_time(
-                            f"Started exporting {count} facilities"
-                        )
-                    i += 1
+            facility_chunk = create_chunk(facilities, size_of_chunk, index)
+            index = index + size_of_chunk
+            modify(filename, facility_chunk)
 
-                    row = serializer.get_row(facility)
-                    writer.writerow(row)
-
-                    if i % 100 == 0:
-                        print_with_time(
-                            f"Wrote {count_with_percent(i, count)}"
-                        )
+            if index > count:
+                index = count
+            if index % size_of_chunk == 0:
+                print_with_time(
+                    f"Wrote {count_with_percent(index, count)}"
+                )
 
         print_with_time(
-            f"Finished {count_with_percent(i, count)}"
+            f"Finished {count_with_percent(index,count)}"
         )
