1ce0ecfefab96bf8ac7a65e4b6b50c88bd8ea9c8
Inna Kocherzhuk <innakocherzhuk@gmail.com>
2024-07-10 08:53:52 +0200

    [OSDEV-981] History of contributor uploads. Fix division by zero (#281)
    [OSDEV-981](https://opensupplyhub.atlassian.net/browse/OSDEV-981)
    Reporting. History of contributor uploads.
    
    Fixed division by zero.
    
    [OSDEV-981]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-981?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: Inessa Druzhkova <inessadruzkova@inessas-mbp.home>
    Co-authored-by: Oleksandr Mazur <85229023+mazursasha1990@users.noreply.github.com>

--- src/django/api/reports/history_of_contributor_uploads.sql
+++ src/django/api/reports/history_of_contributor_uploads.sql
@@ -7,7 +7,15 @@ SELECT
   COALESCE(TO_CHAR(MAX(l.created_at), 'Month DD, YYYY'),TO_CHAR(MAX(s.created_at), 'Month DD, YYYY')) AS "Date of Last Upload",
   COALESCE(CURRENT_DATE - MAX(l.created_at)::date,CURRENT_DATE - MAX(s.created_at)::date) AS "Time Since Last Upload (Days)",
   COUNT(*) AS "Lifetime Uploads",
-  COALESCE(ROUND(COUNT(*) / ROUND((CURRENT_DATE - MIN(l.created_at)::date) / 365.25, 2), 2),ROUND(COUNT(*) / ROUND((CURRENT_DATE - MIN(s.created_at)::date) / 365.25, 2), 2)) AS "Uploads per Year"
+  COALESCE(
+    ROUND(
+      CASE
+        WHEN (CURRENT_DATE - COALESCE(MIN(l.created_at), MIN(s.created_at))::date) / 365.25 = 0
+        THEN COUNT(*) / (1 / 365.25)
+        ELSE COUNT(*) / ((CURRENT_DATE - COALESCE(MIN(l.created_at), MIN(s.created_at))::date) / 365.25)
+      END,
+    2), 0
+  ) AS "Uploads per Year"
 FROM api_contributor ac
 LEFT JOIN api_source s ON s.contributor_id = ac.id
 LEFT JOIN api_facilitylist l ON l.id = s.facility_list_id
