d9c3733a8743afb3acdcceb8719e63a862f93b73
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2024-07-17 09:47:15 +0400

    Refactor sorting for name field

--- src/django/api/views/v1/production_locations/production_locations.py
+++ src/django/api/views/v1/production_locations/production_locations.py
@@ -7,21 +7,6 @@ def production_locations_view(request):
     size = request.query_params.get('size', 10)
     start_after = request.query_params.get('start_after', None)
     query = request.query_params.get('query', None)
-    name = request.query_params.get('name', None)
-    name_local = request.query_params.get('name_local', None)
-    description = request.query_params.get('description', None)
-    address = request.query_params.get('address', None)
-    sector = request.query_params.getlist('sector', None)
-    product_type = request.query_params.getlist('product_type', None)
-    location_type = request.query_params.getlist('location_type', None)
-    number_of_workers = request.query_params.get('number_of_workers', None)
-    coordinates = request.query_params.get('coordinates', None)
-    minimum_order_quantity = request.query_params.get('minimum_order_quantity', None)
-    average_lead_time = request.query_params.get('average_lead_time', None)
-    percent_female_workers = request.query_params.get('percent_female_workers', None)
-    affiliations = request.query_params.get('affiliations', None)
-    certifications = request.query_params.getlist('certifications_standards_regulations', None)
-    country = request.query_params.get('country', None)
     sort_by = request.query_params.get('sort_by', 'name')
     order_by = request.query_params.get('order_by', 'asc')
 
@@ -33,7 +18,8 @@ def production_locations_view(request):
             }
         },
         'sort': [
-            {sort_by: {'order': order_by}}
+            # TODO: using sorting for keyword search will increase response time, see https://forum.opensearch.org/t/issue-with-message-field-data/13420
+            {f"{sort_by}.keyword": {'order': order_by}}
         ]
     }
 
@@ -44,36 +30,32 @@ def production_locations_view(request):
                 'fields': ['name^2', 'address', 'description', 'name_local']
             }
         })
-    if name:
-        query_body['query']['bool']['must'].append({'match': {'name': name}})
-    if name_local:
-        query_body['query']['bool']['must'].append({'match': {'name_local': name_local}})
-    if description:
-        query_body['query']['bool']['must'].append({'match': {'description': description}})
-    if address:
-        query_body['query']['bool']['must'].append({'match': {'address': address}})
-    if sector:
-        query_body['query']['bool']['must'].append({'terms': {'sector': sector}})
-    if product_type:
-        query_body['query']['bool']['must'].append({'terms': {'product_type': product_type}})
-    if location_type:
-        query_body['query']['bool']['must'].append({'terms': {'location_type': location_type}})
-    if number_of_workers:
-        query_body['query']['bool']['must'].append({'range': {'number_of_workers': number_of_workers}})
-    if coordinates:
-        query_body['query']['bool']['must'].append({'geo_distance': {'distance': '100km', 'location': coordinates}})
-    if minimum_order_quantity:
-        query_body['query']['bool']['must'].append({'match': {'minimum_order_quantity': minimum_order_quantity}})
-    if average_lead_time:
-        query_body['query']['bool']['must'].append({'match': {'average_lead_time': average_lead_time}})
-    if percent_female_workers:
-        query_body['query']['bool']['must'].append({'match': {'percent_female_workers': percent_female_workers}})
-    if affiliations:
-        query_body['query']['bool']['must'].append({'match': {'affiliations': affiliations}})
-    if certifications:
-        query_body['query']['bool']['must'].append({'terms': {'certifications_standards_regulations': certifications}})
-    if country:
-        query_body['query']['bool']['must'].append({'match': {'country': country}})
+
+    field_queries = {
+        'name': 'match',
+        'name_local': 'match',
+        'description': 'match',
+        'address': 'match',
+        'sector': 'terms',
+        'product_type': 'terms',
+        'location_type': 'terms',
+        'number_of_workers': 'range',
+        'coordinates': 'geo_distance',
+        'minimum_order_quantity': 'match',
+        'average_lead_time': 'match',
+        'percent_female_workers': 'match',
+        'affiliations': 'match',
+        'certifications_standards_regulations': 'terms',
+        'country': 'match',
+    }
+
+    for field, query_type in field_queries.items():
+        value = request.query_params.getlist(field) if query_type == 'terms' else request.query_params.get(field)
+        if value:
+            if query_type == 'geo_distance':
+                query_body['query']['bool']['must'].append({query_type: {'distance': '100km', 'location': value}})
+            else:
+                query_body['query']['bool']['must'].append({query_type: {field: value}})
 
     if start_after:
         query_body['search_after'] = [start_after]
