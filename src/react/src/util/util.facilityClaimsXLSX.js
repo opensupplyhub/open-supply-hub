9c3dc36f72f9c81cb2e1ac50ea35286407697cbd
Roman Stolar <roman.stolar@speedandfunction.com>
2024-07-19 11:26:31 +0300

    addressed comments

--- src/react/src/util/util.facilityClaimsXLSX.js
+++ src/react/src/util/util.facilityClaimsXLSX.js
@@ -1,5 +1,7 @@
 import moment from 'moment';
 
+import { CLAIM_DECISION_EMPTY } from '../util/constants'
+
 const xlsxHeaders = Object.freeze([
     'Claim ID',
     'Facility Name',
@@ -21,7 +23,7 @@ const formatFacilityClaimsDataForXLSX = facilityClaims =>
             moment(facilityClaim.created_at).format('LL'),
             facilityClaim.claim_decision !== null
                 ? moment(facilityClaim.claim_decision).format('LL')
-                : 'N/A',
+                : CLAIM_DECISION_EMPTY,
             facilityClaim.status,
             moment(facilityClaim.updated_at).format('LL'),
         ]),
