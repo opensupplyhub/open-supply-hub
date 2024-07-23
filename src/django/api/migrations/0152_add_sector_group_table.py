4b291f83edf8777f9dea56d64dbf09de78a3371a
Oleksandr Mazur <85229023+mazursasha1990@users.noreply.github.com>
2024-07-23 08:38:15 +0300

    [OSDEV-360] Facility Search. Make it easier to search for groups of related sectors (BE) (#285)
    [OSDEV-360](https://opensupplyhub.atlassian.net/browse/OSDEV-360) -
    Facility Search. Make it easier to search for groups of related sectors
    (BE)
    
     The following changes have been implemented:
    
    - A new table, `api_sectorgroup`, has been introduced and populated with
    sector group names.
    - A new field named `groups` has been added to the `Sector` model to
    establish a many-to-many relationship between the `Sector` model and the
    `SectorGroup` model.
    - `SectorGroup` was added to the admin page.
    
    
    
    
    [OSDEV-360]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-360?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ src/django/api/migrations/0152_add_sector_group_table.py
@@ -0,0 +1,70 @@
+from django.db import migrations, models
+
+from api.models.sector_group import SectorGroup
+
+
+def populate_sector_groups(apps, schema_editor):
+    groups_names = [
+        "Apparel, Footwear, Textiles",
+        "Agriculture, Food & Beverage",
+        "Technology",
+        "Automotive",
+        "Electronics",
+        "Energy",
+        "Utilities",
+        "Mining",
+        "Construction",
+        "Cosmetics",
+        "Durable Goods",
+        "Transportation",
+        "Aerospace",
+        "Forestry",
+        "Press",
+        "Health",
+        "Entertainment",
+        "Accommodation",
+        "Governmental",
+        "Finance",
+        "General Merchandise",
+        "Storage",
+        "Sporting Goods",
+        "Education, Research, Services",
+        "Other",
+    ]
+
+    for name in groups_names:
+        SectorGroup.objects.create(name=name)
+
+
+class Migration(migrations.Migration):
+    '''
+    This migration creates the SectorGroup model and populates it with
+    the sector groups names.
+    '''
+
+    initial = True
+
+    dependencies = [
+        ('api', '0151_replace_index_number_of_workers'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SectorGroup',
+            fields=[
+                (
+                    'id',
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    ),
+                ),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.RunPython(
+            populate_sector_groups, migrations.RunPython.noop
+        ),
+    ]
