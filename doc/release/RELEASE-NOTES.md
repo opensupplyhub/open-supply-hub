8cb00eeb0e28bc70de35230d24b70f0b9d4375d5
Vadim Kovalenko <90321493+VadimKovalenkoSNF@users.noreply.github.com>
2024-08-07 11:51:20 +0400

    [OSDEV-1006] Create new "production-locations" endpoint (#286)
    Fix [OSDEV-1006](https://opensupplyhub.atlassian.net/browse/OSDEV-1006)
    Boilerplates for production-locations endpoint.
    
    [OSDEV-1006]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-1006?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    1. Introduce OpenSeachConnection class as Singleton to handle connetion
    to the OpenSearch.
    2. Introduce OpenSearchService that implements search methods.
    3. Introduce SearchInterface that defines abstarct search methods that
    implemented by OpenSearchService.
    4. Introduce OpenSearchQueryBuilder as a part of Builder pattern
    implementation.
    5. Introduce ProductionLocationsSerializer using Strategy pattern.
    6. Handle client and server errors.
    7. Move index name to list of reusable constants.
    8. Add unit tests for v1 utils and OpenSearch query builder.
    ---------
    
    Co-authored-by: vladsha-dev <newshapovalov2002@gmail.com>

--- doc/release/RELEASE-NOTES.md
+++ doc/release/RELEASE-NOTES.md
@@ -4,6 +4,35 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html). The format is based on the `RELEASE-NOTES-TEMPLATE.md` file.
 
 
+## Release 1.19.0
+
+## Introduction
+* Product name: Open Supply Hub
+* Release date: August 24, 2024
+
+### Database changes
+#### Migrations:
+* *Describe migrations here.*
+
+#### Scheme changes
+* *Describe scheme changes here.*
+
+### Code/API changes
+* [OSDEV-1006](https://opensupplyhub.atlassian.net/browse/OSDEV-1006) - Create new "api/v1/production-locations" endpoint.
+
+### Architecture/Environment changes
+* *Describe architecture/environment changes here.*
+
+### Bugfix
+* *Describe bugfix here.*
+
+### What's new
+* *Describe what's new here. The changes that can impact user experience should be listed in this section.*
+
+### Release instructions:
+* *Provide release instructions here.*
+
+
 ## Release 1.18.0
 
 ## Introduction
---
+++ src/django/api/services/search.py
@@ -0,0 +1,32 @@
+import logging
+from api.services.opensearch import OpenSearchServiceConnection
+from api.services.search_interface import SearchInterface
+from opensearchpy.exceptions import OpenSearchException
+
+logger = logging.getLogger(__name__)
+
+
+class OpenSearchServiceException(Exception):
+    def __init__(
+        self,
+        message="An unexpected error occurred while processing the request."
+    ):
+        self.message = message
+        super().__init__(self.message)
+
+
+class OpenSearchService(SearchInterface):
+    def __init__(self):
+        self.__client = OpenSearchServiceConnection().client
+
+    def search_index(self, index_name, query_body):
+        try:
+            response = self.__client.search(
+                body=query_body,
+                index=index_name
+            )
+            return response
+        except OpenSearchException as e:
+            logger.error(f"An error occurred while \
+                            searching in index '{index_name}': {e}")
+            raise OpenSearchServiceException()
