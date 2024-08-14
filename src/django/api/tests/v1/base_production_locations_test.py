f47e18a74303b31f28e0af155df66f9972abe27b
Roman Stolar <roman.stolar@speedandfunction.com>
2024-08-14 12:49:39 +0300

    implement production-location endpoint tests

---
+++ src/django/api/tests/v1/base_production_locations_test.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from rest_framework.test import APITestCase
+
+from api.models import (
+    Contributor,
+    User,
+)
+
+
+class BaseProductionLocationsTest(APITestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        setattr(settings, 'DEBUG', True)
+
+        self.user_email = "test@example.com"
+        self.user_password = "example123"
+        self.user = User.objects.create(email=self.user_email)
+        self.user.set_password(self.user_password)
+        self.user.save()
+
+        self.contributor = Contributor.objects.create(
+            admin=self.user,
+            name="test contributor",
+            contrib_type=Contributor.OTHER_CONTRIB_TYPE,
+        )
+
+        self.superuser_email = "superuser@example.com"
+        self.superuser_password = "superuser"
+
+        self.superuser = User.objects.create_superuser(
+            self.superuser_email, self.superuser_password
+        )
+
+        self.supercontributor = Contributor.objects.create(
+            admin=self.superuser,
+            name="test super contributor",
+            contrib_type=Contributor.OTHER_CONTRIB_TYPE,
+        )
+  
+    def tearDown(self):
+        setattr(settings, 'DEBUG', False)
---
+++ src/django/api/tests/v1/test_production_locations.py
@@ -0,0 +1,33 @@
+from api.tests.v1.base_production_locations_test import BaseProductionLocationsTest
+
+
+class ProductionLocationsTest(BaseProductionLocationsTest):
+
+    def test_production_locations_status(self):
+        self.client.login(
+            email=self.superuser_email, password=self.superuser_password
+        )
+
+        response = self.client.get(
+                '/api/v1/production-locations/'
+            )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_production_locations_exact(self):
+        self.client.login(
+            email=self.superuser_email,
+            password=self.superuser_password
+        )
+
+        search_name = "Jiaxing%20Rising%20Garments%20Co.%20Ltd"
+        query = "?size=1&name={}&sort_by=name&order_by=asc".\
+            format(search_name)
+
+        response = self.client.get(
+                '/api/v1/production-locations/{}'.format(query)
+            )
+
+        result = response.json()
+        self.assertEqual(result['count'], 1)
+        self.assertEqual(result['data'][0]['os_id'], 'CN2024225RQ8DJR')
