4ae431f3909d7c65890cc8ec2db3044b7fcdc119
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2024-08-16 16:49:37 +0400

    Add size param validation

---
+++ src/django/api/serializers/v1/opensearch_common_validators/size_validator.py
@@ -0,0 +1,30 @@
+from api.serializers.v1.opensearch_validation_interface \
+    import OpenSearchValidationInterface
+
+
+class SizeValidator(OpenSearchValidationInterface):
+    def validate_opensearch_params(self, data):
+        errors = []
+        size = data.get('size')
+        size_limit = 250
+
+        if size is None:
+            return errors
+
+        try:
+            size = int(size)
+            if size <= 0 or size > size_limit:
+                errors.append({
+                    "field": "size",
+                    "message": (
+                        f"Size must be a positive integer less than or equal "
+                        f"to {size_limit}."
+                    )
+                })
+        except ValueError:
+            errors.append({
+                "field": "size",
+                "message": "Size must be a valid integer."
+            })
+
+        return errors
