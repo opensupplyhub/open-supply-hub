cbccf36c37187962a3152f128916659421131009
Roman Stolar <roman.stolar@speedandfunction.com>
2024-08-19 15:03:20 +0300

    running bash script to build & run OS Hub for code quality move api token to env variable

--- src/tests/v1/base_production_locations_test.py
+++ src/tests/v1/base_production_locations_test.py
@@ -11,10 +11,12 @@ class BaseProductionLocationsTest(OpenSearchIntegrationTestCase):
 
         host = os.getenv('REACT_HOST')
         port = os.getenv('REACT_PORT')
+        token = os.getenv('USER_API_TOKEN')
+
         self.root_url = f"http://{host}:{port}"
 
         self.basic_headers = {
             'Content-Type': 'application/json',
-            'Authorization': 'Token 1d18b962d6f976b0b7e8fcf9fcc39b56cf278051',
+            'Authorization': f'Token {token}',
             'Host': "localhost:6543"
         }
