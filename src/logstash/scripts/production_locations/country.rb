35e4b3b2f9727b22430aa0a39b09ad76ed36e7cc
Roman Stolar <roman.stolar@speedandfunction.com>
2024-08-26 16:53:48 +0300

    added countries constant & connect to country filter method

--- src/logstash/scripts/production_locations/country.rb
+++ src/logstash/scripts/production_locations/country.rb
@@ -1,8 +1,13 @@
+require_relative 'countries'
+
 def filter(event)
     alpha_2_country_code = event.get('country_value')
   
     country = {
+        'name' => COUNTRY_NAMES[alpha_2_country_code],
         'alpha_2' => alpha_2_country_code,
+        'alpha_3' => COUNTRY_ALPHA_3[alpha_2_country_code],
+        'numeric' => COUNTRY_NUMERIC[alpha_2_country_code],
     }
     event.set('country', country)
   
