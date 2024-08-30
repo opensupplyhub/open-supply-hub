c020a987f017a9ad36382be7aa33ab2090cc0733
mazur <mazursasha1990@gmail.com>
2024-08-30 13:18:38 +0300

    Remove phone_number field from the FacilityClaim model

--- src/django/api/migrations/0155_remove_verification_method_column_from_facility_claim.py
+++ src/django/api/migrations/0155_remove_verification_method_column_from_facility_claim.py
@@ -9,7 +9,8 @@ helper = MigrationHelper(connection)
 def update_indexing_function(apps, schema_editor):
     '''
     This function replaces the old index_approved_claim function with a
-    similar one that does not index verification_method field.
+    similar one that does not index verification_method and
+    phone_number fields.
     '''
 
     helper.run_sql_files(['0155_index_approved_claim.sql'])
@@ -17,9 +18,10 @@ def update_indexing_function(apps, schema_editor):
 
 class Migration(Migration):
     '''
-    Since the verification_method field is no longer necessary for the
-    claim form and isn't used anywhere in the codebase, it should be deleted
-    from the FacilityClaim model and the respective history table.
+    Since the verification_method and phone_number fields is no longer
+    necessary for the claim form and isn't used anywhere in the codebase,
+    it should be deleted from the FacilityClaim model and the respective
+    history table.
     '''
 
     dependencies = [
@@ -28,13 +30,21 @@ class Migration(Migration):
 
     operations = [
         RunPython(update_indexing_function),
+
         RemoveField(
             model_name='facilityclaim',
             name='verification_method',
         ),
-
+        RemoveField(
+            model_name='facilityclaim',
+            name='phone_number',
+        ),
         RemoveField(
             model_name='historicalfacilityclaim',
             name='verification_method',
         ),
+        RemoveField(
+            model_name='historicalfacilityclaim',
+            name='phone_number',
+        )
     ]
