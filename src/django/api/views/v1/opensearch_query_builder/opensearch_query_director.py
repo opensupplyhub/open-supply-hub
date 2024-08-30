b26767c002e7914aed1eb671746909deae3c5b15
Inessa Druzhkova <inessadruzkova@inessas-mbp.home>
2024-08-30 09:27:24 +0200

    fixed searching for coordinates

--- src/django/api/views/v1/opensearch_query_builder/opensearch_query_director.py
+++ src/django/api/views/v1/opensearch_query_builder/opensearch_query_director.py
@@ -63,7 +63,7 @@ class OpenSearchQueryDirector:
 
             if query_type == "geo_distance":
                 lat = query_params.get(f"{field}[lat]")
-                lng = query_params.get(f"{field}[lon]")
+                lng = query_params.get(f"{field}[lng]")
                 distance = query_params.get("distance", "10km")
                 self.__add_geo_distance_query(field, lat, lng,
                                               distance)
