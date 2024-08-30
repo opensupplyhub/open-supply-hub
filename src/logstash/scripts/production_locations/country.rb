3bbb5eb55cdd41786973206d1a3dbe30e294574f
roman-stolar <88504352+roman-stolar@users.noreply.github.com>
2024-08-30 13:33:24 +0300

    [OSDEV-1101] API v1/production-locations. Extend the country object to include alpha-3 code, numeric code, and country name (#336)
    [OSDEV-1101](https://opensupplyhub.atlassian.net/browse/OSDEV-1101)
    **API v1/production-locations. Extend the country object to include
    alpha-3 code, numeric code, and country name**
    - added countries format json's & connect to country filter method in
    logstash
    - updated dockerfile to copy static-data folder
    - updated production-locations index json schema
    
    [OSDEV-1101]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-1101?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/logstash/scripts/production_locations/country.rb
+++ src/logstash/scripts/production_locations/country.rb
@@ -1,10 +1,39 @@
+require 'json'
+require 'fileutils'
+
 def filter(event)
+    # Country code Alpha-2 data
     alpha_2_country_code = event.get('country_value')
-  
+
+    # Define the path to the JSON file
+    json_countries_file_path = File.expand_path('../../../static_data/countries.json', __FILE__)
+
+    # Read and parse the JSON file
+    json_countries_data = File.read(json_countries_file_path)
+    countries = JSON.parse(json_countries_data)
+    exact_country = countries[alpha_2_country_code]
+
+    # Build country object
     country = {
+        'name' => exact_country["name"],
         'alpha_2' => alpha_2_country_code,
+        'alpha_3' => exact_country["alpha_3"],
+        'numeric' => exact_country["numeric"],
     }
     event.set('country', country)
   
     return [event]
+end
+
+test "Country filter" do
+
+    in_event { { 'country_value' => "UA" } }
+  
+    expect("Get object data") do |events|
+      events.size == 1
+      events[0].get('country')['name'] == "Ukraine"
+      events[0].get('country')['alpha_2'] == "UA"
+      events[0].get('country')['alpha_3'] == "UKR"
+      events[0].get('country')['numeric'] == "804"
+    end
 end
