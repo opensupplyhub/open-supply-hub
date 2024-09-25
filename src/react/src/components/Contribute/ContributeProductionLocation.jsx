06f0490b3c3713ce153c0cbbb257ce6a6e4cc632
mazur <mazursasha1990@gmail.com>
2024-09-25 14:11:38 +0300

    Use hash in the url to mark active tab

--- src/react/src/components/Contribute/ContributeProductionLocation.jsx
+++ src/react/src/components/Contribute/ContributeProductionLocation.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { useLocation, useHistory } from 'react-router-dom';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
@@ -7,10 +8,32 @@ import SearchByOsId from './SearchByOsId';
 import { makeContributeProductionLocationStyles } from '../../util/styles';
 
 const ContributeProductionLocation = ({ classes }) => {
-    const [selectedTab, setSelectedTab] = useState(0);
+    const location = useLocation();
+    const history = useHistory();
+
+    const hash = location.hash ? location.hash.substring(1) : '';
+    const initialTab = hash === 'name-address' ? 1 : 0;
+
+    const [selectedTab, setSelectedTab] = useState(initialTab);
+
+    useEffect(() => {
+        if (!location.hash) {
+            history.replace('#os-id');
+        }
+
+        const handleHashChange = () => {
+            const newHash = window.location.hash.substring(1);
+            setSelectedTab(newHash === 'name-address' ? 1 : 0);
+        };
+
+        window.addEventListener('hashchange', handleHashChange);
+        return () => window.removeEventListener('hashchange', handleHashChange);
+    }, [location.hash, history]);
 
     const handleChange = (event, value) => {
         setSelectedTab(value);
+        const newHash = value === 1 ? '#name-address' : '#os-id';
+        history.replace(newHash);
     };
 
     return (
