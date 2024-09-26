aa5f145b9817486014134cf4b140e042af831b6f
mazur <mazursasha1990@gmail.com>
2024-09-26 18:48:58 +0300

    Add tets for BackToSearchButton component

---
+++ src/react/src/__tests__/components/BackToSearchButton.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import BackToSearchButton from '../../components/Contribute/BackToSearchButton';
+import renderWithProviders from '../../util/testUtils/renderWithProviders';
+
+describe('BackToSearchButton component', () => {
+    const handleBackToSearch = jest.fn();
+    const defaultProps = {
+        label: 'Back to ID search',
+        handleBackToSearch: handleBackToSearch,
+    };
+
+    it('renders the button with the correct label', () => {
+        const { getByText } = renderWithProviders(<BackToSearchButton {...defaultProps} />);
+
+        expect(getByText('Back to ID search')).toBeInTheDocument();
+    });
+
+    it('calls the handleBackToSearch function when clicked', () => {
+        const { getByText } = renderWithProviders(<BackToSearchButton {...defaultProps} />);
+
+        fireEvent.click(getByText('Back to ID search'));
+
+        expect(handleBackToSearch).toHaveBeenCalledTimes(1);
+    });
+});
