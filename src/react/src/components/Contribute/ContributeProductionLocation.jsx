d222170dc990408eeda28ce13d5a40a570467c7e
mazur <mazursasha1990@gmail.com>
2024-09-26 15:37:59 +0300

    Sort imports in the ContributeProductionLocation.jsx

--- src/react/src/components/Contribute/ContributeProductionLocation.jsx
+++ src/react/src/components/Contribute/ContributeProductionLocation.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import { useLocation, useHistory } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
