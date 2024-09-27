c06099afcfeb9c36f9dddda35ca0a6d340c066c1
mazur <mazursasha1990@gmail.com>
2024-09-27 13:08:15 +0300

    Add tets for SearchByOsIdNotFoundResult component

---
+++ src/react/src/__tests__/components/SearchByOsIdNotFoundResult.test.js
@@ -0,0 +1,29 @@
+import React from 'react';  
+import SearchByOsIdNotFoundResult from '../../components/Contribute/SearchByOsIdNotFoundResult';
+import renderWithProviders from '../../util/testUtils/renderWithProviders';
+
+jest.mock('../../components/Contribute/SearchByOsIdResultActions', () => () => <div>Mocked SearchByOsIdResultActions</div>);
+
+describe('SearchByOsIdNotFoundResult component', () => {
+    const handleBackToSearchByNameAddress = jest.fn();
+    const handleBackToSearchByOsId = jest.fn();
+
+    const defaultProps = {
+        handleBackToSearchByNameAddress,
+        handleBackToSearchByOsId,
+    };
+
+    it('renders without crashing', () => {
+        const { getByText } = renderWithProviders(<SearchByOsIdNotFoundResult {...defaultProps}/>);
+
+        expect(getByText("We didn't find a production location with that ID.")).toBeInTheDocument();
+        expect(getByText('You can try searching by another OS ID or searching by name and address.')).toBeInTheDocument();
+    });
+
+    it('renders SearchByOsIdResultActions', () => {
+        const { getByText } = renderWithProviders(<SearchByOsIdNotFoundResult {...defaultProps}/>);
+
+        expect(getByText('Mocked SearchByOsIdResultActions')).toBeInTheDocument();
+    });
+
+});
