aa5c4aa550e7326ec4f364bf8e63633ff133df49
vladsha-dev <newshapovalov2002@gmail.com>
2024-10-08 15:02:02 +0200

    Removed redundant code related to the previously used facility_list_items.json, removed old matching logic

--- src/django/contricleaner/lib/helpers/clean.py
+++ src/django/contricleaner/lib/helpers/clean.py
@@ -6,8 +6,7 @@ from unidecode import unidecode
 def clean(column):
     """
     Remove punctuation and excess whitespace from a value before using it to
-    find matches. This should be the same function used when developing the
-    training data read from training.json as part of train_gazetteer.
+    find matches.
     """
     column = unidecode(column)
     column = re.sub("\n", " ", column)
