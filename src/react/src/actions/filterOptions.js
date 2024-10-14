b384e66698ee98c75f01befa1f819620779a7f0b
mazur <mazursasha1990@gmail.com>
2024-10-14 20:02:19 +0300

    Add actions for record type options

--- src/react/src/actions/filterOptions.js
+++ src/react/src/actions/filterOptions.js
@@ -116,6 +116,15 @@ export const failFetchClaimStatusOption = createAction(
 export const completeFetchClaimStatusOption = createAction(
     'COMPLETE_FETCH_CLAIM_STATUS_OPTIONS',
 );
+export const startFetchRecordTypeOptions = createAction(
+    'START_FETCH_RECORD_TYPE_OPTIONS',
+);
+export const failFetchRecordTypeOptions = createAction(
+    'FAIL_FETCH_RECORD_TYPE_OPTIONS',
+);
+export const completeFetchRecordTypeOptions = createAction(
+    'COMPLETE_FETCH_RECORD_TYPE_OPTIONS',
+);
 
 export const resetFilterOptions = createAction('RESET_FILTER_OPTIONS');
 
@@ -340,6 +349,31 @@ export function fetchClaimStatusOptions() {
                         err,
                         'An error prevented fetching of claim status options',
                         failFetchClaimStatusOption,
+                    ),
+                ),
+            );
+    };
+}
+
+export function fetchRecordTypeOptions() {
+    return async dispatch => {
+        dispatch(startFetchRecordTypeOptions());
+
+        // TODO: replace with actual API call during implementation OSDEV-1175
+        return new Promise(resolve => {
+            const mockData = ['Claim', 'Single', 'API'];
+            const mappedData = mapDjangoChoiceTuplesValueToSelectOptions(
+                mockData,
+            );
+            setTimeout(() => resolve(mappedData), 1000);
+        })
+            .then(data => dispatch(completeFetchRecordTypeOptions(data)))
+            .catch(err =>
+                dispatch(
+                    logErrorAndDispatchFailure(
+                        err,
+                        'An error prevented fetching record type options',
+                        failFetchRecordTypeOptions,
                     ),
                 ),
             );
