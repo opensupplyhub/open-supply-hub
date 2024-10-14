11416143a9e1884e679d8c898920728770fda699
mazur <mazursasha1990@gmail.com>
2024-10-14 20:05:56 +0300

    Use recordTypesOptions from the state

--- src/react/src/components/Filters/RecordTypeFilter.jsx
+++ src/react/src/components/Filters/RecordTypeFilter.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bool, arrayOf, shape, string } from 'prop-types';
 import StyledSelect from './StyledSelect';
 
 const RECORD_TYPE = 'RECORD_TYPE';
-const RecordTypeFilter = props => {
-    console.log('RecordTypeFilter props >>>', props);
-    const recordTypes = [
-        {
-            label: 'Claim',
-            value: 'Claim',
-        },
-        {
-            label: 'Single',
-            value: 'Single',
-        },
-        {
-            label: 'API',
-            value: 'API',
-        },
-    ];
+const RecordTypeFilter = ({
+    isDisabled,
+    recordTypesOptions,
+    fetchingRecordTypes,
+    // updateRecordType,
+    // recordTypes,
+}) => {
+    console.log('isDisabled >>>', isDisabled);
+    console.log('recordTypesOptions >>>', recordTypesOptions);
+    console.log('fetchingRecordTypes >>>', fetchingRecordTypes);
 
     return (
         <div className="form__field">
             <StyledSelect
                 label="Record Type"
                 name={RECORD_TYPE}
-                options={recordTypes || []}
+                options={recordTypesOptions || []}
                 // value={countries}
                 // onChange={updateCountry}
                 // disabled={fetching}
@@ -34,4 +30,39 @@ const RecordTypeFilter = props => {
     );
 };
 
-export default RecordTypeFilter;
+RecordTypeFilter.defaultProps = {
+    isDisabled: false,
+    recordTypesOptions: null,
+};
+
+RecordTypeFilter.propTypes = {
+    isDisabled: bool,
+    recordTypesOptions: arrayOf(
+        shape({
+            value: string.isRequired,
+            label: string.isRequired,
+        }),
+    ),
+    // recordTypesOptions: recordTypesOptionsPropType,
+    // updateRecordType: func.isRequired,
+    // recordTypes: recordTypesOptionsPropType.isRequired,
+    fetchingRecordTypes: bool.isRequired,
+};
+
+const mapStateToProps = ({
+    // filters: { recordTypes },
+    filterOptions: {
+        recordTypes: {
+            data: recordTypesOptions,
+            fetching: fetchingRecordTypes,
+        },
+    },
+}) => ({
+    // recordTypes,
+    recordTypesOptions,
+    fetchingRecordTypes,
+});
+
+const mapDispatchToProps = () => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecordTypeFilter);
