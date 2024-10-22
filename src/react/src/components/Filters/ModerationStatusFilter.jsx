d90e78ed7bc8f964fbb03f41bf77decf447c9bf1
mazur <mazursasha1990@gmail.com>
2024-10-22 20:05:27 +0300

    Refactor ModerationStatusFilter

--- src/react/src/components/Filters/ModerationStatusFilter.jsx
+++ src/react/src/components/Filters/ModerationStatusFilter.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
+import { bool, string, func } from 'prop-types';
 import { connect } from 'react-redux';
-import { bool, string } from 'prop-types';
-import StyledSelect from './StyledSelect';
+import { updateModerationStatusFilter } from '../../actions/filters';
+import { defaultOptionsPropType } from '../../util/propTypes';
 import { MODERATION_STATUSES_OPTIONS } from '../../util/constants';
+import StyledSelect from './StyledSelect';
 
 const MODERATION_STATUS = 'MODERATION_STATUS';
 
-const ModerationStatusFilter = ({ isDisabled, className }) => {
-    console.log('ModerationStatusFilter isDisabled >>>', isDisabled);
-
-    return (
-        <div className={className}>
-            <StyledSelect
-                label="Moderation Status"
-                name={MODERATION_STATUS}
-                options={MODERATION_STATUSES_OPTIONS || []}
-                // value={countries}
-                // onChange={updateCountry}
-                // disabled={fetching}
-                isDisabled={isDisabled}
-            />
-        </div>
-    );
-};
+const ModerationStatusFilter = ({
+    updateStatus,
+    moderationStatuses,
+    isDisabled,
+    className,
+}) => (
+    <div className={className}>
+        <StyledSelect
+            label="Moderation Status"
+            name={MODERATION_STATUS}
+            options={MODERATION_STATUSES_OPTIONS || []}
+            value={moderationStatuses}
+            onChange={updateStatus}
+            isDisabled={isDisabled}
+        />
+    </div>
+);
 
 ModerationStatusFilter.defaultProps = {
     isDisabled: false,
@@ -30,13 +32,18 @@ ModerationStatusFilter.defaultProps = {
 };
 
 ModerationStatusFilter.propTypes = {
+    updateStatus: func.isRequired,
+    moderationStatuses: defaultOptionsPropType.isRequired,
     isDisabled: bool,
     className: string,
 };
 
-const mapStateToProps = () => {};
-
-const mapDispatchToProps = () => {};
+const mapStateToProps = ({ filters: { moderationStatuses } }) => ({
+    moderationStatuses,
+});
+const mapDispatchToProps = dispatch => ({
+    updateStatus: value => dispatch(updateModerationStatusFilter(value)),
+});
 
 export default connect(
     mapStateToProps,
