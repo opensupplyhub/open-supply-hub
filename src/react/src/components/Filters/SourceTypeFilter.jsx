a6fb21280b22419fc04303c174d257c88dade7cb
mazur <mazursasha1990@gmail.com>
2024-10-22 20:04:57 +0300

    Refactor SourceTypeFilter

--- src/react/src/components/Filters/SourceTypeFilter.jsx
+++ src/react/src/components/Filters/SourceTypeFilter.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
+import { bool, string, func } from 'prop-types';
 import { connect } from 'react-redux';
-import { bool, string } from 'prop-types';
-import StyledSelect from './StyledSelect';
+import { updateSourceTypeFilter } from '../../actions/filters';
+import { defaultOptionsPropType } from '../../util/propTypes';
 import { SOURCE_TYPES_OPTIONS } from '../../util/constants';
+import StyledSelect from './StyledSelect';
 
 const SOURCE_TYPE = 'SOURCE_TYPE';
 
-const SourceTypeFilter = ({ isDisabled, className }) => {
-    console.log('isDisabled >>>', isDisabled);
-
-    return (
-        <div className={className}>
-            <StyledSelect
-                label="Source Type"
-                name={SOURCE_TYPE}
-                options={SOURCE_TYPES_OPTIONS || []}
-                // value={countries}
-                // onChange={updateCountry}
-                // disabled={fetching}
-                isDisabled={isDisabled}
-            />
-        </div>
-    );
-};
+const SourceTypeFilter = ({
+    updateSource,
+    sourceTypes,
+    isDisabled,
+    className,
+}) => (
+    <div className={className}>
+        <StyledSelect
+            label="Source Type"
+            name={SOURCE_TYPE}
+            options={SOURCE_TYPES_OPTIONS || []}
+            value={sourceTypes}
+            onChange={updateSource}
+            isDisabled={isDisabled}
+        />
+    </div>
+);
 
 SourceTypeFilter.defaultProps = {
     isDisabled: false,
@@ -30,12 +32,18 @@ SourceTypeFilter.defaultProps = {
 };
 
 SourceTypeFilter.propTypes = {
+    updateSource: func.isRequired,
+    sourceTypes: defaultOptionsPropType.isRequired,
     isDisabled: bool,
     className: string,
 };
 
-const mapStateToProps = () => {};
+const mapStateToProps = ({ filters: { sourceTypes } }) => ({
+    sourceTypes,
+});
 
-const mapDispatchToProps = () => {};
+const mapDispatchToProps = dispatch => ({
+    updateSource: value => dispatch(updateSourceTypeFilter(value)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SourceTypeFilter);
