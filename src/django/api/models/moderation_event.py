f2fb3dbf7c4d2f58d6bb603d47700fb29ee8f716
Vadim Kovalenko <90321493+VadimKovalenkoSNF@users.noreply.github.com>
2024-10-23 14:55:54 +0400

    [OSDEV-1229] Create moderation events table (#376)
    [OSDEV-1229](https://opensupplyhub.atlassian.net/browse/OSDEV-1229) -
    Create moderation events table, introduces related migration file.
    
    
    [OSDEV-1229]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-1229?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    Created table with columns:
    1. `uuid` - unique identifier to make moderation event table more
    reusable across the app.
    2. `created_at` - date when the moderation queue entry was created.
    3. `updated_at` - date when the moderation queue entry was last updated.
    This column is indexing.
    4. `status_change_date` - date when the moderation decision was made.
    5. `contributor_id` - linked contributor responsible for this moderation
    event.
    6. `claim_id` - linked claim id for this production location.
    7. `request_type` - type of moderation record.
    8. `raw_data` - key-value pairs of the non-parsed row and header for the
    moderation data.
    9. `cleaned_data` - key-value pairs of the parsed row and header for the
    moderation data.
    10. `geocode_result` - result of the geocode operation.
    11. `status` - moderation status of the production location.
    12. `source` - source type of production location. If `request_type` is
    CLAIM, no source type.
      
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new Moderation Events table to track moderation
    activities.
    	- Implemented throttling for API endpoints to enhance performance.
    - Improved search functionality with a new tokenizer for better query
    results.
    
    - **Documentation**
    - Updated release notes to standardize the release date format and
    included new sections for architecture, bug fixes, and features.
    
    - **Bug Fixes**
    - Added placeholders in release notes for future bug fixes and new
    features.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ src/django/api/models/moderation_event.py
@@ -0,0 +1,128 @@
+import uuid
+from django.db import models
+from api.models.contributor.contributor import Contributor
+from api.models.facility.facility_claim import FacilityClaim
+
+
+class ModerationEvent(models.Model):
+    '''
+    Data that is needed for moderation queue.
+    '''
+    class RequestType(models.TextChoices):
+        CREATE = 'CREATE', 'Create'
+        UPDATE = 'UPDATE', 'Update'
+        CLAIM = 'CLAIM', 'Claim'
+
+    class Status(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        RESOLVED = 'RESOLVED', 'Resolved'
+
+    class Source(models.TextChoices):
+        API = 'API', 'API'
+        SLC = 'SLC', 'SLC'
+
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+        db_index=True,
+        help_text=(
+            'Unique identifier to make moderation '
+            'event table more reusable across the app.'
+        )
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        help_text='Date when the moderation queue entry was created.'
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        help_text='Date when the moderation queue entry was last updated.',
+        db_index=True
+    )
+
+    status_change_date = models.DateTimeField(
+        blank=True,
+        null=True,
+        help_text='Date when the moderation decision was made.'
+    )
+
+    contributor = models.ForeignKey(
+        Contributor,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name='moderation_events',
+        help_text='Linked contributor responsible for this moderation event.'
+    )
+
+    claim = models.OneToOneField(
+        FacilityClaim,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='moderation_event',
+        help_text='Linked claim id for this production location.'
+    )
+
+    request_type = models.CharField(
+        max_length=6,
+        null=False,
+        choices=RequestType.choices,
+        help_text='Type of moderation record.'
+    )
+
+    raw_data = models.JSONField(
+        null=False,
+        blank=False,
+        default=dict,
+        help_text=(
+            'Key-value pairs of the non-parsed row and '
+            'header for the moderation data.'
+        )
+    )
+
+    cleaned_data = models.JSONField(
+        null=False,
+        blank=False,
+        default=dict,
+        help_text=(
+            'Key-value pairs of the parsed row and '
+            'header for the moderation data.'
+        )
+    )
+
+    geocode_result = models.JSONField(
+        null=False,
+        blank=False,
+        default=dict,
+        help_text=(
+            'Result of the geocode operation.'
+        )
+    )
+
+    status = models.CharField(
+        max_length=8,
+        choices=Status.choices,
+        default=Status.PENDING,
+        null=False,
+        help_text='Moderation status of the production location.'
+    )
+
+    source = models.CharField(
+        max_length=3,
+        choices=Source.choices,
+        blank=True,
+        default='',
+        help_text=(
+            'Source type of production location.'
+            ' If request_type is CLAIM, no source type.'
+        )
+    )
+
+    def __str__(self):
+        return (
+            f'ModerationEvent entry {self.uuid} '
+            f'with request type {self.request_type} '
+            f'and status {self.status}'
+        )
