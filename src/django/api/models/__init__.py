54b61f1d9b42e472f767999d3736663031ff3d1d
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2024-10-23 13:30:17 +0400

    Revert src/django/api/models/__init__.py

--- src/django/api/models/__init__.py
+++ src/django/api/models/__init__.py
@@ -1,78 +1,77 @@
 # flake8: noqa: F401
 
-from api.models.api.api_block import (
+from .api.api_block import (
   ApiBlock,
   HistoricalApiBlock,
 )
-from api.models.api.api_limit import (
+from .api.api_limit import (
   ApiLimit,
   HistoricalApiLimit,
 )
-from api.models.contributor.contributor import (
+from .contributor.contributor import (
   Contributor,
   HistoricalContributor
 )
-from api.models.contributor.contributor_manager import ContributorManager
-from api.models.contributor.contributor_notifications import (
+from .contributor.contributor_manager import ContributorManager
+from .contributor.contributor_notifications import (
   ContributorNotifications,
   HistoricalContributorNotifications
 )
-from api.models.facility.facility import (
+from .facility.facility import (
   Facility,
   HistoricalFacility,
 )
-from api.models.facility.facility_activity_report import (
+from .facility.facility_activity_report import (
   FacilityActivityReport,
   HistoricalFacilityActivityReport,
 )
-from api.models.facility.facility_alias import (
+from .facility.facility_alias import (
   FacilityAlias,
   HistoricalFacilityAlias,
 )
-from api.models.facility.facility_claim import (
+from .facility.facility_claim import (
   FacilityClaim,
   HistoricalFacilityClaim,
 )
-from api.models.facility.facility_claim_review_note import (
+from .facility.facility_claim_review_note import (
   FacilityClaimReviewNote,
   HistoricalFacilityClaimReviewNote,
 )
-from api.models.facility.facility_claim_attachments import (
+from .facility.facility_claim_attachments import (
     FacilityClaimAttachments
 )
-from api.models.facility.facility_list import FacilityList
-from api.models.facility.facility_list_item import FacilityListItem
-from api.models.facility.facility_list_item_temp import FacilityListItemTemp
-from api.models.facility.facility_location import FacilityLocation
+from .facility.facility_list import FacilityList
+from .facility.facility_list_item import FacilityListItem
+from .facility.facility_list_item_temp import FacilityListItemTemp
+from .facility.facility_location import FacilityLocation
 
-from api.models.facility.facility_match import (
+from .facility.facility_match import (
   FacilityMatch,
   HistoricalFacilityMatch,
 )
-from api.models.facility.facility_match_temp import (
+from .facility.facility_match_temp import (
     FacilityMatchTemp,
     HistoricalFacilityMatchTemp,
 )
-from api.models.download_log import DownloadLog
-from api.models.embed_config import EmbedConfig
-from api.models.embed_field import EmbedField
-from api.models.event import Event
-from api.models.extended_field import (
+from .download_log import DownloadLog
+from .embed_config import EmbedConfig
+from .embed_field import EmbedField
+from .event import Event
+from .extended_field import (
   ExtendedField,
   HistoricalExtendedField
 )
-from api.models.nonstandart_field import NonstandardField
-from api.models.product_type import ProductType
-from api.models.production_type import ProductionType
-from api.models.request_log import RequestLog
-from api.models.sector import Sector
-from api.models.source import Source
-from api.models.user import (
+from .nonstandart_field import NonstandardField
+from .product_type import ProductType
+from .production_type import ProductionType
+from .request_log import RequestLog
+from .sector import Sector
+from .source import Source
+from .user import (
   EmailAsUsernameUserManager,
   get_default_burst_rate,
   get_default_sustained_rate,
   get_default_data_upload_rate,
   User,
 )
-from api.models.version import Version
-from api.models.moderation_event import ModerationEvent
+from .version import Version
