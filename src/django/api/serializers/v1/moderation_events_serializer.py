5d7461a631fbc4439e6ce3824f40b312826b44c7
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2024-10-25 12:35:48 +0400

    Add moderation-event GET endpoint

---
+++ src/django/api/serializers/v1/moderation_events_serializer.py
@@ -0,0 +1,72 @@
+from rest_framework.serializers import (
+    CharField,
+    ChoiceField,
+    DateField,
+    IntegerField,
+    ListField,
+    Serializer,
+    ValidationError
+)
+from api.serializers.v1.opensearch_common_validators.size_validator \
+    import SizeValidator
+from api.serializers.v1.opensearch_error_list_builder  \
+    import OpenSearchErrorListBuilder
+from api.serializers.v1.opensearch_common_validators. \
+    countries_validator import CountryValidator
+from api.serializers.v1.opensearch_common_validators. \
+    date_range_validator import DateRangeValidator
+from api.views.v1.utils import COMMON_ERROR_MESSAGE
+
+
+class ModerationEventsSerializer(Serializer):
+    # These params are checking considering serialize_params output
+    size = IntegerField(required=False)
+    country = ListField(
+        child=CharField(required=False),
+        required=False
+    )
+    contributor_id = IntegerField(required=False)
+    os_id = CharField(required=False)
+    # TODO: add possibility to pass lowercase values?
+    request_type = ChoiceField(
+        choices=['CREATE', 'UPDATE', 'CLAIM'],
+        required=False
+    )
+    source = ChoiceField(
+        choices=['API', 'SLC'],
+        required=False
+    )
+    moderation_status = ChoiceField(
+        choices=['PENDING', 'RESOLVED'],
+        required=False
+    )
+    moderation_id = CharField(required=False)
+    data_dte = DateField(default='', required=False)
+    data_lt = DateField(default='', required=False)
+    sort_by = ChoiceField(
+        choices=['name', 'address'],
+        required=False
+    )
+    order_by = ChoiceField(
+        choices=['asc', 'desc'],
+        required=False
+    )
+
+    def validate(self, data):
+        validators = [
+            SizeValidator(),
+            CountryValidator(),
+            DateRangeValidator(),
+        ]
+
+        error_list_builder = OpenSearchErrorListBuilder(validators)
+        errors = error_list_builder.build_error_list(data)
+
+        if errors:
+            # TODO: Pass error msg to the Rollbar here
+            raise ValidationError({
+                "message": COMMON_ERROR_MESSAGE,
+                "errors": errors
+            })
+
+        return data
---
+++ src/django/api/views/v1/moderation_events.py
@@ -0,0 +1,61 @@
+import logging
+# from django.http import QueryDict
+from rest_framework import status
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from api.views.v1.utils import (
+    serialize_params,
+    handle_errors_decorator
+)
+from api.services.search import OpenSearchService
+from api.views.v1.opensearch_query_builder.opensearch_query_builder \
+    import OpenSearchQueryBuilder
+from api.views.v1.opensearch_query_builder.opensearch_query_director \
+    import OpenSearchQueryDirector
+from api.serializers.v1.moderation_events_serializer \
+    import ModerationEventsSerializer
+# from api.views.v1.index_names import OpenSearchIndexNames
+
+logger = logging.getLogger(__name__)
+
+
+class ModerationEvents(ViewSet):
+    swagger_schema = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.opensearch_service = OpenSearchService()
+        self.opensearch_query_builder = OpenSearchQueryBuilder()
+        self.opensearch_query_director = OpenSearchQueryDirector(
+                self.opensearch_query_builder
+            )
+
+    @handle_errors_decorator
+    def list(self, request):
+        params, error_response = serialize_params(
+            ModerationEventsSerializer,
+            request.GET
+        )
+        if error_response:
+            return Response(
+                error_response,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        query_body = self.opensearch_query_director.build_query(
+            request.GET
+        )
+
+        logger.info(f'@@@ {query_body}')
+
+        # TODO: Apply moderation event Logstash index here
+        '''
+        response = self.opensearch_service.search_index(
+            OpenSearchIndexNames.MODERATION_EVENTS_INDEX,
+            query_body
+        )
+        return Response(response)
+        '''
+        return Response({
+            'moderation_event': 'response'
+        })
