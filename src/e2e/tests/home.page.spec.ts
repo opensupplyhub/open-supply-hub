559ba1ebab7188dd04b22d53816d9bc11fef5803
Peter Ovchyn <peter@boarlabs.xyz>
2024-11-05 14:49:03 +0200

    Prettify description

--- src/e2e/tests/home.page.spec.ts
+++ src/e2e/tests/home.page.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+
+/**
+ * @description Verifies that the home page loads with the correct title
+ */
+test('Home page loads with the correct title', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle(/Open Supply Hub/);
+  await expect(page).toHaveTitle('Open Supply Hub');;
 });
