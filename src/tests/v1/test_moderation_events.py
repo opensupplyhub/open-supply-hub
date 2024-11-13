aa8203b9008cec0f8f81021f4a9610d8473e31d3
VadimKovalenko <vadim.kovalenko@speedandfunction.com>
2024-11-13 12:34:55 +0400

    Minor improve for integration test

--- src/tests/v1/test_moderation_events.py
+++ src/tests/v1/test_moderation_events.py
@@ -10,7 +10,10 @@ class ModerationEventsTest(BaseAPITest):
             f"{self.root_url}/api/v1/moderation-events/",
             headers=self.basic_headers,
         )
+        result = response.json()
         self.assertEqual(response.status_code, 200)
+        self.assertIn('data', result)
+        self.assertIsInstance(result['data'], list)
 
     def test_filter_by_status(self):
         status = "APPROVED"
