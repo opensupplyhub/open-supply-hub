1b8c3b74de7239f059baf4d5ccc3c3efae9f52f2
Vadim Kovalenko <90321493+VadimKovalenkoSNF@users.noreply.github.com>
2024-11-14 17:26:05 +0400

    [OSDEV-1346] Add moderation-event GET endpoint (#391)
    Fix [OSDEV-1346](https://opensupplyhub.atlassian.net/browse/OSDEV-1346)
    
    Add GET `api/v1/moderation-events`
    
    [OSDEV-1346]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-1346?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: vladsha-dev <newshapovalov2002@gmail.com>

---
+++ src/django/api/serializers/v1/opensearch_common_validators/request_type_validator.py
@@ -0,0 +1,23 @@
+from typing import List
+from api.serializers.v1.opensearch_validation_interface \
+    import OpenSearchValidationInterface
+
+
+class RequestTypeValidator(OpenSearchValidationInterface):
+    def validate_opensearch_params(self, data) -> List[dict]:
+        errors: List[dict] = []
+        request_type = data.get('request_type')
+
+        if not request_type:
+            return errors
+
+        valid_request_types = {'CREATE', 'UPDATE', 'CLAIM'}
+
+        if request_type not in valid_request_types:
+            errors.append({
+                "field": "request_type",
+                "message": f"'{request_type}' is not a valid request_type. \
+                    Allowed values are 'CREATE', 'UPDATE' or 'CLAIM'."
+            })
+
+        return errors
--- src/django/api/views/v1/moderation_events.py
+++ src/django/api/views/v1/moderation_events.py
@@ -1,38 +1,37 @@
-from django.http import QueryDict
 from rest_framework import status
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-
 from api.views.v1.utils import (
     serialize_params,
     handle_errors_decorator
 )
-
+from api.permissions import IsRegisteredAndConfirmed
 from api.services.opensearch.search import OpenSearchService
-from api.views.v1.opensearch_query_builder.production_locations_query_builder \
-    import ProductionLocationsQueryBuilder
+from api.views.v1.opensearch_query_builder.moderation_events_query_builder \
+    import ModerationEventsQueryBuilder
 from api.views.v1.opensearch_query_builder.opensearch_query_director \
     import OpenSearchQueryDirector
-from api.serializers.v1.production_locations_serializer \
-    import ProductionLocationsSerializer
+from api.serializers.v1.moderation_events_serializer \
+    import ModerationEventsSerializer
 from api.views.v1.index_names import OpenSearchIndexNames
 
 
-class ProductionLocations(ViewSet):
+class ModerationEvents(ViewSet):
     swagger_schema = None
+    permission_classes = [IsRegisteredAndConfirmed]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.opensearch_service = OpenSearchService()
-        self.opensearch_query_builder = ProductionLocationsQueryBuilder()
+        self.moderation_events_query_builder = ModerationEventsQueryBuilder()
         self.opensearch_query_director = OpenSearchQueryDirector(
-                self.opensearch_query_builder
+                self.moderation_events_query_builder
             )
 
     @handle_errors_decorator
     def list(self, request):
         _, error_response = serialize_params(
-            ProductionLocationsSerializer,
+            ModerationEventsSerializer,
             request.GET
         )
         if error_response:
@@ -44,21 +43,9 @@ class ProductionLocations(ViewSet):
         query_body = self.opensearch_query_director.build_query(
             request.GET
         )
-        response = self.opensearch_service.search_index(
-            OpenSearchIndexNames.PRODUCTION_LOCATIONS_INDEX,
-            query_body
-        )
-        return Response(response)
 
-    @handle_errors_decorator
-    def retrieve(self, request,  pk=None):
-        query_params = QueryDict('', mutable=True)
-        query_params.update({'os_id': pk})
-        query_body = self.opensearch_query_director.build_query(
-            query_params
-        )
         response = self.opensearch_service.search_index(
-            OpenSearchIndexNames.PRODUCTION_LOCATIONS_INDEX,
+            OpenSearchIndexNames.MODERATION_EVENTS_INDEX,
             query_body
         )
         return Response(response)
