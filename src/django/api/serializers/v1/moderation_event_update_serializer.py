8b6f169f6ee10352e8dacd0ef47ae5429d0bb647
VadimKovalenko <vadim.kovalenko@speedandfunction.com>
2024-11-19 11:53:05 +0400

    Remove possible null os_id and claim_id from the PATCH response

--- src/django/api/serializers/v1/moderation_event_update_serializer.py
+++ src/django/api/serializers/v1/moderation_event_update_serializer.py
@@ -13,8 +13,8 @@ class ModerationEventUpdateSerializer(ModelSerializer):
 
     contributor_id = IntegerField(source='contributor.id', read_only=True)
     contributor_name = CharField(source='contributor.name', read_only=True)
-    os_id = IntegerField(source='os.id', read_only=True, allow_null=True)
-    claim_id = IntegerField(source='claim.id', read_only=True, allow_null=True)
+    os_id = IntegerField(source='os.id', read_only=True)
+    claim_id = IntegerField(source='claim.id', read_only=True)
 
     class Meta:
         model = ModerationEvent
