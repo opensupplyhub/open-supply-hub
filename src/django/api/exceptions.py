503ac5264a99f16e2a7b840bc77f92d4b3dbfb64
mazur <mazursasha1990@gmail.com>
2024-11-21 18:57:05 +0200

    Refactor ModerationEvents

--- src/django/api/exceptions.py
+++ src/django/api/exceptions.py
@@ -1,7 +1,22 @@
+from rest_framework import status
 from rest_framework.exceptions import APIException
 
 
 class BadRequestException(APIException):
-    status_code = 400
+    status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'Bad request'
     default_code = 'bad_request'
+
+
+class GoneException(APIException):
+    status_code = status.HTTP_410_GONE
+    default_detail = "The resource is no longer available."
+    default_code = "gone"
+
+
+class InternalServerErrorException(APIException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = (
+        "An unexpected error occurred while processing the request."
+    )
+    default_code = "internal_server_error"
