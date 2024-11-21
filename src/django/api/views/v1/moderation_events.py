203ccdbfb3537175b287d2b63d88ff18df1bdd94
mazur <mazursasha1990@gmail.com>
2024-11-21 18:57:22 +0200

    Add documentation to the process_moderation_event abstract method

--- src/django/api/views/v1/moderation_events.py
+++ src/django/api/views/v1/moderation_events.py
@@ -3,15 +3,16 @@ import logging
 from django.http import QueryDict
 
 from rest_framework import status
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, ParseError, NotFound
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
+from api.exceptions import GoneException, InternalServerErrorException
+from api.models.moderation_event import ModerationEvent
 from api.moderation_event_actions.approval.add_production_location_strategy \
     import AddProductionLocationStrategy
 from api.moderation_event_actions.approval.event_approval_context \
     import EventApprovalContext
-from api.models.moderation_event import ModerationEvent
 from api.permissions import IsRegisteredAndConfirmed
 from api.serializers.v1.moderation_event_update_serializer \
     import ModerationEventUpdateSerializer
@@ -23,13 +24,14 @@ from api.services.opensearch.search import OpenSearchService
 from api.views.v1.index_names import OpenSearchIndexNames
 from api.views.v1.opensearch_query_builder.moderation_events_query_builder \
     import ModerationEventsQueryBuilder
-from api.views.v1.opensearch_query_builder.opensearch_query_director \
-    import OpenSearchQueryDirector
+from api.views.v1.opensearch_query_builder.opensearch_query_director import \
+    OpenSearchQueryDirector
 from api.views.v1.utils import (
+    create_error_detail,
     handle_errors_decorator,
-    handle_path_error,
-    serialize_params,
+    serialize_params
 )
+
 
 log = logging.getLogger(__name__)
 
@@ -70,12 +72,7 @@ class ModerationEvents(ViewSet):
 
     @handle_errors_decorator
     def retrieve(self, _,  pk=None):
-        if not ModerationIdValidator.is_valid_uuid(pk):
-            return handle_path_error(
-                field="moderation_id",
-                message="Invalid UUID format.",
-                status_code=status.HTTP_400_BAD_REQUEST
-            )
+        self.__validate_uuid(pk)
 
         query_params = QueryDict('', mutable=True)
         query_params.update({'moderation_id': pk})
@@ -92,21 +89,9 @@ class ModerationEvents(ViewSet):
     def patch(self, request, pk=None):
         self.__validate_user_permissions(request)
 
-        if not ModerationIdValidator.is_valid_uuid(pk):
-            return handle_path_error(
-                field="moderation_id",
-                message="Invalid UUID format.",
-                status_code=status.HTTP_400_BAD_REQUEST
-            )
+        self.__validate_uuid(pk)
 
-        try:
-            event = ModerationEvent.objects.get(uuid=pk)
-        except ModerationEvent.DoesNotExist:
-            return handle_path_error(
-                field="moderation_id",
-                message="Moderation event not found.",
-                status_code=status.HTTP_404_NOT_FOUND
-            )
+        event = self.__fetch_moderation_event_by_uuid(pk)
 
         serializer = ModerationEventUpdateSerializer(
             event,
@@ -120,7 +105,7 @@ class ModerationEvents(ViewSet):
 
         return Response(
             {
-                "message": 'The request body contains '
+                "detail": 'The request body contains '
                 'invalid or missing fields.',
                 "error": [serializer.errors]
             },
@@ -131,28 +116,11 @@ class ModerationEvents(ViewSet):
     def add_production_location(self, request, moderation_id=None):
         self.__validate_user_permissions(request)
 
-        if not ModerationIdValidator.is_valid_uuid(moderation_id):
-            return handle_path_error(
-                field="moderation_id",
-                message="Invalid UUID format.",
-                status_code=status.HTTP_400_BAD_REQUEST,
-            )
+        self.__validate_uuid(moderation_id)
 
-        try:
-            event = ModerationEvent.objects.get(uuid=moderation_id)
-        except ModerationEvent.DoesNotExist:
-            return handle_path_error(
-                field="moderation_id",
-                message="Moderation event not found.",
-                status_code=status.HTTP_404_NOT_FOUND,
-            )
+        event = self.__fetch_moderation_event_by_uuid(moderation_id)
 
-        if event.status != ModerationEvent.Status.PENDING:
-            return handle_path_error(
-                field="status",
-                message="Moderation status must be PENDING.",
-                status_code=status.HTTP_410_GONE,
-            )
+        self.__validate_moderation_status(event.status)
 
         add_production_location = EventApprovalContext(
             AddProductionLocationStrategy(event)
@@ -174,13 +142,48 @@ class ModerationEvents(ViewSet):
                 detail="Only the Moderator can perform this action."
             )
 
+    def __validate_uuid(self, value):
+        if not ModerationIdValidator.is_valid_uuid(value):
+            self.__raise_invalid_uuid_error()
+
+    @staticmethod
+    def __raise_invalid_uuid_error():
+        raise ParseError(
+            create_error_detail(
+                field="moderation_id",
+                detail="Invalid UUID format."
+            )
+        )
+
+    def __fetch_moderation_event_by_uuid(self, uuid):
+        try:
+            return ModerationEvent.objects.get(uuid=uuid)
+        except ModerationEvent.DoesNotExist:
+            self.__raise_not_found_error()
+
+    @staticmethod
+    def __raise_not_found_error():
+        raise NotFound(
+            create_error_detail(
+                field="moderation_id",
+                detail="Moderation event not found."
+            )
+        )
+
+    def __validate_moderation_status(self, status):
+        if status != ModerationEvent.Status.PENDING:
+            self.__raise_status_not_pending_error()
+
+    @staticmethod
+    def __raise_status_not_pending_error():
+        raise GoneException(
+            create_error_detail(
+                field="status",
+                detail="The moderation event should be in PENDING status."
+            )
+        )
+
     @staticmethod
     def __handle_processing_error(error_message):
         log.error(f'[Moderation Event] Error: {str(error_message)}')
-        return Response(
-            {
-                "message": "An unexpected error occurred while "
-                "processing the request."
-            },
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        )
+        raise InternalServerErrorException()
