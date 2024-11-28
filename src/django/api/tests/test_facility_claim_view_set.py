ba18b6eaceaf0bd51fc38270808a6f2e5c2dca04
Inessa Druzhkova <inessadruzkova@inessas-mbp.home>
2024-11-28 12:07:42 +0100

    returned back changes in test_facility_claim_view_set

--- src/django/api/tests/test_facility_claim_view_set.py
+++ src/django/api/tests/test_facility_claim_view_set.py
@@ -188,7 +188,7 @@ class FacilityClaimViewSetTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(
-            response.data['message'],
+            response.data['detail'],
             'Authentication credentials were not provided.',
         )
 
@@ -206,7 +206,7 @@ class FacilityClaimViewSetTest(APITestCase):
         )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['message'], 'Message is required.')
+        self.assertEqual(response.data['detail'], 'Message is required.')
 
     def test_message_claimant_success(self):
         response = self._post_message_claimant(
