5e2cb737b01e65bff31b59e04b40b6e7cd0e4c23
mazur <mazursasha1990@gmail.com>
2024-11-28 20:22:23 +0200

    Modify EventApprovalStrategy and AddProductionLocationStrategy

--- src/django/api/moderation_event_actions/approval/add_production_location_strategy.py
+++ src/django/api/moderation_event_actions/approval/add_production_location_strategy.py
@@ -1,21 +1,14 @@
 import logging
 from datetime import datetime
+from typing import Dict, KeysView
 
-from typing import Dict, KeysView, Type, Union
-
-from django.contrib.gis.geos import Point
 from django.db import transaction
-from django.utils import timezone
 
-from api.constants import ProcessingAction
 from api.extended_fields import create_extendedfields_for_single_item
 from api.models.contributor.contributor import Contributor
-from api.models.extended_field import ExtendedField
 from api.models.facility.facility import Facility
 from api.models.facility.facility_list_item import FacilityListItem
 from api.models.facility.facility_list_item_temp import FacilityListItemTemp
-from api.models.facility.facility_match import FacilityMatch
-from api.models.facility.facility_match_temp import FacilityMatchTemp
 from api.models.moderation_event import ModerationEvent
 from api.models.source import Source
 from api.moderation_event_actions.approval.event_approval_strategy import (
@@ -25,6 +18,7 @@ from api.views.fields.create_nonstandard_fields import (
     create_nonstandard_fields,
 )
 from api.os_id import make_os_id
+
 
 log = logging.getLogger(__name__)
 
@@ -46,7 +40,7 @@ class AddProductionLocationStrategy(EventApprovalStrategy):
             contributor: Contributor = self.__event.contributor
             log.info(f'[Moderation Event] Contributor: {contributor}')
 
-            source: Source = self.__create_source(contributor)
+            source: Source = self._create_source(contributor)
             log.info(f'[Moderation Event] Source created. Id: {source.id}')
 
             header_row_keys: KeysView[str] = data["raw_json"].keys()
@@ -54,7 +48,7 @@ class AddProductionLocationStrategy(EventApprovalStrategy):
             log.info('[Moderation Event] Nonstandard fields created.')
 
             header_str: str = ','.join(header_row_keys)
-            item: FacilityListItem = self.__create_facility_list_item(
+            item: FacilityListItem = self._create_facility_list_item(
                 source, data, header_str, FacilityListItem.MATCHED
             )
             log.info(
@@ -65,7 +59,7 @@ class AddProductionLocationStrategy(EventApprovalStrategy):
             create_extendedfields_for_single_item(item, data["fields"])
             log.info('[Moderation Event] Extended fields created.')
 
-            self.__set_geocoded_location(item, data, self.__event)
+            self._set_geocoded_location(item, data, self.__event)
             log.info('[Moderation Event] Geocoded location set.')
 
             facility_id = make_os_id(item.country_code)
@@ -74,7 +68,7 @@ class AddProductionLocationStrategy(EventApprovalStrategy):
             self.__create_new_facility(item, facility_id)
             log.info(f'[Moderation Event] Facility created. Id: {facility_id}')
 
-            self.__update_item_with_facility_id_and_processing_results(
+            self._update_item_with_facility_id_and_processing_results(
                 item, facility_id
             )
             log.info(
@@ -84,109 +78,26 @@ class AddProductionLocationStrategy(EventApprovalStrategy):
             FacilityListItemTemp.copy(item)
             log.info('[Moderation Event] FacilityListItemTemp created.')
 
-            self.__update_extended_fields(item)
+            self._update_extended_fields(item)
             log.info(
                 '[Moderation Event] Extended fields updated with facility ID.'
             )
 
-            self.__create_facility_match_temp(item)
+            self._create_facility_match_temp(
+                item, FacilityListItem.NEW_FACILITY
+            )
             log.info('[Moderation Event] FacilityMatchTemp created.')
 
-            self.__create_facility_match(item)
+            self._create_facility_match(item, FacilityListItem.NEW_FACILITY)
             log.info('[Moderation Event] FacilityMatch created.')
 
-            self.__update_event(self.__event, item)
+            self._update_event(self.__event, item)
             log.info(
                 '[Moderation Event] Status and os_id of Moderation Event '
                 'updated.'
             )
 
             return item
-
-    @staticmethod
-    def __create_source(contributor: Contributor) -> Source:
-        return Source.objects.create(
-            contributor=contributor,
-            source_type=Source.SINGLE,
-            is_public=True,
-            create=True,
-        )
-
-    @staticmethod
-    def __create_facility_list_item(
-        source: Source, data: Dict, header_str: str, status: str
-    ) -> FacilityListItem:
-        return FacilityListItem.objects.create(
-            source=source,
-            row_index=0,
-            raw_data=','.join(
-                f'"{value}"' for value in data["raw_json"].values()
-            ),
-            raw_json=data["raw_json"],
-            raw_header=header_str,
-            name=data["name"],
-            clean_name=data["clean_name"],
-            address=data["address"],
-            clean_address=data["clean_address"],
-            country_code=data["country_code"],
-            sector=data["sector"],
-            status=status,
-            processing_results=[
-                {
-                    'action': ProcessingAction.PARSE,
-                    'started_at': str(timezone.now()),
-                    'error': False,
-                    'finished_at': str(timezone.now()),
-                    'is_geocoded': False,
-                }
-            ],
-        )
-
-    def __set_geocoded_location(
-        self, item: FacilityListItem, data: Dict, event: ModerationEvent
-    ) -> None:
-        geocode_result = event.geocode_result
-        fields = data["fields"]
-
-        if geocode_result:
-            self.__apply_geocode_result(item, geocode_result)
-
-        else:
-            self.__apply_manual_location(item, fields)
-
-        item.save()
-
-    @staticmethod
-    def __apply_geocode_result(
-        item: FacilityListItem, geocode_result: Dict
-    ) -> None:
-        item.geocoded_point = Point(
-            geocode_result["longitude"], geocode_result["latitude"]
-        )
-        item.geocoded_address = geocode_result["geocoded_address"]
-        item.processing_results.append(
-            {
-                "action": ProcessingAction.GEOCODE,
-                "started_at": str(timezone.now()),
-                "error": False,
-                "skipped_geocoder": False,
-                "data": geocode_result["full_response"],
-                "finished_at": str(timezone.now()),
-            }
-        )
-
-    @staticmethod
-    def __apply_manual_location(item: FacilityListItem, fields: Dict) -> None:
-        item.geocoded_point = Point(fields["lng"], fields["lat"])
-        item.processing_results.append(
-            {
-                "action": ProcessingAction.GEOCODE,
-                "started_at": str(timezone.now()),
-                "error": False,
-                "skipped_geocoder": True,
-                "finished_at": str(timezone.now()),
-            }
-        )
 
     @staticmethod
     def __create_new_facility(
@@ -202,65 +113,3 @@ class AddProductionLocationStrategy(EventApprovalStrategy):
             created_at=datetime.now(),
             updated_at=datetime.now(),
         )
-
-    @staticmethod
-    def __update_item_with_facility_id_and_processing_results(
-        item: FacilityListItem, facility_id: str
-    ) -> None:
-        item.facility_id = facility_id
-        item.processing_results.append(
-            {
-                'action': ProcessingAction.MATCH,
-                'started_at': str(timezone.now()),
-                'error': False,
-                'finished_at': str(timezone.now()),
-            }
-        )
-        item.save()
-
-    @staticmethod
-    def __update_extended_fields(item: FacilityListItem) -> None:
-        extended_fields = ExtendedField.objects.filter(
-            facility_list_item=item.id
-        )
-        for field in extended_fields:
-            field.facility_id = item.facility_id
-            field.save()
-
-    def __create_facility_match_temp(
-        self, item: FacilityListItem
-    ) -> FacilityMatchTemp:
-        return self.__create_facility_match_record(
-            model=FacilityMatchTemp,
-            item=item,
-            status=FacilityMatchTemp.AUTOMATIC,
-        )
-
-    def __create_facility_match(self, item: FacilityListItem) -> FacilityMatch:
-        return self.__create_facility_match_record(
-            model=FacilityMatch,
-            item=item,
-            status=FacilityMatch.AUTOMATIC,
-        )
-
-    @staticmethod
-    def __create_facility_match_record(
-        model: Union[Type[FacilityMatchTemp], Type[FacilityMatch]],
-        item: FacilityListItem,
-        status: str,
-    ) -> Union[FacilityMatchTemp, FacilityMatch]:
-        return model.objects.create(
-            facility_id=item.facility_id,
-            confidence=1.0,
-            facility_list_item_id=item.id,
-            status=status,
-            results={"match_type": "moderation_event"},
-            created_at=timezone.now(),
-            updated_at=timezone.now(),
-        )
-
-    @staticmethod
-    def __update_event(event: ModerationEvent, item: FacilityListItem) -> None:
-        event.status = ModerationEvent.Status.APPROVED
-        event.os_id = item.facility_id
-        event.save()
--- src/django/api/moderation_event_actions/approval/event_approval_strategy.py
+++ src/django/api/moderation_event_actions/approval/event_approval_strategy.py
@@ -1,6 +1,17 @@
 from abc import ABC, abstractmethod
+from typing import Dict, Type, Union
 
+from django.utils import timezone
+from django.contrib.gis.geos import Point
+
+from api.constants import ProcessingAction
+from api.models.extended_field import ExtendedField
 from api.models.facility.facility_list_item import FacilityListItem
+from api.models.facility.facility_match_temp import FacilityMatchTemp
+from api.models.facility.facility_match import FacilityMatch
+from api.models.contributor.contributor import Contributor
+from api.models.moderation_event import ModerationEvent
+from api.models.source import Source
 
 
 class EventApprovalStrategy(ABC):
@@ -14,3 +25,155 @@ class EventApprovalStrategy(ABC):
         Abstract method to process a moderation event.
         '''
         pass
+
+    @staticmethod
+    def _create_source(contributor: Contributor) -> Source:
+        return Source.objects.create(
+            contributor=contributor,
+            source_type=Source.SINGLE,
+            is_public=True,
+            create=True,
+        )
+
+    @staticmethod
+    def _create_facility_list_item(
+        source: Source, data: Dict, header_str: str, status: str
+    ) -> FacilityListItem:
+        return FacilityListItem.objects.create(
+            source=source,
+            row_index=0,
+            raw_data=','.join(
+                f'"{value}"' for value in data["raw_json"].values()
+            ),
+            raw_json=data["raw_json"],
+            raw_header=header_str,
+            name=data["name"],
+            clean_name=data["clean_name"],
+            address=data["address"],
+            clean_address=data["clean_address"],
+            country_code=data["country_code"],
+            sector=data["sector"],
+            status=status,
+            processing_results=[
+                {
+                    'action': ProcessingAction.PARSE,
+                    'started_at': str(timezone.now()),
+                    'error': False,
+                    'finished_at': str(timezone.now()),
+                    'is_geocoded': False,
+                }
+            ],
+        )
+
+    def _set_geocoded_location(
+        self, item: FacilityListItem, data: Dict, event: ModerationEvent
+    ) -> None:
+        geocode_result = event.geocode_result
+        fields = data["fields"]
+
+        if geocode_result:
+            self.__apply_geocode_result(item, geocode_result)
+
+        else:
+            self.__apply_manual_location(item, fields)
+
+        item.save()
+
+    @staticmethod
+    def __apply_geocode_result(
+        item: FacilityListItem, geocode_result: Dict
+    ) -> None:
+        item.geocoded_point = Point(
+            geocode_result["longitude"], geocode_result["latitude"]
+        )
+        item.geocoded_address = geocode_result["geocoded_address"]
+        item.processing_results.append(
+            {
+                "action": ProcessingAction.GEOCODE,
+                "started_at": str(timezone.now()),
+                "error": False,
+                "skipped_geocoder": False,
+                "data": geocode_result["full_response"],
+                "finished_at": str(timezone.now()),
+            }
+        )
+
+    @staticmethod
+    def __apply_manual_location(item: FacilityListItem, fields: Dict) -> None:
+        item.geocoded_point = Point(fields["lng"], fields["lat"])
+        item.processing_results.append(
+            {
+                "action": ProcessingAction.GEOCODE,
+                "started_at": str(timezone.now()),
+                "error": False,
+                "skipped_geocoder": True,
+                "finished_at": str(timezone.now()),
+            }
+        )
+
+    @staticmethod
+    def _update_item_with_facility_id_and_processing_results(
+        item: FacilityListItem, facility_id: str
+    ) -> None:
+        item.facility_id = facility_id
+        item.processing_results.append(
+            {
+                'action': ProcessingAction.MATCH,
+                'started_at': str(timezone.now()),
+                'error': False,
+                'finished_at': str(timezone.now()),
+            }
+        )
+        item.save()
+
+    @staticmethod
+    def _update_extended_fields(item: FacilityListItem) -> None:
+        extended_fields = ExtendedField.objects.filter(
+            facility_list_item=item.id
+        )
+        for field in extended_fields:
+            field.facility_id = item.facility_id
+            field.save()
+
+    def _create_facility_match_temp(
+        self, item: FacilityListItem, match_type: str
+    ) -> FacilityMatchTemp:
+        return self.__create_facility_match_record(
+            model=FacilityMatchTemp,
+            item=item,
+            status=FacilityMatchTemp.AUTOMATIC,
+            match_type=match_type,
+        )
+
+    def _create_facility_match(
+            self, item: FacilityListItem, match_type: str
+    ) -> FacilityMatch:
+        return self.__create_facility_match_record(
+            model=FacilityMatch,
+            item=item,
+            status=FacilityMatch.AUTOMATIC,
+            match_type=match_type,
+        )
+
+    @staticmethod
+    def __create_facility_match_record(
+        model: Union[Type[FacilityMatchTemp], Type[FacilityMatch]],
+        item: FacilityListItem,
+        status: str,
+        match_type: str
+    ) -> Union[FacilityMatchTemp, FacilityMatch]:
+        return model.objects.create(
+            facility_id=item.facility_id,
+            confidence=1.0,
+            facility_list_item_id=item.id,
+            status=status,
+            results={"match_type": f"moderation_event - {match_type}"},
+            created_at=timezone.now(),
+            updated_at=timezone.now(),
+        )
+
+    @staticmethod
+    def _update_event(event: ModerationEvent, item: FacilityListItem) -> None:
+        event.status = ModerationEvent.Status.APPROVED
+        event.os_id = item.facility_id
+        event.save()
