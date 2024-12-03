b18d44823b9fd34d3eba9a3f3f61fabdd9d3f65c
Inna Kocherzhuk <innakocherzhuk@gmail.com>
2024-12-03 13:52:19 +0100

    [OSDEV-1453] Use the 'detail' keyword instead of 'message' in response objects (#431)
    [OSDEV-1453](https://opensupplyhub.atlassian.net/browse/OSDEV-1453) Use
    the detail keyword instead of message in response objects for endpoints
    in V1 specs.
    
    <img width="400" alt="Screenshot 2024-11-28 at 11 43 58"
    src="https://github.com/user-attachments/assets/9de4a0ab-9f69-4cb5-aa1b-f2f613007406">
    
    
    [OSDEV-1453]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-1453?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    <img width="400" alt="Screenshot 2024-11-28 at 13 49 44"
    src="https://github.com/user-attachments/assets/bdffc771-523b-443c-b148-d7a02e7c5f37">
    
    ---------
    
    Co-authored-by: Inessa Druzhkova <inessadruzkova@inessas-mbp.home>
    Co-authored-by: Vadim Kovalenko <90321493+VadimKovalenkoSNF@users.noreply.github.com>

--- src/django/api/views/v1/moderation_events.py
+++ src/django/api/views/v1/moderation_events.py
@@ -64,7 +64,7 @@ class ModerationEvents(ViewSet):
         if not ModerationIdValidator.is_valid_uuid(pk):
             return handle_path_error(
                 field="moderation_id",
-                message="Invalid UUID format.",
+                detail="Invalid UUID format.",
                 status_code=status.HTTP_400_BAD_REQUEST
             )
 
@@ -89,7 +89,7 @@ class ModerationEvents(ViewSet):
         if not ModerationIdValidator.is_valid_uuid(pk):
             return handle_path_error(
                 field="moderation_id",
-                message="Invalid UUID format.",
+                detail="Invalid UUID format.",
                 status_code=status.HTTP_400_BAD_REQUEST
             )
 
@@ -98,7 +98,7 @@ class ModerationEvents(ViewSet):
         except ModerationEvent.DoesNotExist:
             return handle_path_error(
                 field="moderation_id",
-                message="Moderation event not found.",
+                detail="Moderation event not found.",
                 status_code=status.HTTP_404_NOT_FOUND
             )
 
@@ -114,7 +114,7 @@ class ModerationEvents(ViewSet):
 
         return Response(
             {
-                "message": 'The request body contains '
+                "detail": 'The request body contains '
                 'invalid or missing fields.',
                 "error": [serializer.errors]
             },
