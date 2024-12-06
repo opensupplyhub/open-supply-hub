7dc417d27c154c622ad1a4a8308268ff1777c05c
roman-stolar <88504352+roman-stolar@users.noreply.github.com>
2024-12-06 15:33:51 +0200

    [OSDEV-1482] GET api/v1/moderation-events/{moderation_id} should return single response instead of array (#444)
    [OSDEV-1482](https://opensupplyhub.atlassian.net/browse/OSDEV-1482) GET
    api/v1/moderation-events/{moderation_id} should return single response
    instead of array
    
    - Updated response format and added handling for 404
    
    [OSDEV-1482]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-1482?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/tests/v1/test_moderation_events.py
+++ src/tests/v1/test_moderation_events.py
@@ -12,8 +12,7 @@ class ModerationEventsTest(BaseAPITest):
                 headers=self.basic_headers,
             )
 
-        result = response.json()
-        self.assertEqual(result['count'], 0)
+        self.assertEqual(response.status_code, 404)
 
         moderation_id = '1f35a90f-70a0-4c3e-8e06-2ed8e1fc6800'
         response = requests.get(
@@ -22,8 +21,7 @@ class ModerationEventsTest(BaseAPITest):
             )
 
         result = response.json()
-        self.assertEqual(result['count'], 1)
-        self.assertEqual(result['data'][0]['moderation_id'], moderation_id)
+        self.assertEqual(result['moderation_id'], moderation_id)
 
     def test_moderation_events_status(self):
         response = requests.get(
