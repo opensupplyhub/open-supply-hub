181a5b55a834a9ec40480fcb977ca086dcd6a5b6
VadimKovalenko <vadim.kovalenko@speedandfunction.com>
2024-12-26 12:12:05 +0400

    Upd made token file

--- src/django/api/management/commands/make_token.py
+++ src/django/api/management/commands/make_token.py
@@ -4,13 +4,21 @@ from django.core.management import call_command
 
 class Command(BaseCommand):
     help = ('Usage: Create an API token for the user during the database '
-            'reset.')
+            'reset, and set admin rights.')
 
     def handle(self, *args, **options):
-        call_command('shell',
-                     '-c',
-                     ("from rest_framework.authtoken.models import Token; "
-                      "from api.models import User; token = "
-                      "Token.objects.create(user=User.objects.get(id=2),"
-                      "key='1d18b962d6f976b0b7e8fcf9fcc39b56cf278051'); "
-                      "print('Token for c2@example.com'); print(token)"))
+        call_command(
+            'shell',
+            '-c',
+            (
+                "from rest_framework.authtoken.models import Token;"
+                "from api.models import User;"
+                "user = User.objects.get(id=2);"
+                "user.is_staff = True;"
+                "user.is_superuser = True;"
+                "user.save();"
+                "token = Token.objects.create(user=user,"
+                "key='1d18b962d6f976b0b7e8fcf9fcc39b56cf278051');"
+                "print(f'Token for {user.email}: {token.key}')"
+            )
+        )
