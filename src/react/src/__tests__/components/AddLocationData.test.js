99ea2f635a704485afd9000395c220dd4231439e
Roman Stolar <roman.stolar@speedandfunction.com>
2024-12-27 16:47:29 +0200

    Refactor + added UI component rendering tests

---
+++ src/react/src/__tests__/components/AddLocationData.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { MemoryRouter } from 'react-router-dom';
+import AddLocationData from '../../components/AddLocationData';
+import { render } from '@testing-library/react';
+
+const mockStore = configureMockStore();
+
+describe('AddLocationData component', () => {
+    const mockAuthorizedState = {
+        auth: {
+            user: { user: { isAnon: false } },
+            session: { fetching: false },
+        },
+    };
+
+    const mockNotAuthorizedState = {
+        auth: {
+            user: { user: { isAnon: true } },
+            session: { fetching: false },
+        },
+    };
+
+    const renderComponent = (state = {}) => {
+        const store = mockStore(state);
+        const theme = createMuiTheme({
+            palette: {
+                action: { main: '#000', dark: '#333' }, // Define a valid palette
+                getContrastText: jest.fn(() => '#fff'), // Mock the method to return a contrast color
+            },
+        });
+        return render(
+            <Provider store={store}>
+                <MuiThemeProvider theme={theme}>
+                    <MemoryRouter>
+                        <AddLocationData />
+                    </MemoryRouter>
+                </MuiThemeProvider>
+            </Provider>
+        );
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('renders with the authorized user', () => {
+        const { getByText } = renderComponent(mockAuthorizedState);
+        expect(getByText('Add production location data to OS Hub')).toBeInTheDocument();
+    });
+
+    it('renders with the not authorized user', () => {
+        const { getByText } = renderComponent(mockNotAuthorizedState);
+        expect(getByText('Log in to contribute to Open Supply Hub')).toBeInTheDocument();
+    });
+});
