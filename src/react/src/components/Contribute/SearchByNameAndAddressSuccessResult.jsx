b0798168766b30c979cb775f085b5f330a422e14
mazur <mazursasha1990@gmail.com>
2024-12-30 15:40:08 +0200

    Move makeSearchByNameAndAddressSuccessResultStyles to the styles.js

--- src/react/src/components/Contribute/SearchByNameAndAddressSuccessResult.jsx
+++ src/react/src/components/Contribute/SearchByNameAndAddressSuccessResult.jsx
@@ -3,106 +3,10 @@ import { arrayOf, func, number, object } from 'prop-types';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import COLOURS from '../../util/COLOURS';
+import { makeSearchByNameAndAddressSuccessResultStyles } from '../../util/styles';
 import { productionLocationPropType } from '../../util/propTypes';
 import ConfirmNotFoundLocationDialog from './ConfirmNotFoundLocationDialog';
 import ProductionLocationDetails from './ProductionLocationDetails';
-
-const makeSearchByNameAndAddressSuccessResultStyles = theme =>
-    Object.freeze({
-        searchResultsContainerStyles: Object.freeze({
-            padding: '24px 5% 0 5%',
-        }),
-        titleStyles: Object.freeze({
-            fontWeight: theme.typography.fontWeightExtraBold,
-            fontSize: '56px',
-            lineHeight: '60px',
-            marginBottom: '16px',
-        }),
-        descriptionStyles: Object.freeze({
-            fontSize: '18px',
-            lineHeight: '21px',
-            marginBottom: '32px',
-            maxWidth: '656px',
-        }),
-        subTitleStyles: Object.freeze({
-            fontWeight: theme.typography.fontWeightSemiBoldPlus,
-            fontSize: '36px',
-            lineHeight: '44px',
-            margin: '32px 0 24px 0',
-        }),
-        resultsInfoContainerStyles: Object.freeze({
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginBottom: '24px',
-        }),
-        resultsInfoStyles: Object.freeze({
-            fontSize: '16px',
-            lineHeight: '20px',
-            fontWeight: theme.typography.fontWeightBold,
-        }),
-        resultsSortStyles: Object.freeze({
-            fontSize: '16px',
-            lineHeight: '20px',
-        }),
-        resultContainer: Object.freeze({
-            display: 'flex',
-            justifyContent: 'space-between',
-            gap: '10px',
-            padding: '12px',
-            borderBottom: `1px solid ${COLOURS.ACCENT_GREY}`,
-            '&:hover': {
-                backgroundColor: COLOURS.LIGHT_GREY,
-            },
-            '&:last-child': {
-                marginBottom: '32px',
-            },
-        }),
-        buttonBaseStyles: Object.freeze({
-            height: '48px',
-            textTransform: 'none',
-            backgroundColor: theme.palette.action.main,
-            color: theme.palette.common.black,
-            '&:hover': {
-                backgroundColor: theme.palette.action.dark,
-            },
-        }),
-        selectButtonStyles: Object.freeze({
-            minWidth: '100px',
-        }),
-        selectButtonLabelStyles: Object.freeze({
-            fontSize: '16px',
-            lineHeight: '20px',
-            fontWeight: theme.typography.fontWeightExtraBold,
-        }),
-        notFoundButtonOuterContainerStyles: Object.freeze({
-            position: 'sticky',
-            bottom: -108,
-            height: '188px',
-        }),
-        notFoundButtonContainerStyles: Object.freeze({
-            display: 'flex',
-            alignItems: 'center',
-            boxShadow: '0px -2px 12px 0px #00000014',
-            position: 'sticky',
-            bottom: 0,
-            padding: '0 5%',
-            height: '80px',
-            backgroundColor: '#fff',
-        }),
-        notFoundButtonContainerScrolledStyles: Object.freeze({
-            boxShadow: 'none',
-        }),
-        notFoundButtonStyles: Object.freeze({
-            width: '275px',
-        }),
-        notFoundButtonLabelStyles: Object.freeze({
-            fontSize: '18px',
-            lineHeight: '22px',
-            fontWeight: theme.typography.fontWeightExtraBold,
-        }),
-    });
 
 const SearchByNameAndAddressSuccessResult = ({
     productionLocationsCount,
