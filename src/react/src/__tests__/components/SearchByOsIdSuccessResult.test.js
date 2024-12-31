9ce2fa3c894f2f6a6a32ca37cef2d7013be81dca
mazur <mazursasha1990@gmail.com>
2024-12-31 18:12:24 +0200

    Fix formatting for SearchByOsIdSuccessResult component

--- src/react/src/__tests__/components/SearchByOsIdSuccessResult.test.js
+++ src/react/src/__tests__/components/SearchByOsIdSuccessResult.test.js
@@ -6,26 +6,25 @@ import renderWithProviders from '../../util/testUtils/renderWithProviders';
 jest.mock('../../components/Contribute/SearchByOsIdResultActions', () => ({
     __esModule: true,
     default: ({ defaultButtonLabel, defaultButtonAction, secondaryButtonLabel, secondaryButtonAction }) => (
-      <div>
-        <button onClick={defaultButtonAction}>{defaultButtonLabel}</button>
-        <button onClick={secondaryButtonAction}>{secondaryButtonLabel}</button>
-      </div>
+        <div>
+            <button onClick={defaultButtonAction}>{defaultButtonLabel}</button>
+            <button onClick={secondaryButtonAction}>{secondaryButtonLabel}</button>
+        </div>
     ),
-  }));
-  
-  jest.mock('../../components/Contribute/ProductionLocationDetails', () => ({
+}));
+
+jest.mock('../../components/Contribute/ProductionLocationDetails', () => ({
     __esModule: true,
     default: ({ osId, name, address, countryName, historicalOsIds }) => (
-      <div>
-        <div>{`OS ID: ${osId}`}</div>
-        <div>{`Name: ${name}`}</div>
-        <div>{`Address: ${address}`}</div>
-        <div>{`Country: ${countryName}`}</div>
-        {historicalOsIds &&
-          historicalOsIds.map((id, index) => <div key={index}>{`Historical OS ID: ${id}`}</div>)}
-      </div>
+        <div>
+            <div>{`OS ID: ${osId}`}</div>
+            <div>{`Name: ${name}`}</div>
+            <div>{`Address: ${address}`}</div>
+            <div>{`Country: ${countryName}`}</div>
+            {historicalOsIds && historicalOsIds.map((id, index) => <div key={index}>{`Historical OS ID: ${id}`}</div>)}
+        </div>
     ),
-  }));
+}));
 
 describe('SearchByOsIdSuccessResult component', () => {
     const defaultProps = {
@@ -37,17 +36,16 @@ describe('SearchByOsIdSuccessResult component', () => {
                 alpha_2: 'US',
                 alpha_3: 'USA',
                 name: 'United States',
-                numeric: '840', 
+                numeric: '840',
             },
             historical_os_id: ['US2020053ZH1RY4', 'US2020053ZH1RY5'],
-
         },
         handleBackToSearchByNameAddress: jest.fn(),
     };
 
     test('renders the production location details and actions correctly', () => {
-        const { getByText, getByRole } = renderWithProviders(<SearchByOsIdSuccessResult {...defaultProps}/>);
-    
+        const { getByText, getByRole } = renderWithProviders(<SearchByOsIdSuccessResult {...defaultProps} />);
+
         expect(getByText('Is this your production location?')).toBeInTheDocument();
         expect(getByText('OS ID: US2021250D1DTN7')).toBeInTheDocument();
         expect(getByText('Name: Production Location Name')).toBeInTheDocument();
@@ -57,10 +55,10 @@ describe('SearchByOsIdSuccessResult component', () => {
         expect(getByText('Historical OS ID: US2020053ZH1RY5')).toBeInTheDocument();
         expect(getByRole('button', { name: 'No, search by name and address' })).toBeInTheDocument();
         expect(getByRole('button', { name: 'Yes, add data and claim' })).toBeInTheDocument();
-      });
-    
+    });
+
     test('calls handleBackToSearchByNameAddress when the default button is clicked', () => {
-        const { getByRole } = renderWithProviders(<SearchByOsIdSuccessResult {...defaultProps}/>);
+        const { getByRole } = renderWithProviders(<SearchByOsIdSuccessResult {...defaultProps} />);
 
         const backButton = getByRole('button', {
             name: 'No, search by name and address',
