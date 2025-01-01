721c4d98777196411a114f3537185473323e63d1
Stepan Protsak <stephan.protsack@gmail.com>
2025-01-01 12:25:23 +0100

    Refactor of initial script

--- src/django/api/management/commands/export_csv.py
+++ src/django/api/management/commands/export_csv.py
@@ -1,83 +1,88 @@
 import csv
+import logging
 from datetime import datetime
-
 from django.core.management.base import BaseCommand
-
 from api.serializers.facility.facility_download_serializer import (
     FacilityDownloadSerializer
 )
 from api.models.facility.facility_index import FacilityIndex
 
 serializer = FacilityDownloadSerializer()
+logger = logging.getLogger(__name__)
+
+def create_cvs_writer(file):
+    writer = csv.writer(file)
+
+    headers = serializer.get_headers()
+    writer.writerow(headers)
+
+    logger.info("Wrote headers to file")
+
+    return writer
 
 
-def count_with_percent(i: int, count: int) -> str:
-    return f"{i} out of {count} ({i / count * 100})%"
+def write_facilities(writer, facilities):
+    for facility in facilities:
+        row = serializer.get_row(facility)
+        writer.writerow(row)
 
+def get_facilities(id=None, limit=50000):
+    facility_objects = FacilityIndex.objects
 
-def print_with_time(text: str) -> None:
-    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- {text}")
+    if id is not None:
+        facility_objects = facility_objects.filter(id__gt=id)
 
-
-def create_chunk(list, size, index):
-    return list[index:index + size]
-
-
-def create_with_headers():
-    now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    filename = f"./facilities-command-{now}.csv"
-    with open(filename, 'w+') as f:
-        writer = csv.writer(f)
-        print_with_time("Created file")
-
-        headers = serializer.get_headers()
-        writer.writerow(headers)
-        print_with_time("Written headers")
-
-    return filename
-
-
-def modify(filename, chunk):
-    with open(filename, 'a') as f:
-        writer = csv.writer(f)
-        for facility in chunk:
-            row = serializer.get_row(facility)
-            writer.writerow(row)
-
+    return facility_objects.order_by("id")[:limit]
 
 class Command(BaseCommand):
     help = 'Export all facilities to a CSV file.'
 
     def add_arguments(self, parser):
-        return
+        parser.add_argument(
+            '--limit',
+            type=int,
+            default=50000,
+            help='Limit the number of facilities to fetch in each iteration'
+        )
+
 
     def handle(self, *args, **options):
-        facilities = FacilityIndex.objects.order_by("id").all()
-        size_of_chunk = 100000
-        count = facilities.count()
-        print_with_time(f"Amount of indexed facilities: {count}")
+        logger.info("Starting the export process!")
 
-        filename = create_with_headers()
+        limit = options['limit']
+        logger.info(f"Limit set to: {limit}")
 
-        index = 0
-        print_with_time("Fetching data from DB")
-        while count > index:
-            if index == 0:
-                print_with_time(
-                    f"Started exporting {count} facilities"
+        now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        filename = f"./facilities-command-{now}.csv"
+
+        with open(filename, 'w+') as file:
+            logger.info(f"Opened file for writing: {filename}")
+
+            writer = create_cvs_writer(file=file)
+            last_id = None
+            facilities = None
+            total_facilities = 0
+
+            while True:
+                logger.info(f"New loop iteration, ID: {last_id}")
+                facilities = get_facilities(
+                    id=last_id,
+                    limit=limit,
+                )
+                total_facilities += len(facilities)
+
+                if len(facilities) == 0:
+                    logger.info("No more facilities, breaking the loop!")
+                    break
+
+                write_facilities(
+                    writer=writer,
+                    facilities=facilities,
                 )
 
-            facility_chunk = create_chunk(facilities, size_of_chunk, index)
-            index = index + size_of_chunk
-            modify(filename, facility_chunk)
+                last_id = facilities[len(facilities) - 1].id
+                logger.info(f"Number of processed facilities: {len(facilities)}")
+                logger.info(f"End of iteration, ID: {last_id}")
 
-            if index > count:
-                index = count
-            if index % size_of_chunk == 0:
-                print_with_time(
-                    f"Wrote {count_with_percent(index, count)}"
-                )
-
-        print_with_time(
-            f"Finished {count_with_percent(index,count)}"
-        )
+        logger.info("Finished the export process!")
+        logger.info(f"Total number of facilities: {total_facilities}")
