ded0e08a49da3c811c92548ee2fe3a27ed3a8689
mazur <mazursasha1990@gmail.com>
2025-01-08 21:09:50 +0200

    Change type of variable google_service_account_creds to any

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -670,19 +670,7 @@ variable "export_csv_schedule_expression" {
 }
 
 variable "google_service_account_creds" {
-  type        = object({
-    type                        = string
-    project_id                  = string
-    private_key_id              = string
-    private_key                 = string
-    client_email                = string
-    client_id                   = string
-    auth_uri                    = string
-    token_uri                   = string
-    auth_provider_x509_cert_url = string
-    client_x509_cert_url        = string
-    universe_domain             = string
-  })
+  type        = any
   description = "Credentials for the Google service account"
 }
 
