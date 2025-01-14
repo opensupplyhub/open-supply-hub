e3fb4189d72266ead1c9fad6a1c0a307f55e4b81
mazur <mazursasha1990@gmail.com>
2025-01-14 15:37:30 +0200

    Remove commented code and prepare it for testing on the Test env

--- deployment/terraform/batch.tf
+++ deployment/terraform/batch.tf
@@ -280,16 +280,15 @@ resource "aws_batch_job_definition" "export_csv" {
   }
 }
 
-# This section creates schedules for the export_csv job
 resource "aws_cloudwatch_event_rule" "export_csv_schedule" {
-  count               = var.environment == "Development" ? 1 : 0
-  name                = "cwOpenSupplyHubExportCsvScheduler"
+  count               = var.environment == "Test" ? 1 : 0
+  name                = "schedule-export-csv"
   description         = "Runs the export_csv job on a schedule"
   schedule_expression = var.export_csv_schedule_expression
 }
 
 resource "aws_cloudwatch_event_target" "export_csv" {
-  count     = var.environment == "Development" ? 1 : 0
+  count     = var.environment == "Test" ? 1 : 0
   rule      = aws_cloudwatch_event_rule.export_csv_schedule[count.index].name
   arn       = aws_batch_job_queue.export_csv.arn
   role_arn  = aws_iam_role.cloudwatch_events_batch_role.arn
@@ -347,105 +346,6 @@ data "aws_iam_policy_document" "cloudwatch_events_batch_policy" {
   }
 }
 
-
-# # Schedule the export_csv job
-# resource "aws_iam_role" "export_csv_scheduler_role" {
-#   name = "evb${local.short}ExportCsvSchedulerExecutionRole"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "scheduler.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
-
-# resource "aws_cloudwatch_event_rule" "export_csv_schedule" {
-#   name                = "cwOpenSupplyHubExportCsvScheduler"
-#   schedule_expression = var.export_csv_schedule_expression
-# }
-
-# resource "aws_cloudwatch_event_target" "export_csv" {
-#   rule      = aws_cloudwatch_event_rule.export_csv_schedule.name
-#   arn       = aws_batch_job_queue.export_csv.arn
-#   role_arn  = aws_iam_role.export_csv_scheduler_role.arn  # Must allow "events:InvokeBatchJobQueue"
-
-#   batch_target {
-#     job_definition = aws_batch_job_definition.export_csv.arn
-#     job_name       = "export_csv"
-#   }
-# }
-
-# resource "aws_iam_role_policy" "export_csv_scheduler_policy" {
-#   name   = "evb${local.short}ExportCsvSchedulerPolicy"
-#   role   = aws_iam_role.export_csv_scheduler_role.id
-#   policy = data.aws_iam_policy_document.export_csv_scheduler_policy.json
-# }
-
-# data "aws_iam_policy_document" "export_csv_scheduler_policy" {
-#   statement {
-#     actions = [
-#       "batch:SubmitJob"
-#     ]
-#     resources = [
-#       aws_batch_job_queue.export_csv.arn,
-#       aws_batch_job_definition.export_csv.arn
-#     ]
-#   }
-# }
-
-# resource "aws_cloudwatch_event_permission" "allow_eventbridge" {
-#   principal = "*"
-#   # The statement_id must be unique within the policy
-#   statement_id  = "AllowEventBridgeToInvokeBatch"
-#   action        = "events:PutTargets"
-#   event_bus_name = "default"
-# }
-
-# end of schedule the export_csv job
-
-# resource "aws_iam_role" "export_csv_scheduler_role" {
-#   name = "evb${local.short}ExportCsvSchedulerExecutionRole"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "scheduler.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
-
-# resource "aws_scheduler_schedule" "export_csv" {
-#   name                = "evb${local.short}ExportCsvScheduler"
-#   group_name          = "evb${local.short}ExportCsvScheduler"
-#   schedule_expression = var.export_csv_schedule_expression
-
-#   flexible_time_window {
-#     maximum_window_in_minutes = 60
-#     mode                      = "FLEXIBLE"
-#   }
-
-#   target {
-#     arn      = aws_batch_job_queue.export_csv.arn
-#     role_arn = aws_iam_role.export_csv_scheduler_role.arn
-#   }
-# }
-
-#
-# CloudWatch Resources
-#
 resource "aws_cloudwatch_log_group" "batch" {
   name              = "log${local.short}Batch"
   retention_in_days = 0
