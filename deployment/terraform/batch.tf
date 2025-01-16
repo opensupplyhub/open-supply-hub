663d823a62e8eb5aafeb45fd7fefc72ac857d22c
mazur <mazursasha1990@gmail.com>
2025-01-16 13:26:19 +0200

    Add scheduling for all environments

--- deployment/terraform/batch.tf
+++ deployment/terraform/batch.tf
@@ -281,17 +281,13 @@ resource "aws_batch_job_definition" "export_csv" {
 }
 
 resource "aws_cloudwatch_event_rule" "export_csv_schedule" {
-  # count               = var.environment == "Production" ? 1 : 0
-  count               = var.environment == "Development" ? 1 : 0
-  name                = "schedule-export-csv"
+  name                = "rule${local.short}ExportCsvSchedule"
   description         = "Runs the export_csv job on a schedule"
   schedule_expression = var.export_csv_schedule_expression
 }
 
 resource "aws_cloudwatch_event_target" "export_csv" {
-  # count     = var.environment == "Production" ? 1 : 0
-  count     = var.environment == "Development" ? 1 : 0
-  rule      = aws_cloudwatch_event_rule.export_csv_schedule[count.index].name
+  rule      = aws_cloudwatch_event_rule.export_csv_schedule.name
   arn       = aws_batch_job_queue.export_csv.arn
   role_arn  = aws_iam_role.cloudwatch_events_batch_role.arn
 
