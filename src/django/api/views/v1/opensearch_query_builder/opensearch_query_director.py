5a9790a2dc9689fd2b50e921ccd5358bf1846e5d
mazur <mazursasha1990@gmail.com>
2025-01-29 10:36:00 +0200

    Fix __process_multi_match

--- src/django/api/views/v1/opensearch_query_builder/opensearch_query_director.py
+++ src/django/api/views/v1/opensearch_query_builder/opensearch_query_director.py
@@ -121,7 +121,7 @@ class OpenSearchQueryDirector:
         multi_match_query = query_params.get(V1_PARAMETERS_LIST.QUERY)
 
         if multi_match_query and hasattr(self.__builder, 'add_multi_match'):
-            self.add_multi_match(multi_match_query)
+            self.__builder.add_multi_match(multi_match_query)
 
     def __process_aggregation(self, query_params):
         aggregation = query_params.get(V1_PARAMETERS_LIST.AGGREGATION)
