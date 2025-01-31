cd8ee371e45a48bb341ab4941724047838022e5e
Vadim Kovalenko <90321493+VadimKovalenkoSNF@users.noreply.github.com>
2025-01-31 18:01:02 +0400

    [OSDEV-1365]  (quickfix) Fix validation in utility function (#496)
    Follow-up fix for
    [OSDEV-1365](https://opensupplyhub.atlassian.net/browse/OSDEV-1365)
    
    [OSDEV-1365]:
    https://opensupplyhub.atlassian.net/browse/OSDEV-1365?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/react/src/util/util.js
+++ src/react/src/util/util.js
@@ -418,7 +418,7 @@ export const mapParamToReactSelectOption = param => {
 };
 
 export const updateStateFromData = (obj, dataKey, setter) => {
-    if (obj[dataKey]?.length > 0) {
+    if (isArray(obj[dataKey]) && obj[dataKey].length > 0) {
         const transformedData = obj[dataKey].map(mapParamToReactSelectOption);
         setter(transformedData);
     }
