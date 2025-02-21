359da3d45e80731d6d3b102532da755066f12e57
Inessa Druzhkova <inessadruzkova@inessas-mbp.home>
2025-02-21 09:55:06 +0100

    removed extra space

--- src/react/src/__tests__/components/SearchByOsIdResult.test.js
+++ src/react/src/__tests__/components/SearchByOsIdResult.test.js
@@ -42,7 +42,6 @@ describe('SearchByOsIdResult component', () => {
         jest.clearAllMocks();
     });
 
-
     it('renders without crashing', () => {
         const {getByText} = renderComponent();
 
