b02f74261fbf78f0a7a5aa320e4bf3a499d313f8
Volodymyr Popov <vladimir.p@opsworks.co>
2025-02-25 16:39:38 +0200

    removed commented code

--- deployment/terraform/lambda.tf
+++ deployment/terraform/lambda.tf
@@ -89,11 +89,9 @@ resource "aws_lambda_function" "alert_sfn_failures" {
   }
 }
 
-
 #
 # Redirect to S3 origin
 #
-
 data "aws_iam_policy_document" "lambda_edge_redirect_to_s3_origin_assume_role" {
   statement {
     effect = "Allow"
@@ -130,31 +128,6 @@ resource "aws_iam_role_policy" "lambda_edge_redirect_to_s3_origin_exec_role" {
   role   = aws_iam_role.lambda_edge_redirect_to_s3_origin.id
   policy = data.aws_iam_policy_document.lambda_edge_redirect_to_s3_origin_exec_role_policy.json
 }
-
-# data "archive_file" "lambda_edge_redirect_to_s3_origin" {
-#   type        = "zip"
-#   source_file = "lambda-functions/redirect_to_s3_origin/index.mjs"
-#   output_path = "/tmp/redirect_to_s3_origin_${timestamp()}.zip"
-# }
-
-# data "archive_file" "lambda_edge_redirect_to_s3_origin" {
-#   type        = "zip"
-#   output_path = "/tmp/redirect_to_s3_origin.zip"
-#   source {
-#     content  = <<EOF
-# 'use strict';
-#
-# export const handler = async (event) => {
-#     const request = event.Records[0].cf.request;
-#     if (request.uri === '/') {
-#         request.uri = '/index.html';
-#     }
-#     return request;
-# }
-# EOF
-#     filename = "index.mjs"
-#   }
-# }
 
 resource "aws_lambda_function" "redirect_to_s3_origin" {
   filename         = "lambda-functions/redirect_to_s3_origin/redirect_to_s3_origin.zip"
