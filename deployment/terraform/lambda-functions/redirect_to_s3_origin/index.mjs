75120d5db643a2937d3ec58815ac807341893de5
Volodymyr Popov <vladimir.p@opsworks.co>
2025-02-25 14:05:20 +0200

    debug

--- deployment/terraform/lambda-functions/redirect_to_s3_origin/index.mjs
+++ deployment/terraform/lambda-functions/redirect_to_s3_origin/index.mjs
@@ -2,8 +2,10 @@
 
 export const handler = async (event) => {
     const request = event.Records[0].cf.request;
+
     if (!request.uri.includes('.')) {
         request.uri = '/index.html';
     }
+
     return request;
 }
