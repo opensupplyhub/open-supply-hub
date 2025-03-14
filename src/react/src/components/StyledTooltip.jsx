898f5f82b31206f767f5a929c184cc0d0da53d26
mazur <mazursasha1990@gmail.com>
2025-03-14 22:04:08 +0200

    Modify StyledTooltip component

--- src/react/src/components/StyledTooltip.jsx
+++ src/react/src/components/StyledTooltip.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { node, object } from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 
 import { makeStyledTooltipStyles } from '../util/styles';
 
-const StyledTooltip = props => <Tooltip {...props} />;
+const StyledTooltip = ({ classes, children, ...props }) => (
+    <Tooltip classes={{ tooltip: classes.tooltip }} {...props}>
+        {children}
+    </Tooltip>
+);
+
+StyledTooltip.propTypes = {
+    classes: object.isRequired,
+    children: node.isRequired,
+};
 
 export default withStyles(makeStyledTooltipStyles)(StyledTooltip);
