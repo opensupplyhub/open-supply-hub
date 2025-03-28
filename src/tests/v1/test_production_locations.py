e995787950d25e047cc0502e5ab9180db70275ff
VadimKovalenko <vadim.kovalenko@speedandfunction.com>
2025-03-28 11:46:35 +0400

    Add integration test for more than 100 geo_polygon parameters

--- src/tests/v1/test_production_locations.py
+++ src/tests/v1/test_production_locations.py
@@ -225,3 +225,59 @@ class ProductionLocationsTest(BaseAPITest):
         self.assertEqual(len(result['data']), 1)
         self.assertEqual(result['data'][0]['os_id'], "GL202309INSIDE")
 
+    def test_production_locations_with_more_than_hundred_points(self):
+        query = (
+            "?geo_polygon=71.0,-25.0&geo_polygon=70.5,-22.0&geo_polygon=70.0,-19.0"
+            "&geo_polygon=69.5,-16.0&geo_polygon=69.0,-13.0&geo_polygon=68.5,-10.0"
+            "&geo_polygon=68.0,-7.0&geo_polygon=67.5,-4.0&geo_polygon=67.0,-1.0"
+            "&geo_polygon=66.5,2.0&geo_polygon=66.0,5.0&geo_polygon=65.5,8.0"
+            "&geo_polygon=65.0,11.0&geo_polygon=64.5,14.0&geo_polygon=64.0,17.0"
+            "&geo_polygon=63.5,20.0&geo_polygon=63.0,23.0&geo_polygon=62.5,26.0"
+            "&geo_polygon=62.0,29.0&geo_polygon=61.5,32.0&geo_polygon=61.0,35.0"
+            "&geo_polygon=60.5,38.0&geo_polygon=60.0,41.0&geo_polygon=59.5,44.0"
+            "&geo_polygon=59.0,47.0&geo_polygon=58.5,50.0&geo_polygon=58.0,53.0"
+            "&geo_polygon=57.5,56.0&geo_polygon=57.0,59.0&geo_polygon=56.5,62.0"
+            "&geo_polygon=56.0,65.0&geo_polygon=55.5,68.0&geo_polygon=55.0,71.0"
+            "&geo_polygon=54.5,74.0&geo_polygon=54.0,77.0&geo_polygon=53.5,80.0"
+            "&geo_polygon=53.0,83.0&geo_polygon=52.5,86.0&geo_polygon=52.0,89.0"
+            "&geo_polygon=51.5,92.0&geo_polygon=51.0,95.0&geo_polygon=50.5,98.0"
+            "&geo_polygon=50.0,101.0&geo_polygon=49.5,104.0&geo_polygon=49.0,107.0"
+            "&geo_polygon=48.5,110.0&geo_polygon=48.0,113.0&geo_polygon=47.5,116.0"
+            "&geo_polygon=47.0,119.0&geo_polygon=46.5,122.0&geo_polygon=46.0,125.0"
+            "&geo_polygon=45.5,128.0&geo_polygon=45.0,131.0&geo_polygon=44.5,134.0"
+            "&geo_polygon=44.0,137.0&geo_polygon=43.5,140.0&geo_polygon=43.0,143.0"
+            "&geo_polygon=42.5,146.0&geo_polygon=42.0,149.0&geo_polygon=41.5,152.0"
+            "&geo_polygon=41.0,155.0&geo_polygon=40.5,158.0&geo_polygon=40.0,161.0"
+            "&geo_polygon=39.5,164.0&geo_polygon=39.0,167.0&geo_polygon=38.5,170.0"
+            "&geo_polygon=38.0,173.0&geo_polygon=37.5,176.0&geo_polygon=37.0,179.0"
+            "&geo_polygon=36.5,-178.0&geo_polygon=36.0,-175.0&geo_polygon=35.5,-172.0"
+            "&geo_polygon=35.0,-169.0&geo_polygon=34.5,-166.0&geo_polygon=34.0,-163.0"
+            "&geo_polygon=33.5,-160.0&geo_polygon=33.0,-157.0&geo_polygon=32.5,-154.0"
+            "&geo_polygon=32.0,-151.0&geo_polygon=31.5,-148.0&geo_polygon=31.0,-145.0"
+            "&geo_polygon=30.5,-142.0&geo_polygon=30.0,-139.0&geo_polygon=29.5,-136.0"
+            "&geo_polygon=29.0,-133.0&geo_polygon=28.5,-130.0&geo_polygon=28.0,-127.0"
+            "&geo_polygon=27.5,-124.0&geo_polygon=27.0,-121.0&geo_polygon=26.5,-118.0"
+            "&geo_polygon=26.0,-115.0&geo_polygon=25.5,-112.0&geo_polygon=25.0,-109.0"
+            "&geo_polygon=24.5,-106.0&geo_polygon=24.0,-103.0&geo_polygon=23.5,-100.0"
+            "&geo_polygon=23.0,-97.0&geo_polygon=22.5,-94.0&geo_polygon=22.0,-91.0"
+            "&geo_polygon=21.5,-88.0&geo_polygon=21.0,-85.0&geo_polygon=20.5,-82.0"
+            "&geo_polygon=20.0,-79.0&geo_polygon=19.5,-76.0&geo_polygon=19.0,-73.0"
+            "&geo_polygon=18.5,-70.0&geo_polygon=18.0,-67.0&geo_polygon=17.5,-64.0"
+            "&geo_polygon=17.0,-61.0&geo_polygon=16.5,-58.0&geo_polygon=16.0,-55.0"
+            "&geo_polygon=15.5,-52.0&geo_polygon=15.0,-49.0&geo_polygon=14.5,-46.0"
+            "&geo_polygon=14.0,-43.0&geo_polygon=13.5,-40.0&geo_polygon=13.0,-37.0"
+            "&geo_polygon=12.5,-34.0&geo_polygon=12.0,-31.0&geo_polygon=11.5,-28.0"
+            "&geo_polygon=11.0,-25.0&geo_polygon=10.5,-22.0&geo_polygon=10.0,-19.0"
+        )
+
+        response = requests.get(
+            f"{self.root_url}/api/v1/production-locations/{query}",
+            headers=self.basic_headers,
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        result = response.json()
+        self.assertIsNotNone(result['data'])
+        self.assertGreater(len(result['data']), 0)
+
