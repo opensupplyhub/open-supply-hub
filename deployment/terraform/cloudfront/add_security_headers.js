b5f4af63d2c031c22c4ff7f458fe7b92095a6e40
mazur <mazursasha1990@gmail.com>
2025-04-08 10:45:00 +0300

    Remove add_security_headers.js file

--- deployment/terraform/cloudfront/add_security_headers.js
+++
@@ -1,9 +0,0 @@
-function handler(event) {
-    var response = event.response;
-    var headers = response.headers;
-
-    headers["x-frame-options"] = { value: "DENY" };
-    headers["content-security-policy"] = { value: "frame-ancestors 'none';" };
-
-    return response;
-}
