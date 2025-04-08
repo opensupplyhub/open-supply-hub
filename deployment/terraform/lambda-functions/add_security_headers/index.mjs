82f39953a755af6d568a0d9d5fe4dbcd59c72c15
mazur <mazursasha1990@gmail.com>
2025-04-08 12:03:02 +0300

    Add logic to skip adding the headers if the URL contains embed=1

--- deployment/terraform/lambda-functions/add_security_headers/index.mjs
+++ deployment/terraform/lambda-functions/add_security_headers/index.mjs
@@ -4,10 +4,17 @@ export const handler = async (event) => {
     const response = event.Records[0].cf.response;
     const headers = response.headers;
 
-    headers["x-frame-options"] = [{ key: "X-Frame-Options", value: "DENY" }];
-    headers["content-security-policy"] = [
-        { key: "Content-Security-Policy", value: "frame-ancestors 'none'" },
-    ];
+    const request = event.Records[0].cf.request;
+    const queryString = request.querystring || "";
+
+    if (!queryString.includes("embed=1")) {
+        headers["x-frame-options"] = [
+            { key: "X-Frame-Options", value: "DENY" },
+        ];
+        headers["content-security-policy"] = [
+            { key: "Content-Security-Policy", value: "frame-ancestors 'none'" },
+        ];
+    }
 
     return response;
 };
