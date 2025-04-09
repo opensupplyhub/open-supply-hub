3c47f8a5f143f94fbb3392a3b20d1380f144e229
Stepan Protsak <stephan.protsack@gmail.com>
2025-04-09 23:16:10 +0200

    Add additional logging to opensearch configuration

--- src/django/api/management/commands/configure_opensearch.py
+++ src/django/api/management/commands/configure_opensearch.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from api.services.opensearch.opensearch import OpenSearchServiceConnection
 from api.models.settings import Settings
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -49,13 +50,65 @@ class Command(BaseCommand):
                     "description": "A model group for NLP models.",
                 },
             )
-            model_group_id.value = model_reg_res["model_group_id"]
-            model_group_id.save()
+            model_group_id.update(value=model_reg_res["model_group_id"])
             logger.info(
                 "Model group with ID '%s' created successfully!",
                 model_reg_res["model_group_id"],
             )
         else:
-            logger.info("Model group ID already exists.")
+            logger.info("Model group with ID '%s' already exists.",
+                        model_group_id.value)
 
-        logger.info(model_group_id.value)
+        model_id = Settings.get(
+            name=Settings.Name.OS_SENTENCE_TRANSFORMER_MODEL_ID,
+            description="Model ID for OpenSearch embedding generation model.",
+        )
+
+        if not model_id.value:
+            logger.info(
+                "Creating model for OpenSearch embedding generation model.")
+            model_name = Settings.get(
+                name=Settings.Name.OS_SENTENCE_TRANSFORMER_MODEL_NAME,
+                description="Model name for OpenSearch embedding generation model.",
+                value="huggingface/sentence-transformers/all-MiniLM-L6-v2",
+            )
+            model_reg_res = opensearch.client.plugins.ml.register_model(
+                body={
+                    "name": model_name.value,
+                    "version": "1.0.1",
+                    "model_group_id": model_group_id.value,
+                    "model_format": "TORCH_SCRIPT",
+                },
+            )
+            logger.info(
+                "Model registration task with ID '%s' created successfully!",
+                model_reg_res["task_id"],
+            )
+
+            while True:
+                task_res = opensearch.client.plugins.ml.get_task(
+                    task_id=model_reg_res["task_id"],
+                )
+
+                if task_res["state"] == "COMPLETED":
+                    model_id.update(value=task_res["model_id"])
+                    logger.info(
+                        "Model with ID '%s' created successfully!",
+                        task_res["model_id"],
+                    )
+                    break
+
+                if task_res["state"] == "FAILED":
+                    raise RuntimeError(
+                        "Model creation failed!",
+                    )
+
+                logger.info(
+                    "Waiting for model creation to complete! Task state: '%s'",
+                    task_res["state"],
+                )
+
+                time.sleep(2)
+        else:
+            logger.info("Model with ID '%s' already exists.",
+                        model_id.value)
--- src/django/api/models/settings.py
+++ src/django/api/models/settings.py
@@ -8,6 +8,8 @@ class Settings(models.Model):
 
     class Name(models.TextChoices):
         OS_SENTENCE_TRANSFORMER_GROUP_ID = 'os_sentence_transformer_group_id', 'OS Sentence Transformer Group ID'
+        OS_SENTENCE_TRANSFORMER_MODEL_ID = 'os_sentence_transformer_model_id', 'OS Sentence Transformer Model ID'
+        OS_SENTENCE_TRANSFORMER_MODEL_NAME = 'os_sentence_transformer_model_name', 'OS Sentence Transformer Model Name'
 
     name = models.CharField(
         max_length=255,
@@ -36,13 +38,28 @@ class Settings(models.Model):
         db_index=True
     )
 
+    def update(self, value: str):
+        """
+        Update the value of the setting.
+        """
+        self.value = value
+        self.save()
+
     @staticmethod
-    def get(name: str, description: str):
-        setting, _ = Settings.objects.get_or_create(
-            defaults={
-                "name": name,
-                "description": description,
-            }
+    def get(name: str, description: str, value: any = None):
+        """
+        Get or create a setting with the given name and description.
+        """
+        defaults = {
+            "name": name,
+            "description": description,
+        }
+
+        if value is not None:
+            defaults["value"] = value
+
+        setting, _ = Settings.objects.filter(name=name).get_or_create(
+            defaults=defaults,
         )
 
         return setting
