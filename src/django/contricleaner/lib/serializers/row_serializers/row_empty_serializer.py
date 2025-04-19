b098735a58397b8438d717090c334a6528625ed6
mazur <mazursasha1990@gmail.com>
2025-04-19 10:37:06 +0300

    Modify RowEmptySerializer to exclude the additional_ids field from response

--- src/django/contricleaner/lib/serializers/row_serializers/row_empty_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_empty_serializer.py
@@ -3,7 +3,12 @@ from .row_serializer import RowSerializer
 
 class RowEmptySerializer(RowSerializer):
     def validate(self, row: dict, current: dict) -> dict:
-        keys = set(row.keys()).difference(current.keys())
+        excluded_keys = ['additional_ids']
+        keys = (
+            set(row.keys())
+            .difference(current.keys())
+            .difference(excluded_keys)
+        )
         for key in keys:
             current[key] = row[key]
 
