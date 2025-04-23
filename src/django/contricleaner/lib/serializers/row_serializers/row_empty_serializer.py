a8a64dc2c5f34913b0abf0df6927c48ec00e9002
mazur <mazursasha1990@gmail.com>
2025-04-23 22:15:38 +0300

    Revert changes in the RowEmptySerializer

--- src/django/contricleaner/lib/serializers/row_serializers/row_empty_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_empty_serializer.py
@@ -3,12 +3,7 @@ from .row_serializer import RowSerializer
 
 class RowEmptySerializer(RowSerializer):
     def validate(self, row: dict, current: dict) -> dict:
-        excluded_keys = ['additional_identifiers']
-        keys = (
-            set(row.keys())
-            .difference(current.keys())
-            .difference(excluded_keys)
-        )
+        keys = set(row.keys()).difference(current.keys())
         for key in keys:
             current[key] = row[key]
 
