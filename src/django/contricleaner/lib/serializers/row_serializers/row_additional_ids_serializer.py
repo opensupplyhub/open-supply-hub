8a5b9bad09fd4ca616a355c99bfc4492629043b8
mazur <mazursasha1990@gmail.com>
2025-04-23 22:34:00 +0300

    Modify the RowAdditionalIdsSerializer

--- src/django/contricleaner/lib/serializers/row_serializers/row_additional_ids_serializer.py
+++ src/django/contricleaner/lib/serializers/row_serializers/row_additional_ids_serializer.py
@@ -11,59 +11,61 @@ class RowAdditionalIdsSerializer(RowSerializer):
 
     def validate(self, row: dict, current: dict) -> dict:
         for key in AdditionalIDs.ALLOWED_KEYS:
-            if key in row:
-                value = row[key]
-                if not isinstance(value, str):
-                    current['errors'].append(
-                        {
-                            'message': f'Expected value for {key} to be a '
-                            f'string but got {type(value).__name__}.',
-                            'field': key,
-                            'type': 'ValueError',
-                        }
-                    )
-                    continue
+            if key not in row:
+                continue
 
-                if key == AdditionalIDs.DUNS_ID and not self.__is_valid_duns(
-                    value
-                ):
-                    current['errors'].append(
-                        {
-                            'message': f'Invalid `duns_id`: {value}. '
-                            'It should be a 9-digit number.',
-                            'field': key,
-                            'type': 'ValueError',
-                        }
-                    )
+            value = row[key]
+            if not isinstance(value, str):
+                current['errors'].append(
+                    {
+                        'message': f'Expected value for {key} to be a '
+                        f'string but got {type(value).__name__}.',
+                        'field': key,
+                        'type': 'ValueError',
+                    }
+                )
+                continue
 
-                elif key == AdditionalIDs.LEI_ID and not self.__is_valid_lei(
-                    value
-                ):
-                    current['errors'].append(
-                        {
-                            'message': f'Invalid `lei_id`: {value}. '
-                            'It should be a 20-character string '
-                            'with 18 alphanumeric characters followed by '
-                            '2 digits.',
-                            'field': key,
-                            'type': 'ValueError',
-                        }
-                    )
+            if key == AdditionalIDs.DUNS_ID and not self.__is_valid_duns(
+                value
+            ):
+                current['errors'].append(
+                    {
+                        'message': f'Invalid `duns_id`: {value}. '
+                        'It should be a 9-digit number.',
+                        'field': key,
+                        'type': 'ValueError',
+                    }
+                )
 
-                elif key == AdditionalIDs.RBA_ID and not self.__is_valid_rba(
-                    value
-                ):
-                    current['errors'].append(
-                        {
-                            'message': f'Invalid `rba_id`: {value}. '
-                            'It should be a string with a maximum length of '
-                            '255 characters.',
-                            'field': key,
-                            'type': 'ValueError',
-                        }
-                    )
+            elif key == AdditionalIDs.LEI_ID and not self.__is_valid_lei(
+                value
+            ):
+                current['errors'].append(
+                    {
+                        'message': f'Invalid `lei_id`: {value}. '
+                        'It should be a 20-character string '
+                        'with 18 alphanumeric characters followed by '
+                        '2 digits.',
+                        'field': key,
+                        'type': 'ValueError',
+                    }
+                )
 
-                current[key] = value
+            elif key == AdditionalIDs.RBA_ID and not self.__is_valid_rba(
+                value
+            ):
+                current['errors'].append(
+                    {
+                        'message': f'Invalid `rba_id`: {value}. '
+                        'It should be a string with a maximum length of '
+                        '255 characters.',
+                        'field': key,
+                        'type': 'ValueError',
+                    }
+                )
+
+            current[key] = value
 
         return current
 
@@ -74,4 +76,4 @@ class RowAdditionalIdsSerializer(RowSerializer):
         return bool(self.__LEI_RE.fullmatch(value))
 
     def __is_valid_rba(self, value: str) -> bool:
-        return bool(isinstance(value, str) and len(value) <= 255)
+        return len(value) <= 255
