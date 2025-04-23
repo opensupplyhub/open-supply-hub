13f10c7c8e6d8e3fe6c4deab2d11af91a04f8eca
Roman Stolar <roman.stolar@speedandfunction.com>
2025-04-23 15:53:28 +0300

    fix linter

--- src/django/api/management/commands/make_token.py
+++ src/django/api/management/commands/make_token.py
@@ -5,11 +5,26 @@ from django.core.management import call_command
 class Command(BaseCommand):
     help = ('Create an API token for a given user and '
             'set admin/superuser rights.')
-    
+
     def add_arguments(self, parser):
-        parser.add_argument('--user-id', type=int, required=True, help='ID of the user')
-        parser.add_argument('--is-admin', type=bool, required=True, help='Grant superuser and staff rights')
-        parser.add_argument('--token-key', type=str, required=True, help='Token key to set manually')
+        parser.add_argument(
+            '--user-id',
+            type=int,
+            required=True,
+            help='ID of the user'
+        )
+        parser.add_argument(
+            '--is-admin',
+            type=bool,
+            required=True,
+            help='Grant superuser and staff rights'
+        )
+        parser.add_argument(
+            '--token-key',
+            type=str,
+            required=True,
+            help='Token key to set manually'
+        )
 
     def handle(self, *args, **options):
         user_id = options['user_id']
