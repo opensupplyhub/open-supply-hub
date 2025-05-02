b5f149dbba1b6838f1f4214e390a61ff002ee6c3
VadimKovalenko <vadim.kovalenko@speedandfunction.com>
2025-05-02 14:37:35 +0400

    Fix to replace rule on the same WAF

--- deployment/terraform/waf.tf
+++ deployment/terraform/waf.tf
@@ -7,39 +7,44 @@ locals {
   is_whitelist_enabled = length(var.ip_whitelist) > 0
   is_denylist_enabled  = length(var.ip_denylist) > 0
 
-  environment_enabled = toset(["Development", "Production", "Preprod", "Staging", "Test"])
-  is_env_enabled      = contains(local.environment_enabled, var.environment)
+  conflicting_lists = local.is_whitelist_enabled && local.is_denylist_enabled
 
-  invalid_config = local.is_whitelist_enabled && local.is_denylist_enabled
-  waf_enabled = local.is_env_enabled && (local.is_whitelist_enabled || local.is_denylist_enabled)
+  waf_enabled = local.is_whitelist_enabled || local.is_denylist_enabled
+  web_acl_default_action = local.is_whitelist_enabled ? "block" : "allow"
+
+  ip_set_addresses = local.is_whitelist_enabled ? var.ip_whitelist : (
+                     local.is_denylist_enabled ? var.ip_denylist : [])
+
+  ip_set_type = local.is_whitelist_enabled ? "whitelist" : (
+                local.is_denylist_enabled ? "denylist" : "")
 }
 
-resource "null_resource" "validate_ip_list_exclusivity" {
-  count = local.invalid_config ? 1 : 0
+resource "null_resource" "validate_ip_lists" {
+  count = local.conflicting_lists ? 1 : 0
 
   provisioner "local-exec" {
-    command = "echo 'ERROR: Only one of ip_whitelist or ip_denylist should be defined.' && exit 1"
+    command = "echo 'ERROR: ip_whitelist and ip_denylist cannot both be set' && exit 1"
   }
 }
 
 resource "aws_wafv2_ip_set" "ip_whitelist" {
-  count              = local.is_whitelist_enabled && local.is_env_enabled ? 1 : 0
   provider           = aws.us-east-1
   name               = "whitelist-ipset"
   description        = "Allowed IPs"
   scope              = "CLOUDFRONT"
   ip_address_version = "IPV4"
   addresses          = var.ip_whitelist
+  depends_on = [aws_wafv2_web_acl.web_acl]
 }
 
 resource "aws_wafv2_ip_set" "ip_denylist" {
-  count              = local.is_denylist_enabled && local.is_env_enabled ? 1 : 0
   provider           = aws.us-east-1
   name               = "denylist-ipset"
   description        = "Blocked IPs"
   scope              = "CLOUDFRONT"
   ip_address_version = "IPV4"
   addresses          = var.ip_denylist
+  depends_on = [aws_wafv2_web_acl.web_acl]
 }
 
 resource "aws_wafv2_web_acl" "web_acl" {
