71b6fc780715d4936fb42e0051d85d965d41b3ab
VadimKovalenko <vadim.kovalenko@speedandfunction.com>
2025-05-08 14:50:46 +0400

    Revert changes for find_cloudfront_distribution_id

--- scripts/find_cloudfront_distribution_id.sh
+++ scripts/find_cloudfront_distribution_id.sh
@@ -4,15 +4,14 @@ set -euo pipefail
 domain="$1"
 found_id=""
 
-aliases=$(aws cloudfront get-distribution-config --id "$id" --query "DistributionConfig.Aliases.Items[]" --output text)
-for alias in $aliases; do
-  if [[ "$alias" == "$domain" ]]; then
+for id in $(aws cloudfront list-distributions --query "DistributionList.Items[*].Id" --output text); do
+  domains=$(aws cloudfront get-distribution-config --id "$id" --query "DistributionConfig.Aliases.Items" --output text)
+  if [[ "$domains" == "$domain" ]]; then
     if [ -n "$found_id" ]; then
       echo "Error: Multiple CloudFront distributions found for domain: $domain" >&2
       exit 1
     fi
     found_id="$id"
-    break
   fi
 done
 
