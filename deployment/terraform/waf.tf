fae811967c3df624d5ffbb046f6d45b5c6d688e6
VadimKovalenko <vadim.kovalenko@speedandfunction.com>
2025-06-02 16:50:47 +0400

    add IPv6 for deny and allow ip rules

--- deployment/terraform/waf.tf
+++ deployment/terraform/waf.tf
@@ -7,6 +7,10 @@ locals {
   is_whitelist_enabled = length(var.ip_whitelist) > 0
   is_denylist_enabled  = length(var.ip_denylist) > 0
   ip_list_conflict = local.is_whitelist_enabled && local.is_denylist_enabled
+  ipv4_whitelist = [for ip in var.ip_whitelist : ip if can(regex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}", ip))]
+  ipv6_whitelist = [for ip in var.ip_whitelist : ip if can(regex("^[0-9a-fA-F:]+", ip))]
+  ipv4_denylist = [for ip in var.ip_denylist : ip if can(regex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}", ip))]
+  ipv6_denylist = [for ip in var.ip_denylist : ip if can(regex("^[0-9a-fA-F:]+", ip))]
 }
 
 resource "null_resource" "validate_ip_lists" {
@@ -17,27 +21,53 @@ resource "null_resource" "validate_ip_lists" {
   }
 }
 
-resource "aws_wafv2_ip_set" "ip_whitelist" {
-  count             = local.is_whitelist_enabled ? 1 : 0
+resource "aws_wafv2_ip_set" "ipv4_whitelist" {
+  count             = local.is_whitelist_enabled && length(local.ipv4_whitelist) > 0 ? 1 : 0
   provider          = aws.us-east-1
-  name = "${lower(var.environment)}-whitelist-ipset"
-  description       = "Allowed IPs"
+  name              = "${lower(var.environment)}-whitelist-ipv4-ipset"
+  description       = "Allowed IPv4 addresses"
   scope             = "CLOUDFRONT"
   ip_address_version = "IPV4"
-  addresses         = var.ip_whitelist
+  addresses         = local.ipv4_whitelist
   tags = { 
     Environment = var.environment
   }
 }
 
-resource "aws_wafv2_ip_set" "ip_denylist" {
-  count             = local.is_denylist_enabled ? 1 : 0
+resource "aws_wafv2_ip_set" "ipv6_whitelist" {
+  count             = local.is_whitelist_enabled && length(local.ipv6_whitelist) > 0 ? 1 : 0
   provider          = aws.us-east-1
-  name = "${lower(var.environment)}-denylist-ipset"
-  description       = "Blocked IPs"
+  name              = "${lower(var.environment)}-whitelist-ipv6-ipset"
+  description       = "Allowed IPv6 addresses"
+  scope             = "CLOUDFRONT"
+  ip_address_version = "IPV6"
+  addresses         = local.ipv6_whitelist
+  tags = { 
+    Environment = var.environment
+  }
+}
+
+resource "aws_wafv2_ip_set" "ipv4_denylist" {
+  count             = local.is_denylist_enabled && length(local.ipv4_denylist) > 0 ? 1 : 0
+  provider          = aws.us-east-1
+  name              = "${lower(var.environment)}-denylist-ipv4-ipset"
+  description       = "Blocked IPv4 addresses"
   scope             = "CLOUDFRONT"
   ip_address_version = "IPV4"
-  addresses         = var.ip_denylist
+  addresses         = local.ipv4_denylist
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_wafv2_ip_set" "ipv6_denylist" {
+  count             = local.is_denylist_enabled && length(local.ipv6_denylist) > 0 ? 1 : 0
+  provider          = aws.us-east-1
+  name              = "${lower(var.environment)}-denylist-ipv6-ipset"
+  description       = "Blocked IPv6 addresses"
+  scope             = "CLOUDFRONT"
+  ip_address_version = "IPV6"
+  addresses         = local.ipv6_denylist
   tags = {
     Environment = var.environment
   }
@@ -52,42 +82,84 @@ resource "aws_wafv2_web_acl" "web_acl" {
   scope       = "CLOUDFRONT"
 
   dynamic "rule" {
-    for_each = local.is_denylist_enabled ? [true] : []
+    for_each = local.is_denylist_enabled && length(local.ipv4_denylist) > 0 ? [true] : []
     content {
-      name     = "BlockDenylistedIPs"
+      name     = "BlockDenylistedIPv4"
       priority = 0
       action {
         block {}
       }
       statement {
         ip_set_reference_statement {
-          arn = aws_wafv2_ip_set.ip_denylist[0].arn
+          arn = aws_wafv2_ip_set.ipv4_denylist[0].arn
         }
       }
       visibility_config {
         cloudwatch_metrics_enabled = true
-        metric_name                = "BlockDenylistedIPs"
+        metric_name                = "BlockDenylistedIPv4"
         sampled_requests_enabled   = true
       }
     }
   }
 
   dynamic "rule" {
-    for_each = local.is_whitelist_enabled ? [true] : []
+    for_each = local.is_denylist_enabled && length(local.ipv6_denylist) > 0 ? [true] : []
     content {
-      name     = "AllowWhitelistedIPs"
-      priority = 0
+      name     = "BlockDenylistedIPv6"
+      priority = 1
+      action {
+        block {}
+      }
+      statement {
+        ip_set_reference_statement {
+          arn = aws_wafv2_ip_set.ipv6_denylist[0].arn
+        }
+      }
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "BlockDenylistedIPv6"
+        sampled_requests_enabled   = true
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = local.is_whitelist_enabled && length(local.ipv4_whitelist) > 0 ? [true] : []
+    content {
+      name     = "AllowWhitelistedIPv4"
+      priority = 2
       action {
         allow {}
       }
       statement {
         ip_set_reference_statement {
-          arn = aws_wafv2_ip_set.ip_whitelist[0].arn
+          arn = aws_wafv2_ip_set.ipv4_whitelist[0].arn
         }
       }
       visibility_config {
         cloudwatch_metrics_enabled = true
-        metric_name                = "AllowWhitelistedIPs"
+        metric_name                = "AllowWhitelistedIPv4"
+        sampled_requests_enabled   = true
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = local.is_whitelist_enabled && length(local.ipv6_whitelist) > 0 ? [true] : []
+    content {
+      name     = "AllowWhitelistedIPv6"
+      priority = 3
+      action {
+        allow {}
+      }
+      statement {
+        ip_set_reference_statement {
+          arn = aws_wafv2_ip_set.ipv6_whitelist[0].arn
+        }
+      }
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "AllowWhitelistedIPv6"
         sampled_requests_enabled   = true
       }
     }
