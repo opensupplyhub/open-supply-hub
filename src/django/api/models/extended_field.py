f5e3405db12d591e6c735195a6d17a4b2b306458
Roman Stolar <roman.stolar@speedandfunction.com>
2025-06-06 12:49:06 +0300

    fix missed

--- src/django/api/models/extended_field.py
+++ src/django/api/models/extended_field.py
@@ -83,7 +83,7 @@ class ExtendedField(models.Model):
                    'and max to it.'))
     origin_source = models.CharField(
         choices=OriginSource.CHOICES,
-        default=OriginSource.OSHUB,
+        null=True,
         max_length=200,
         help_text="The environment value where instance running"
     )
