166d1b2cefb786e9aa70ae2c31ae0c96eb0c2e92
Roman Stolar <roman.stolar@speedandfunction.com>
2025-06-06 13:00:38 +0300

    added triggers and reverts to relevant tables

--- src/django/sqls/0170_revert_origin_source_triggers.sql
+++ src/django/sqls/0170_revert_origin_source_triggers.sql
@@ -1,3 +1,23 @@
 DROP TRIGGER IF EXISTS trg_set_origin_source_contributor ON api_contributor;
 
+DROP TRIGGER IF EXISTS trg_set_origin_source_extendedfield ON api_extendedfield;
+
 DROP TRIGGER IF EXISTS trg_set_origin_source_facility ON api_facility;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_facilityactivityreport ON api_facilityactivityreport;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_facilityalias ON api_facilityalias;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_facilityclaim ON api_facilityclaim;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_facilitylist ON api_facilitylist;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_facilitylistitem ON api_facilitylistitem;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_facilitymatch ON api_facilitymatch;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_source ON api_source;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_facilitylocation ON api_facilitylocation;
+
+DROP TRIGGER IF EXISTS trg_set_origin_source_user ON api_user;
