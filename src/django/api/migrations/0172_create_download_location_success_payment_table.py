7d8c18ad31d0cf06c11f7b705a6a8c3cdfc84ff2
mazur <mazursasha1990@gmail.com>
2025-06-19 16:15:10 +0300

    Formatt the 0172_create_download_location_success_payment_table.py

--- src/django/api/migrations/0172_create_download_location_success_payment_table.py
+++ src/django/api/migrations/0172_create_download_location_success_payment_table.py
@@ -13,18 +13,65 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='DownloadLocationSuccessPayment',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('stripe_session_id', models.CharField(help_text='The unique identifier for the Stripe checkout session.', max_length=255, unique=True)),
-                ('payment_id', models.CharField(help_text='The unique identifier for the payment, if available.', max_length=255, unique=True)),
-                ('amount_subtotal', models.IntegerField(help_text='The subtotal amount of the payment, stored in cents.')),
-                ('amount_total', models.IntegerField(help_text='The total amount of the payment, stored in cents.')),
-                ('promotion_code', models.CharField(blank=True, help_text='The promotion code applied to the payment, if any.', max_length=255, null=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True, db_index=True)),
+                (
+                    'id',
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    ),
+                ),
+                (
+                    'stripe_session_id',
+                    models.CharField(
+                        help_text='The unique identifier for the Stripe checkout session.',
+                        max_length=255,
+                        unique=True,
+                    ),
+                ),
+                (
+                    'payment_id',
+                    models.CharField(
+                        help_text='The unique identifier for the payment, if available.',
+                        max_length=255,
+                        unique=True,
+                    ),
+                ),
+                (
+                    'amount_subtotal',
+                    models.IntegerField(
+                        help_text='The subtotal amount of the payment, stored in cents.'
+                    ),
+                ),
+                (
+                    'amount_total',
+                    models.IntegerField(
+                        help_text='The total amount of the payment, stored in cents.'
+                    ),
+                ),
+                (
+                    'promotion_code',
+                    models.CharField(
+                        blank=True,
+                        help_text='The promotion code applied to the payment, if any.',
+                        max_length=255,
+                        null=True,
+                    ),
+                ),
+                (
+                    'created_at',
+                    models.DateTimeField(auto_now_add=True, db_index=True),
+                ),
             ],
         ),
         migrations.AddField(
             model_name='downloadlocationsuccesspayment',
             name='user',
-            field=models.ForeignKey(help_text='The user who made the payment.', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(
+                help_text='The user who made the payment.',
+                on_delete=django.db.models.deletion.CASCADE,
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
