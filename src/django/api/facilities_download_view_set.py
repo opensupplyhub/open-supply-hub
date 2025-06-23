fde749f517cea897562cf5773419409f7c325974
Inessa Druzhkova <inessadruzkova@inessas-mbp.home>
2025-06-23 13:00:37 +0200

    added FacilitiesDownloadService

--- src/django/api/facilities_download_view_set.py
+++ src/django/api/facilities_download_view_set.py
@@ -1,25 +1,12 @@
-import logging
-
-from rest_framework.exceptions import ValidationError
 from rest_framework import viewsets, mixins
-from waffle import switch_is_active
-from datetime import datetime
-from django.utils.timezone import make_aware
-
 from api.pagination import PageAndSizePagination
 from api.models.facility.facility_index import FacilityIndex
-from api.models.facility_download_limit import FacilityDownloadLimit
-from api.serializers.facility.facility_query_params_serializer import (
-    FacilityQueryParamsSerializer)
 from api.serializers.facility.facility_download_serializer \
     import FacilityDownloadSerializer
 from api.serializers.facility.facility_download_serializer_embed_mode \
     import FacilityDownloadSerializerEmbedMode
 from api.serializers.utils import get_embed_contributor_id_from_query_params
-from api.exceptions import ServiceUnavailableException
-from api.constants import APIErrorMessages, FacilitiesDownloadSettings
-
-logger = logging.getLogger(__name__)
+from api.services.facilities_download_service import FacilitiesDownloadService
 
 
 class FacilitiesDownloadViewSet(mixins.ListModelMixin,
@@ -28,9 +15,7 @@ class FacilitiesDownloadViewSet(mixins.ListModelMixin,
     Get facilities in array format, suitable for CSV/XLSX download.
     """
     queryset = FacilityIndex.objects.all()
-
     pagination_class = PageAndSizePagination
-
     def get_serializer(self, page_queryset):
         if self.request.query_params.get('embed') == '1':
             contributor_id = get_embed_contributor_id_from_query_params(
@@ -47,82 +32,31 @@ class FacilitiesDownloadViewSet(mixins.ListModelMixin,
         Returns a list of facilities in array format for a given query.
         (Maximum of 250 facilities per page.)
         """
-        if switch_is_active('block_location_downloads'):
-            raise ServiceUnavailableException(
-                APIErrorMessages.TEMPORARILY_UNAVAILABLE
-            )
+        service = FacilitiesDownloadService()
+        service._check_if_downloads_are_blocked()
+        service._validate_query_params(request)
+        service._log_request(request)
 
-        params = FacilityQueryParamsSerializer(data=request.query_params)
-
-        if not params.is_valid():
-            raise ValidationError(params.errors)
-
-        logger.info(
-            f'Facility downloads request for User ID: {request.user.id}'
-        )
-
-        queryset = FacilityIndex \
-            .objects \
-            .filter_by_query_params(request.query_params) \
-            .order_by('name', 'address', 'id')
-
+        queryset = service._get_filtered_queryset(request)
         total_records = queryset.count()
 
-        initial_release_date = make_aware(datetime(2025, 6, 28))
+        facility_download_limit = service._get_download_limit(request)
+        service._enforce_limits(request, total_records, facility_download_limit)
 
-        facility_download_limit = FacilityDownloadLimit \
-            .get_or_create_user_download_limit(
-                request.user,
-                initial_release_date
-            )
-
-        current_page = int(request.query_params.get("page", 1))
-
-        if (
-            current_page == 1 and
-            facility_download_limit
-            and (
-                facility_download_limit.free_download_records +
-                facility_download_limit.paid_download_records
-            ) == 0
-        ):
-            raise ValidationError('You have reached your annual limit '
-                                  'for facility record downloads, including'
-                                  ' both free and paid. Additional downloads'
-                                  ' will be available at the start of the next'
-                                  ' calendar year.')
-
-        is_large_download_allowed = (
-            not facility_download_limit
-            or total_records <= (
-                FacilitiesDownloadSettings.FACILITIES_DOWNLOAD_LIMIT
-            )
-        )
-
-        if (not is_large_download_allowed):
-            records_sum = (
-                facility_download_limit.free_download_records +
-                facility_download_limit.paid_download_records
-            )
-            raise ValidationError(
-                ('Downloads are supported only for searches resulting in '
-                 f'{records_sum} '
-                 'facilities or less.'))
-
-        page_queryset = self.paginate_queryset(queryset)
-
-        if page_queryset is None:
-            raise ValidationError("Invalid pageSize parameter")
-
+        page_queryset = service\
+            ._check_pagination(self.paginate_queryset(queryset))
         list_serializer = self.get_serializer(page_queryset)
+
         rows = [f['row'] for f in list_serializer.data]
         headers = list_serializer.child.get_headers()
         data = {'rows': rows, 'headers': headers}
+
         response = self.get_paginated_response(data)
 
         records_to_subtract = len(data['rows'])
-
-        if facility_download_limit:
-            facility_download_limit.register_download(records_to_subtract)
+        service._register_download_if_needed(
+            facility_download_limit,
+            records_to_subtract
+        )
 
         return response
---
+++ src/django/api/services/facilities_download_service.py
@@ -0,0 +1,87 @@
+import logging
+
+from rest_framework.exceptions import ValidationError
+from waffle import switch_is_active
+from datetime import datetime
+from django.utils.timezone import make_aware
+
+from api.models.facility.facility_index import FacilityIndex
+from api.models.facility_download_limit import FacilityDownloadLimit
+from api.serializers.facility.facility_query_params_serializer import (
+    FacilityQueryParamsSerializer)
+from api.exceptions import ServiceUnavailableException
+from api.constants import APIErrorMessages, FacilitiesDownloadSettings
+
+logger = logging.getLogger(__name__)
+
+
+
+class FacilitiesDownloadService:
+    def _check_if_downloads_are_blocked(self):
+        if switch_is_active('block_location_downloads'):
+            raise ServiceUnavailableException(
+                    APIErrorMessages.TEMPORARILY_UNAVAILABLE
+                )
+
+    def _validate_query_params(self, request):
+        params = FacilityQueryParamsSerializer(data=request.query_params)
+
+        if not params.is_valid():
+            raise ValidationError(params.errors)
+
+    def _log_request(self, request):
+        logger.info(f'Facility downloads request for User ID: {request.user.id}')
+
+    def _get_filtered_queryset(self, request):
+        return FacilityIndex.objects.filter_by_query_params(
+            request.query_params
+        ).order_by('name', 'address', 'id')
+
+    def _get_download_limit(self, request):
+        initial_release_date = make_aware(datetime(2025, 6, 28))
+
+        return FacilityDownloadLimit.get_or_create_user_download_limit(
+            request.user, initial_release_date
+        )
+
+    def _enforce_limits(self, request, total_records, limit):
+        current_page = int(request.query_params.get("page", 1))
+
+        has_exhausted_limit = (
+            current_page == 1 and
+            limit is not None and
+            (
+                limit.free_download_records +
+                limit.paid_download_records
+            ) == 0
+        )
+
+        if has_exhausted_limit:
+            raise ValidationError(
+                'You have reached your annual limit for facility record downloads, '
+                'including both free and paid. Additional downloads will be available '
+                'at the start of the next calendar year.'
+            )
+
+        has_download_limit = limit is not None
+        max_allowed = FacilitiesDownloadSettings.FACILITIES_DOWNLOAD_LIMIT
+
+        is_large_download_blocked = has_download_limit and total_records > max_allowed
+
+        if is_large_download_blocked:
+            records_used = (
+                limit.free_download_records +
+                limit.paid_download_records
+            )
+            raise ValidationError(
+                f'Downloads are only allowed for results containing {records_used} facilities or fewer.'
+            )
+
+    def _check_pagination(self, page_queryset):
+        if page_queryset is None:
+            raise ValidationError("Invalid pageSize parameter")
+        return page_queryset
+
+    def _register_download_if_needed(self, limit, record_count):
+        if limit:
+            limit.register_download(record_count)
