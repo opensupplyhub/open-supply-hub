77d272cd3d799b713ae77fddcd7d3da083ca2eed
mazur <mazursasha1990@gmail.com>
2025-07-04 11:33:08 +0300

    Modify description for dark_visitors_project_key and dark_visitors_token terraform variables

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -834,11 +834,11 @@ variable "direct_data_load_tab_id" {
 
 variable "dark_visitors_project_key" {
   type        = string
-  description = "Dark Visitors project key"
+  description = "Dark Visitors project key. This variable is not sensitive as it is exposed to the browser."
 }
 
 variable "dark_visitors_token" {
   type        = string
-  description = "Dark Visitors token"
+  description = "Dark Visitors token."
   sensitive   = true
 }
