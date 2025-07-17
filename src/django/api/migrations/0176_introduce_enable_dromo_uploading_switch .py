2ae5b6636635e2af51aa32f7b5bb95eeb5d12c0e
mazur <mazursasha1990@gmail.com>
2025-07-17 13:39:35 +0300

    Rename local variable Switch to switch in the 0176_introduce_enable_dromo_uploading_switch migration

--- src/django/api/migrations/0176_introduce_enable_dromo_uploading_switch .py
+++ src/django/api/migrations/0176_introduce_enable_dromo_uploading_switch .py
@@ -2,13 +2,13 @@ from django.db import migrations
 
 
 def create_enable_dromo_uploading_switch(apps, schema_editor):
-    Switch = apps.get_model('waffle', 'Switch')
-    Switch.objects.create(name='enable_dromo_uploading', active=False)
+    switch = apps.get_model('waffle', 'Switch')
+    switch.objects.create(name='enable_dromo_uploading', active=False)
 
 
 def delete_enable_dromo_uploading_switch(apps, schema_editor):
-    Switch = apps.get_model('waffle', 'Switch')
-    Switch.objects.get(name='enable_dromo_uploading').delete()
+    switch = apps.get_model('waffle', 'Switch')
+    switch.objects.get(name='enable_dromo_uploading').delete()
 
 
 class Migration(migrations.Migration):
