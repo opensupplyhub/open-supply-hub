9cb1a33b28ddbef4cef52e10804dbda23aaaf0b1
vladsha-dev <newshapovalov2002@gmail.com>
2025-08-26 12:33:48 +0200

    Setup correct IPs

--- deployment/terraform/database-private-link.tf
+++ deployment/terraform/database-private-link.tf
@@ -14,5 +14,5 @@ module "database_private_link" {
   database_security_group_id = module.database_enc.database_security_group_id
   debug_logging = true
 
-  db_proxy_ips = ["10.0.0.1", "10.0.0.2"]
+  db_proxy_ips = ["10.0.3.111", "10.0.1.124"]
 }
