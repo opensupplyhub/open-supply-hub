fd3acaac04fc44d917725d3a87819391a7878b9f
vladsha-dev <newshapovalov2002@gmail.com>
2025-08-26 20:29:06 +0200

    Added egress rule for the VPC endpoint

--- deployment/terraform/database-vpc-endpoint/main.tf
+++ deployment/terraform/database-vpc-endpoint/main.tf
@@ -41,6 +41,16 @@ resource "aws_security_group_rule" "target_consumer_database_vpc_endpoint_ingres
   description              = "Allow incoming traffic to the VPC endpoint from the target consumer"
 }
 
+resource "aws_security_group_rule" "database_vpc_endpoint_egress" {
+  type                     = "egress"
+  from_port                = var.db_port
+  to_port                  = var.db_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.database_vpc_endpoint_sg.id
+  cidr_blocks              = ["0.0.0.0/0"]
+  description              = "Allow outgoing traffic from the VPC endpoint to the VPC endpoint service"
+}
+
 # Security group rules for the target consumer
 
 resource "aws_security_group_rule" "target_consumer_database_vpc_endpoint_egress" {
