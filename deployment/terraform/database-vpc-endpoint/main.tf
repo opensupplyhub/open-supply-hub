99ea5bc0e066275b1f8c476b0d57b292077a4cc4
vladsha-dev <newshapovalov2002@gmail.com>
2025-08-27 12:54:32 +0200

    Updated SGs

--- deployment/terraform/database-vpc-endpoint/main.tf
+++ deployment/terraform/database-vpc-endpoint/main.tf
@@ -55,13 +55,13 @@ resource "aws_security_group_rule" "database_vpc_endpoint_egress" {
 
 # Security group rules for the target consumer
 
-resource "aws_security_group_rule" "target_consumer_database_vpc_endpoint_egress" {
-  type                     = "egress"
-  from_port                = var.db_port
-  to_port                  = var.db_port
-  protocol                 = "tcp"
+# resource "aws_security_group_rule" "target_consumer_database_vpc_endpoint_egress" {
+#   type                     = "egress"
+#   from_port                = var.db_port
+#   to_port                  = var.db_port
+#   protocol                 = "tcp"
 
-  security_group_id        = var.target_consumer_security_group_id
-  source_security_group_id = aws_security_group.database_vpc_endpoint_sg.id
-  description              = "Allow outgoing traffic to the VPC endpoint from the target consumer"
-}
+#   security_group_id        = var.target_consumer_security_group_id
+#   source_security_group_id = aws_security_group.database_vpc_endpoint_sg.id
+#   description              = "Allow outgoing traffic to the VPC endpoint from the target consumer"
+# }
