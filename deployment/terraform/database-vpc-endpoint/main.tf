134df059d54c92375fc001cf25730fee973dd9e1
vladsha-dev <newshapovalov2002@gmail.com>
2025-08-27 12:35:03 +0200

    Reverted too permissive SG rule for vpc endpoint

--- deployment/terraform/database-vpc-endpoint/main.tf
+++ deployment/terraform/database-vpc-endpoint/main.tf
@@ -47,31 +47,20 @@ resource "aws_security_group_rule" "database_vpc_endpoint_egress" {
   from_port                = var.db_port
   to_port                  = var.db_port
   protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
 
   security_group_id        = aws_security_group.database_vpc_endpoint_sg.id
-  cidr_blocks              = ["0.0.0.0/0"]
   description              = "Allow outgoing traffic from the VPC endpoint to the VPC endpoint service"
 }
 
 # Security group rules for the target consumer
 
-# resource "aws_security_group_rule" "target_consumer_database_vpc_endpoint_egress" {
-#   type                     = "egress"
-#   from_port                = var.db_port
-#   to_port                  = var.db_port
-#   protocol                 = "tcp"
-#   security_group_id        = var.target_consumer_security_group_id
-#   source_security_group_id = aws_security_group.database_vpc_endpoint_sg.id
-#   description              = "Allow outgoing traffic to the VPC endpoint from the target consumer"
-# }
-
 resource "aws_security_group_rule" "target_consumer_database_vpc_endpoint_egress" {
   type                     = "egress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  cidr_blocks              = ["0.0.0.0/0"]
-
+  from_port                = var.db_port
+  to_port                  = var.db_port
+  protocol                 = "tcp"
   security_group_id        = var.target_consumer_security_group_id
+  source_security_group_id = aws_security_group.database_vpc_endpoint_sg.id
   description              = "Allow outgoing traffic to the VPC endpoint from the target consumer"
 }
