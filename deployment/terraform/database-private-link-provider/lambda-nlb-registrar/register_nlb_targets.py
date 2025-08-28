2a5fd48869f00f3d33241139499f2bce30d8b72d
vladsha-dev <newshapovalov2002@gmail.com>
2025-08-28 17:00:36 +0200

    Fixed the function name

--- deployment/terraform/database-private-link-provider/lambda-nlb-registrar/register_nlb_targets.py
+++ deployment/terraform/database-private-link-provider/lambda-nlb-registrar/register_nlb_targets.py
@@ -5,7 +5,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def lambda_handler(event, context):
+def handler(event, context):
     host = event['host']
     tg_arn = event['tg_arn']
 
