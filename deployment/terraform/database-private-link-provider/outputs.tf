34d6de72f07838756d079ba7c8fdeb7102eb92fa
vladsha-dev <newshapovalov2002@gmail.com>
2025-08-28 20:45:07 +0200

    Removed outputs

--- deployment/terraform/database-private-link-provider/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "registrar_result" {
-  value = jsondecode(data.aws_lambda_invocation.nlb_targets_registrar.result)
-}
--- deployment/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "registrar_result" {
-    value = module.database_private_link_provider[0].registrar_result
-}
