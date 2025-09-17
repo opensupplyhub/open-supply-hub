e37c68adf53df0a74d33442b9bbd5f1603012d20
Roman Stolar <roman.stolar@speedandfunction.com>
2025-09-17 10:31:17 +0300

    WIP

--- src/django/api/moderation_event_actions/creation/location_contribution/processors/permission_processor.py
+++ src/django/api/moderation_event_actions/creation/location_contribution/processors/permission_processor.py
@@ -43,6 +43,46 @@ class PermissionProcessor(ContributionProcessor):
 
                     return event_dto
 
+                validation_errors = {
+                    'detail': APIV1CommonErrorMessages.COMMON_REQ_BODY_ERROR,
+                    'errors': []
+                }
+
+                for field_name in matching_partner_field_names:
+                    value = event_dto.raw_data.get(field_name)
+                    field_type = PartnerField.objects.get(name=field_name).type
+
+                    try:
+                        if field_type == 'int':
+                            int(value)
+                        elif field_type == 'float':
+                            float(value)
+                        elif field_type == 'string':
+                            str(value)
+                        elif field_type == 'object':
+                            if not isinstance(value, (dict, list)):
+                                validation_errors['errors'].append(
+                                    self.__transform_type_error(
+                                        field_name,
+                                        field_type,
+                                        value
+                                    )
+                                )
+                    except (ValueError, TypeError):
+                        validation_errors['errors'].append(
+                            self.__transform_type_error(
+                                field_name,
+                                field_type,
+                                value
+                            )
+                        )
+                
+                if len(validation_errors['errors']) > 0:
+                    event_dto.errors = validation_errors
+                    event_dto.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+
+                    return event_dto
+
         return super().process(event_dto)
 
     @staticmethod
@@ -62,3 +102,13 @@ class PermissionProcessor(ContributionProcessor):
             )
 
         return validation_errors
+    
+    @staticmethod
+    def __transform_type_error(name: str, type: str, value) -> Dict:
+        validation_error = {
+            'field': name,
+            'detail': f'Field "{name}" must be of type {type}, '
+            f'but received {type(value).__name__}'
+        }
+
+        return validation_error
