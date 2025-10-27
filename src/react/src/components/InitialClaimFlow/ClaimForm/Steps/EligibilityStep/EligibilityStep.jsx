5ec4498913f320c88194ae18a020672f1191dacf
VadimKovalenkoSNF <vadim.kovalenko@speedandfunction.com>
2025-10-27 18:53:27 +0400

    Move RELATIONSHIP_OPTIONS to constants.js

--- src/react/src/components/InitialClaimFlow/ClaimForm/Steps/EligibilityStep/EligibilityStep.jsx
+++ src/react/src/components/InitialClaimFlow/ClaimForm/Steps/EligibilityStep/EligibilityStep.jsx
@@ -16,31 +16,7 @@ import StyledSelect from '../../../../Filters/StyledSelect';
 import { getSelectStyles } from '../../../../../util/util';
 import { mainRoute } from '../../../../../util/constants';
 import eligibilityStepStyles from './styles';
-
-const RELATIONSHIP_OPTIONS = [
-    { value: 'owner', label: 'I am the owner of this production location' },
-    {
-        value: 'manager',
-        label: 'I am a manager working at this production location',
-    },
-    {
-        value: 'parent_company_owner_or_manager',
-        label: 'I represent the parent company that owns/manages this facility',
-    },
-    {
-        value: 'worker',
-        label:
-            "I work here but don't have management authority (will require supervisor verification)",
-    },
-    {
-        value: 'partner',
-        label: 'I am a buyer, supplier, agent, or other business partner',
-    },
-    {
-        value: 'other',
-        label: 'Other relationship',
-    },
-];
+import RELATIONSHIP_OPTIONS from './constants';
 
 const EligibilityStep = ({
     classes,
