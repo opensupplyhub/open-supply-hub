bc3510fd73df75bce99869e11fb558b5dfc5513d
vladsha-dev <newshapovalov2002@gmail.com>
2025-10-28 17:39:58 +0100

    Updated the comment

--- src/react/src/components/InitialClaimFlow/ClaimForm/Steps/utils.js
+++ src/react/src/components/InitialClaimFlow/ClaimForm/Steps/utils.js
@@ -1,6 +1,7 @@
 /**
- * Converts the value to an object that can be supported by the StyledSelect
- * component.
+ * Finds and returns the option object whose label matches the selectedLabel.
+ * Helps convert the value to an object that can be supported by the
+ * StyledSelect component.
  */
 const findSelectedOption = (options, selectedLabel) => {
     if (!selectedLabel) return null;
