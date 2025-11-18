b21386c2dcc1c237fa20c3d8ac273df78bff6d4f
vladsha-dev <newshapovalov2002@gmail.com>
2025-11-18 13:07:00 +0100

    Fixed tests

--- src/django/api/tests/test_facility_claim.py
+++ src/django/api/tests/test_facility_claim.py
@@ -395,7 +395,7 @@ class FacilityClaimTest(APITestCase):
         )
         self.assertEqual(
             claim.facility_type,
-            None,
+            'Manufacturing',
         )
         self.assertEqual(
             claim.point_of_contact_email,
