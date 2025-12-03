7934de83e0bd8ba1e0ff1ff10a0f89e8e635286e
Roman Stolar <roman.stolar@speedandfunction.com>
2025-12-03 17:08:51 +0200

    create FormFieldHint component for instructions purpose

---
+++ src/react/src/components/InitialClaimFlow/Shared/FormFieldHint/styles.js
@@ -0,0 +1,10 @@
+const styles = theme =>
+    Object.freeze({
+        hint: Object.freeze({
+            fontSize: '14px',
+            color: theme.palette.text.secondary,
+            margin: '-8px 0 8px 0',
+        }),
+    });
+
+export default styles;
