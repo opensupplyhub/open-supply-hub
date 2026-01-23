f87facba7524eb86209a893e161aba64785f8bf6
VadimKovalenkoSNF <vadim.kovalenko@speedandfunction.com>
2026-01-23 12:22:54 +0400

    Update static files backend settings

--- src/django/oar/settings.py
+++ src/django/oar/settings.py
@@ -612,7 +612,7 @@ if not DEBUG or (AWS_S3_ENDPOINT_URL and not TESTING):
             "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
         },
         "staticfiles": {
-            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+            "BACKEND": "spa.storage.SPAStaticFilesStorage",
         },
     }
 
