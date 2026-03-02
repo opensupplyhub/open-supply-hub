c4645f177f78f8c424c0f07e260507472d82c7da
VadimKovalenkoSNF <vadim.kovalenko@speedandfunction.com>
2026-03-02 18:44:27 +0400

    Update claim badge tooltip text

--- src/react/src/components/ProductionLocation/Heading/ClaimFlag/constants.js
+++ src/react/src/components/ProductionLocation/Heading/ClaimFlag/constants.js
@@ -1,4 +1,4 @@
 export const CLAIMED_PROFILE_TOOLTIP_TEXT =
-    "This profile has been claimed and verified by the production location's owner or manager. Claimed data is considered the most authoritative source.";
+    "This profile has been claimed by the production location's owner, manager or by an authorized employee, which allows you to see a larger suite of information provided directly by the production location.";
 export const CLAIM_LEARN_MORE_URL =
     'https://info.opensupplyhub.org/resources/claim-a-facility';
