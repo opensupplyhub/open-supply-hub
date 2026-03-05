0c2645bac28c732f6205b2e846c4fdbf8af62f8a
Emily Tanabe <emilytanabe@Emilys-MacBook-Pro.local>
2026-03-05 16:50:29 -0700

    fixing plural_map issues

--- src/react/src/components/ProductionLocation/Sidebar/SupplyChain/utils.js
+++ src/react/src/components/ProductionLocation/Sidebar/SupplyChain/utils.js
@@ -1,9 +1,14 @@
 const PLURAL_MAP = {
-    'Brand / Retailer': 'Brand / Retailers',
+    'Academic / Researcher / Journalist / Student':
+        'Academics / Researchers / Journalists / Students',
+    'Auditor / Certification Scheme / Service Provider':
+        'Auditors / Certification Schemes / Service Providers',
+    'Brand / Retailer': 'Brands / Retailers',
     'Civil Society Organization': 'Civil Society Organizations',
-    Auditor: 'Auditors',
-    Government: 'Governments',
+    'Facility / Factory / Manufacturing Group / Supplier / Vendor':
+        'Facilities / Factories / Manufacturing Groups / Suppliers / Vendors',
     'Multi-Stakeholder Initiative': 'Multi-Stakeholder Initiatives',
+    Union: 'Unions',
     Other: 'Others',
 };
 
