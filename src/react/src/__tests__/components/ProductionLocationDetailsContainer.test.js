13f778c7ea834c6dfab145345f0a302bec27958e
VadimKovalenkoSNF <vadim.kovalenko@speedandfunction.com>
2026-03-10 11:08:45 +0400

    Fix unit test

--- src/react/src/__tests__/components/ProductionLocationDetailsContainer.test.js
+++ src/react/src/__tests__/components/ProductionLocationDetailsContainer.test.js
@@ -28,6 +28,7 @@ jest.mock(
 jest.mock('../../actions/facilities', () => ({
     fetchSingleFacility: () => ({ type: 'noop' }),
     resetSingleFacility: () => ({ type: 'RESET_SINGLE_FACILITY' }),
+    fetchFacilities: () => () => {},
 }));
 
 jest.mock('../../actions/partnerFieldGroups', () => ({
