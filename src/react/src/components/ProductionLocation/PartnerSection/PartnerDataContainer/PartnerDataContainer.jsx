6af9290e8cad19c50633e2df53c1641910ad7b65
Stephan Protsack <stephan@speedandfunction.com>
2026-03-10 15:37:46 +0100

    Add loading for the partner fields section

--- src/react/src/components/ProductionLocation/PartnerSection/PartnerDataContainer/PartnerDataContainer.jsx
+++ src/react/src/components/ProductionLocation/PartnerSection/PartnerDataContainer/PartnerDataContainer.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 
 import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import InfoOutlined from '@material-ui/icons/InfoOutlined';
 import PartnershipIcon from '../../../Icons/Partnership';
@@ -12,7 +13,7 @@ import PartnerSectionItem from '../ParentSectionItem/PartnerSectionItem';
 import partnerDataContainerStyles from './styles';
 import getPartnerFieldsAndGroups from './utils';
 
-function PartnerDataContainer({ classes, groups, facilityData }) {
+function PartnerDataContainer({ classes, groups, facilityData, fetching }) {
     const { partnerFields, partnerGroups } = useMemo(
         () => getPartnerFieldsAndGroups(facilityData, groups),
         [facilityData, groups],
@@ -22,57 +23,64 @@ function PartnerDataContainer({ classes, groups, facilityData }) {
         <Grid container className={classes.root}>
             <Grid item xs={12}>
                 <div className={classes.titleRow}>
-                    <PartnershipIcon className={classes.icon} />
+                    {!fetching && <PartnershipIcon className={classes.icon} />}
+                    {fetching && <CircularProgress size={24} />}
                     <Typography
                         variant="title"
                         className={classes.title}
                         component="h3"
                     >
-                        Partner Data
+                        {!fetching ? 'Partner Data' : 'Loading Partner Data...'}
                     </Typography>
-                    <IconComponent
-                        title="Information provided by third-party partners who host additional social or environmental data."
-                        icon={InfoOutlined}
-                        className={classes.infoButton}
-                    />
+                    {!fetching && (
+                        <IconComponent
+                            title="Information provided by third-party partners who host additional social or environmental data."
+                            icon={InfoOutlined}
+                            className={classes.infoButton}
+                        />
+                    )}
                 </div>
-                <Typography
-                    variant="subheading"
-                    className={classes.description}
-                >
-                    The following information is provided by third-party
-                    partners who host additional social or environmental data
-                    related to this production location, its context, and/or its
-                    operations.{' '}
-                    <a
-                        href="https://info.opensupplyhub.org/data-integrations"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                {!fetching && (
+                    <Typography
+                        variant="subheading"
+                        className={classes.description}
                     >
-                        Learn more.
-                    </a>
-                </Typography>
+                        The following information is provided by third-party
+                        partners who host additional social or environmental
+                        data related to this production location, its context,
+                        and/or its operations.{' '}
+                        <a
+                            href="https://info.opensupplyhub.org/data-integrations"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Learn more.
+                        </a>
+                    </Typography>
+                )}
             </Grid>
-            {partnerGroups.map(group => (
-                <Grid item xs={12} key={group.uuid} id={group.uuid}>
-                    <PartnerSectionItem
-                        group={group}
-                        partnerFields={partnerFields}
-                    />
-                </Grid>
-            ))}
+            {!fetching &&
+                partnerGroups.map(group => (
+                    <Grid item xs={12} key={group.uuid} id={group.uuid}>
+                        <PartnerSectionItem
+                            group={group}
+                            partnerFields={partnerFields}
+                        />
+                    </Grid>
+                ))}
         </Grid>
     );
 }
 
 const mapStateToProps = ({
-    partnerFieldGroups: { data },
+    partnerFieldGroups: { data, fetching },
     facilities: {
         singleFacility: { data: facilityData },
     },
 }) => ({
     groups: data?.results || [],
     facilityData,
+    fetching,
 });
 
 export default connect(mapStateToProps)(
