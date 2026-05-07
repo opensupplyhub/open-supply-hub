dfe0c68e1e97adf0e3fefd24f3943768ed7318b2
VadimKovalenkoSNF <vadim.kovalenko@speedandfunction.com>
2026-05-07 13:08:38 +0400

    Update release notes and post-deploy script

--- src/django/api/management/commands/post_deployment.py
+++ src/django/api/management/commands/post_deployment.py
@@ -10,4 +10,3 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         call_command('migrate')
         call_command('reindex_database')
-        call_command('reindex_locations_with_approved_claim')
