cbd263edbdede38255a446966c6ed51b3f126762
vlad-shapik <vlad.shapik@speedandfunction.com>
2026-05-20 18:01:49 +0200

    [Backmerge][OSDEV-2717] Run migrate and reindex in post_deployment
    Restore post_deployment to run only migrate and reindex_database so release backmerge behavior matches deployment requirements.

--- src/django/api/management/commands/post_deployment.py
+++ src/django/api/management/commands/post_deployment.py
@@ -1,3 +1,4 @@
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 
@@ -7,4 +8,5 @@ class Command(BaseCommand):
             'post-deployment tasks.')
 
     def handle(self, *args, **options):
-        pass
+        call_command('migrate')
+        call_command('reindex_database')
