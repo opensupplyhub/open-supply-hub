e43a78c5ae3b38c639e294800014d674fd2f6fcf
James Johnson <james@opensupplyhub.org>
2026-06-10 11:42:55 -0400

    Add unsaved propTypes change and remove mistaken setupProxy change

--- src/react/src/setupProxy.js
+++ src/react/src/setupProxy.js
@@ -1,4 +1,4 @@
-const createProxyMiddleware = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const _ = require('lodash');
 
 const pathsToProxy = Object.freeze([
