8d0e3b60b08919d44d49f36755870aa9fe8f8ccc
Max Valencia <max@opensupplyhub.org>
2026-07-01 09:25:07 -0600

    Addressing comments

--- deployment/terraform/modules/ecr-repository/main.tf
+++ deployment/terraform/modules/ecr-repository/main.tf
@@ -10,6 +10,10 @@
 resource "aws_ecr_repository" "default" {
   name                 = var.repository_name
   image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
 }
 
 resource "aws_ecr_lifecycle_policy" "default" {
