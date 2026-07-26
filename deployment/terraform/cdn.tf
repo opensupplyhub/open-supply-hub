493377215ed933ea0481a682ee2933019d90ffd4
Max Valencia <max@opensupplyhub.org>
2026-07-25 22:55:00 -0600

    Support query params and put them in the route

--- deployment/terraform/cdn.tf
+++ deployment/terraform/cdn.tf
@@ -162,12 +162,30 @@ resource "aws_cloudfront_function" "homepage_uri_rewrite" {
       // contributor filter, search, etc.). Redirect to /map preserving the
       // full query string so existing URLs continue to work without any
       // changes on the customer side.
-      if (qs) {
+      //
+      // In CloudFront Functions runtime 1.0, request.querystring is an object
+      // ({ key: { value: "..." } }) not a string, so we serialize it manually.
+      // Multi-value params (e.g. ?country=US&country=UK) are stored as
+      // { multiValue: [{ value: "US" }, { value: "UK" }] } and expanded here.
+      var keys = Object.keys(qs);
+      if (keys.length > 0) {
+        var parts = [];
+        for (var i = 0; i < keys.length; i++) {
+          var key = keys[i];
+          var param = qs[key];
+          if (param.multiValue) {
+            for (var j = 0; j < param.multiValue.length; j++) {
+              parts.push(key + '=' + param.multiValue[j].value);
+            }
+          } else {
+            parts.push(key + '=' + param.value);
+          }
+        }
         return {
           statusCode: 302,
           statusDescription: 'Found',
           headers: {
-            location: { value: '/map?' + qs }
+            location: { value: '/map?' + parts.join('&') }
           }
         };
       }
