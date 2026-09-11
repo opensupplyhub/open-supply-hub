430d6cf001aac0ae54f103f4b00ffdf98c479228
Max Valencia <max@opensupplyhub.org>
2026-09-11 13:15:09 -0600

    Addressing migrations

--- src/django/api/migrations/0240_claim_attachment_cascade_and_upload_prefix.py
+++ src/django/api/migrations/0240_claim_attachment_cascade_and_upload_prefix.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
     """
 
     dependencies = [
-        ('api', '0238_add_claimant_updated_at'),
+        ('api', '0239_add_claims_v2_dashboard_switch'),
     ]
 
     operations = [
